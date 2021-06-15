c2ef31df8096428e602acb25631de017a7f14654
nakanakagames <nakanakagames@gmail.com>
2021-06-16 08:37:26 +0900

    Create BukkitPlayerEntity class

---
+++ src/main/java/tokyo/nakanaka/bukkit/BukkitPlayerEntity.java
@@ -0,0 +1,43 @@
+package tokyo.nakanaka.bukkit;
+
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+
+import tokyo.nakanaka.shapeGenerator.PlayerEntity;
+import tokyo.nakanaka.world.World;
+
+public class BukkitPlayerEntity implements PlayerEntity{
+	private Server server;
+	private Player player;
+	
+	public BukkitPlayerEntity(Server server, Player player) {
+		this.server = server;
+		this.player = player;
+	}
+
+	@Override
+	public World getWorld() {
+		return new BukkitWorld(this.server, this.player.getWorld());
+	}
+
+	@Override
+	public int getX() {
+		return this.player.getLocation().getBlockX();
+	}
+
+	@Override
+	public int getY() {
+		return this.player.getLocation().getBlockY();
+	}
+
+	@Override
+	public int getZ() {
+		return this.player.getLocation().getBlockZ();
+	}
+
+	@Override
+	public void print(String msg) {
+		this.player.sendMessage(msg);
+	}
+
+}
