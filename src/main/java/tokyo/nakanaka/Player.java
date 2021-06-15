7e1e9c6761d5a9aca04808063de8d828b354b8a0
nakanakagames <nakanakagames@gmail.com>
2021-06-15 21:14:46 +0900

    Create Player class

---
+++ src/main/java/tokyo/nakanaka/Player.java
@@ -0,0 +1,74 @@
+package tokyo.nakanaka;
+
+import java.util.UUID;
+
+import tokyo.nakanaka.logger.Logger;
+import tokyo.nakanaka.world.World;
+
+public class Player {
+	private UUID uid;
+	private String name;
+	private Logger logger;
+	private World world;
+	private int x;
+	private int y;
+	private int z;
+	
+	public Player(UUID uid, String name) {
+		this.uid = uid;
+		this.name = name;
+	}
+
+	public Player(UUID uid) {
+		this.uid = uid;
+	}
+	
+	public UUID getUniqueId() {
+		return uid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+	public World getWorld() {
+		return world;
+	}
+
+	public void setWorld(World world) {
+		this.world = world;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	public void print(String msg) {
+		this.logger.print(msg);
+	}
+	
+}
