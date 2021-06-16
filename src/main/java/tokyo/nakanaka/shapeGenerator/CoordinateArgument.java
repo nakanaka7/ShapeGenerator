0f57e23f6e83e1510ee2c7666438d2ed095df569
nakanakagames <nakanakagames@gmail.com>
2021-06-16 15:22:30 +0900

    create CoordinateArugment class

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/CoordinateArgument.java
@@ -0,0 +1,31 @@
+package tokyo.nakanaka.shapeGenerator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CoordinateArgument {
+	public int onParsing(String arg, int offset) {
+		int s;
+		try {
+			s = Integer.parseInt(arg);
+		}catch (IllegalArgumentException e) {
+			if (!arg.startsWith("~")) {
+				throw new IllegalArgumentException();
+			}else {
+				arg = arg.substring(1);
+				int t;
+				if(arg.isEmpty()){
+					t = 0;
+				}else{
+					t = Integer.parseInt(arg);
+				}
+				s = offset + t;
+			}
+		}
+		return s;
+	}
+	
+	public List<String> onTabComplete(String arg) {
+		return Arrays.asList("~");
+	}
+}
