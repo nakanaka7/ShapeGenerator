2253a5b3d7b1d659a85198f304e79cd5d9756cd2
nakanakagames <nakanakagames@gmail.com>
2021-06-16 21:43:41 +0900

    Cleaned CommandLine class

--- src/main/java/tokyo/nakanaka/commandLine/CommandLine.java
+++ src/main/java/tokyo/nakanaka/commandLine/CommandLine.java
@@ -5,6 +5,8 @@ import java.util.List;
 import tokyo.nakanaka.shapeGenerator.Player;
 
 public interface CommandLine {
+	String getLabel();
+	List<String> getAliases();
 	boolean onCommand(Player player, String[] args);
 	List<String> onTabComplete(Player player, String[] args);
 }
