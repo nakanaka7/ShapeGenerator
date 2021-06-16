8a2016630bccd68ed24850243cb60db043f96b03
nakanakagames <nakanakagames@gmail.com>
2021-06-16 22:09:51 +0900

    Create SelCommandLine class

--- src/main/java/tokyo/nakanaka/commandLine/SelCommandLine.java
+++ src/main/java/tokyo/nakanaka/commandLine/SelCommandLine.java
@@ -1,5 +1,39 @@
 package tokyo.nakanaka.commandLine;
 
-public class SelCommandLine {
+import java.util.Arrays;
+import java.util.List;
+
+import tokyo.nakanaka.selection.SelectionBuilder;
+import tokyo.nakanaka.shapeGenerator.Player;
+import tokyo.nakanaka.world.World;
+
+public class SelCommandLine implements CommandLine{
+
+	@Override
+	public String getLabel() {
+		return "selection";
+	}
+
+	@Override
+	public List<String> getAliases() {
+		return Arrays.asList("sel");
+	}
+
+	@Override
+	public boolean onCommand(Player player, String[] args) {
+		World world = player.getWorld();
+		int offsetX = player.getX();
+		int offsetY = player.getY();
+		int offsetZ = player.getZ();
+		SelectionBuilder builder = player.getSelectionBuilder();
+		boolean success = builder.onCommand(world, offsetX, offsetY, offsetZ, args);
+		return success;
+	}
+
+	@Override
+	public List<String> onTabComplete(Player player, String[] args) {
+		SelectionBuilder builder = player.getSelectionBuilder();
+		return builder.onTabComplete(args);
+	}
 
 }
