854df708418a7b92850d76ff45085b3ec132ea08
nakanakagames <nakanakagames@gmail.com>
2021-06-16 21:33:33 +0900

    Create CommandLine interface

---
+++ src/main/java/tokyo/nakanaka/commandLine/CommandLine.java
@@ -0,0 +1,10 @@
+package tokyo.nakanaka.commandLine;
+
+import java.util.List;
+
+import tokyo.nakanaka.shapeGenerator.Player;
+
+public interface CommandLine {
+	boolean onCommand(Player player, String[] args);
+	List<String> onTabComplete(Player player, String[] args);
+}
