429ea6120d12cd9b2e185761fea607620acaa536
nakanakagames <nakanakagames@gmail.com>
2021-06-17 16:11:14 +0900

    Develop

--- src/main/java/tokyo/nakanaka/CommandHandler.java
+++ src/main/java/tokyo/nakanaka/CommandHandler.java
@@ -1,10 +1,18 @@
 package tokyo.nakanaka;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import tokyo.nakanaka.commandLine.CommandLine;
 import tokyo.nakanaka.commandLine.CommandLineRepository;
 
 public class CommandHandler {
 	private CommandLineRepository cmdLineRepo = new CommandLineRepository();
+	
+	public void register(CommandLine cmdLine) {
+		this.cmdLineRepo.register(cmdLine);
+	}
 	
 	public void onCommand(Player player, String alias, String[] args) {
 		CommandLine cmdLine = this.cmdLineRepo.findBy(alias);
@@ -15,5 +23,23 @@ public class CommandHandler {
 			}
 		}
 		player.getLogger().print("Invalid command");
+	}
+	
+	public List<String> onTabComplete(Player player, String alias, String[] args){
+		CommandLine cmdLine = this.cmdLineRepo.findBy(alias);
+		if(cmdLine != null) {
+			return cmdLine.onTabComplete(player, args);
+		}
+		return new ArrayList<>();
+	}
+	
+	public List<String> getAliases(){
+		List<String> aliasList = new ArrayList<>();
+		Set<CommandLine> set = this.cmdLineRepo.getAll();
+		for(CommandLine e : set) {
+			List<String> list = e.getAliases();
+			aliasList.addAll(list);
+		}
+		return aliasList;
 	}
 }
