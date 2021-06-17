603b46a5ef06c4e3df3ae564a22d25eb3ac49067
nakanakagames <nakanakagames@gmail.com>
2021-06-17 09:52:43 +0900

    Add java doc @throw

--- src/main/java/tokyo/nakanaka/selection/SelectionBuilder.java
+++ src/main/java/tokyo/nakanaka/selection/SelectionBuilder.java
@@ -9,5 +9,8 @@ public interface SelectionBuilder {
 	boolean onRightClickBlock(World world, int x, int y, int z);
 	boolean onCommand(World world, int offsetX, int offsetY, int offsetZ, String[] args);
 	List<String> onTabComplete(String[] args);
+	/**
+	 * @throw IllegalStateException
+	 */
 	Selection build();
 }
