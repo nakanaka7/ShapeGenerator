95160a65bbf3af58008d4dfb3a755b2443aee766
nakanakagames <nakanakagames@gmail.com>
2021-06-17 16:36:39 +0900

    Create CommandLine class

---
+++ src/main/java/tokyo/nakanaka/commandLine/CommandLine.java
@@ -0,0 +1,28 @@
+package tokyo.nakanaka.commandLine;
+
+import tokyo.nakanaka.Player;
+
+public class CommandLine {
+	private Player player;
+	private String alias;
+	private String[] args;
+	
+	public CommandLine(Player player, String alias, String[] args) {
+		this.player = player;
+		this.alias = alias;
+		this.args = args;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public String[] getArgs() {
+		return args;
+	}
+
+}
