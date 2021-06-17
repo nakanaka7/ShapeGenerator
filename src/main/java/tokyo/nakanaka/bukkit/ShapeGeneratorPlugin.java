83f1631ff85b01356633a290aa880e2f71d465bd
nakanakagames <nakanakagames@gmail.com>
2021-06-17 10:43:29 +0900

    Register command lines to be able to run them

--- src/main/java/tokyo/nakanaka/bukkit/ShapeGeneratorPlugin.java
+++ src/main/java/tokyo/nakanaka/bukkit/ShapeGeneratorPlugin.java
@@ -1,7 +1,5 @@
 package tokyo.nakanaka.bukkit;
 
-import static tokyo.nakanaka.logger.LogConstant.HEAD_NORMAL;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -10,34 +8,57 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static tokyo.nakanaka.logger.LogConstant.*;
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.PlayerEntity;
-import tokyo.nakanaka.block.Block;
-import tokyo.nakanaka.logger.Logger;
+import tokyo.nakanaka.commandLine.CommandLine;
+import tokyo.nakanaka.commandLine.CommandLineRepository;
+import tokyo.nakanaka.commandLine.GenerateCommandLine;
+import tokyo.nakanaka.commandLine.SelCommandLine;
+import tokyo.nakanaka.commandLine.SelShapeCommandLine;
 
 public class ShapeGeneratorPlugin extends JavaPlugin{
-	private Map<UUID, Player> playerMap = new HashMap<>(); 
+	private Map<UUID, Player> playerMap = new HashMap<>();
+	private CommandLineRepository cmdLineRepo = new CommandLineRepository();
+	
+	@Override
+	public void onEnable() {
+		this.cmdLineRepo.register(new SelCommandLine());
+		this.cmdLineRepo.register(new SelShapeCommandLine());
+		this.cmdLineRepo.register(new GenerateCommandLine());
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Logger logger = new BukkitLogger(sender);
 		Player player = null;
 		if(sender instanceof org.bukkit.entity.Player) {
 			org.bukkit.entity.Player bplayer = (org.bukkit.entity.Player)sender;
 			PlayerEntity pe = new BukkitPlayerEntity(this.getServer(), bplayer);
 			player = this.playerMap.get(pe.getUniqueID());
-			player.setLogger(logger);
+			player.setLogger(pe);
 			player.setWorld(pe.getWorld());
 			player.setX(pe.getX());
 			player.setY(pe.getY());
 			player.setZ(pe.getZ());
+		}else {
+			return true;
 		}
-		String s = args[0];
-		Block b = Block.valueOf(s);
-		int x = player.getX();
-		int y = player.getY();
-		int z = player.getZ();
-		player.getWorld().setBlock(x, y, z, b, false);
-		logger.print(HEAD_NORMAL + "set");
-		return true;
+		if(args.length == 0) {
+			return true;
+		}
+		String shiftAlias = args[0];
+		String[] shiftArgs = new String[args.length - 1];
+		System.arraycopy(args, 1, shiftArgs, 0, shiftArgs.length);
+		CommandLine cmdLine = this.cmdLineRepo.findBy(shiftAlias);
+		if(cmdLine != null) {
+			boolean success = cmdLine.onCommand(player, shiftArgs);
+			if(!success) {
+				player.getLogger().print(HEAD_ERROR + "Usage");
+			}
+			return true;
+		}else {
+			player.getLogger().print("Type " + "\"/" + "sg" + " help\" " + "for help");
+			return true;
+		}
 	}
 }
