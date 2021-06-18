2aa1022603148c24a82c8f1a28c9625e9845a3e4
nakanakagames <nakanakagames@gmail.com>
2021-06-18 19:39:36 +0900

    Add some methods

--- src/main/java/tokyo/nakanaka/math/BlockVector3D.java
+++ src/main/java/tokyo/nakanaka/math/BlockVector3D.java
@@ -1,5 +1,7 @@
 package tokyo.nakanaka.math;
 
+import com.gmail.q19570678.math.BlockVector3D;
+
 public class BlockVector3D {
 	private int x;
 	private int y;
@@ -22,4 +24,50 @@ public class BlockVector3D {
 	public int getZ() {
 		return this.z;
 	}
+	
+	public BlockVector3D add(BlockVector3D vec) {
+		return new BlockVector3D(this.x + vec.getX(), this.y + vec.getY(), this.z + vec.getZ());
+	}
+	
+	public BlockVector3D negate(BlockVector3D vec) {
+		return new BlockVector3D(this.x - vec.getX(), this.y - vec.getY(), this.z - vec.getZ());
+	}
+	
+	public BlockVector3D multiply(int times) {
+		return new BlockVector3D(times * this.x, times * this.y, times * this.z);
+	}
+	
+	public double getAbsolute() {
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		result = prime * result + z;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BlockVector3D other = (BlockVector3D) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		if (z != other.z)
+			return false;
+		return true;
+	}
+	
+	
 }
