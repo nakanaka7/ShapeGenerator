f2ca3f4a708c2b2f0f7e8d2cf54d159e9644f318
nakanakagames <nakanakagames@gmail.com>
2021-06-19 16:49:36 +0900

    Create SelectionMessenger class

---
+++ src/main/java/tokyo/nakanaka/selection/SelectionMessenger.java
@@ -0,0 +1,26 @@
+package tokyo.nakanaka.selection;
+
+import static tokyo.nakanaka.logger.LogConstant.HEAD_NORMAL;
+import static tokyo.nakanaka.logger.LogConstant.INDENT_NORMAL;
+
+import java.util.List;
+
+import tokyo.nakanaka.Player;
+
+public class SelectionMessenger {
+	private SelectionManager selManager;
+
+	public SelectionMessenger(SelectionManager selManager) {
+		this.selManager = selManager;
+	}
+	
+	public void sendMessage(Player player) {
+		SelectionBuilder builder = player.getSelectionBuilder();
+		SelectionShape shape = this.selManager.getSelectionShape(builder);
+		player.getLogger().print(HEAD_NORMAL + shape.toString() + " selection");
+		List<String> lines = builder.getStateLines();
+		for(String line : lines) {
+			player.getLogger().print(INDENT_NORMAL + line);
+		}
+	}
+}
