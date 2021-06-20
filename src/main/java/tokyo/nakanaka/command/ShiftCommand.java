a1904dd2ac461b23bc82e0918909debb7a324ab1
nakanakagames <nakanakagames@gmail.com>
2021-06-20 09:24:42 +0900

    Complete ShiftCommand

--- src/main/java/tokyo/nakanaka/command/ShiftCommand.java
+++ src/main/java/tokyo/nakanaka/command/ShiftCommand.java
@@ -1,28 +1,34 @@
 package tokyo.nakanaka.command;
 
+import tokyo.nakanaka.selection.Selection;
+
 public class ShiftCommand implements MoveCommand{
 	private GenerateCommand originalCmd;
+	private GenerateCommand newCmd;
 	
-	public ShiftCommand(GenerateCommand originalCmd, int dx, int dy, int dz) {
+	public ShiftCommand(GenerateCommand originalCmd, int dx, int dy, int dz, boolean physics) {
 		this.originalCmd = originalCmd;
+		Selection sel = originalCmd.getSelection();
+		sel.shift(dx, dy, dz);
+		this.newCmd = new GenerateCommand(sel, originalCmd.getBlock(), physics);
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		this.originalCmd.undo();
+		this.newCmd.execute();
 	}
 
 	@Override
 	public void undo() {
-		// TODO Auto-generated method stub
-		
+		this.newCmd.undo();
+		this.originalCmd.redo();
 	}
 
 	@Override
 	public void redo() {
-		// TODO Auto-generated method stub
-		
+		this.originalCmd.undo();
+		this.newCmd.redo();
 	}
 
 	@Override
