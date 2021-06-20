2fef4f24d2aa99ff523f0791a3c7555a01c9d28b
nakanakagames <nakanakagames@gmail.com>
2021-06-20 16:41:10 +0900

    BoundRegion fix

--- src/main/java/tokyo/nakanaka/math/region3D/TransformedRegion3D.java
+++ src/main/java/tokyo/nakanaka/math/region3D/TransformedRegion3D.java
@@ -4,24 +4,27 @@ import tokyo.nakanaka.math.LinearTransformation;
 import tokyo.nakanaka.math.Vector3D;
 
 public class TransformedRegion3D implements Region3D{
-	private Region3D original;
+	private Region3D shiftOriginal;
 	private LinearTransformation invTrans;
+	private Vector3D offset;
 	
 	/**
 	 * @param original
 	 * @param trans
 	 * @throws SingularOperatorException
 	 */
-	public TransformedRegion3D(Region3D original, LinearTransformation trans) {
-		this.original = original;
+	public TransformedRegion3D(Region3D original, LinearTransformation trans, Vector3D offset) {
+		this.shiftOriginal = new ShiftedRegion3D(original, - offset.getX(), - offset.getY(), - offset.getZ());
 		this.invTrans = trans.getInverse();
+		this.offset = offset;
 	}
 
 	@Override
 	public boolean contains(double x, double y, double z) {
 		Vector3D v = new Vector3D(x, y, z);
-		Vector3D w = this.invTrans.apply(v);
-		return this.original.contains(w.getX(), w.getY(), w.getZ());
+		v = v.negate(this.offset);
+		v = this.invTrans.apply(v);
+		return this.shiftOriginal.contains(v.getX(), v.getY(), v.getZ());
 	}
 	
 	
