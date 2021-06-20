24c315736f1c322be85870a38d091199d41ca81b
nakanakagames <nakanakagames@gmail.com>
2021-06-20 09:07:18 +0900

    Refactored

--- src/main/java/tokyo/nakanaka/command/GenerateCommand.java
+++ src/main/java/tokyo/nakanaka/command/GenerateCommand.java
@@ -5,54 +5,21 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import tokyo.nakanaka.BlockRegion3D;
 import tokyo.nakanaka.block.Block;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.selection.Selection;
 import tokyo.nakanaka.world.World;
 
 public class GenerateCommand implements UndoableCommand{
-	private World world;
-	private BlockRegion3D region;
+	private Selection sel;
 	private Block block;
 	private boolean physics;
 	private Map<BlockVector3D, Block> originalBlockMap = new HashMap<>();
 	
 	public GenerateCommand(Selection sel, Block block, boolean physics) {
-		this.world = sel.getWorld();
-		this.region = sel.getBlockRegion3D();
+		this.sel = sel;
 		this.block = block;
 		this.physics = physics;
-	}
-	
-	public GenerateCommand(Builder builder) {
-		this.world = builder.world;
-		this.region = builder.region;
-		this.block = builder.block;
-		this.physics = builder.physics;
-	}
-	
-	public static class Builder{
-		private World world;
-		private BlockRegion3D region;
-		private Block block;
-		private boolean physics = false;
-		
-		public Builder(World world, BlockRegion3D region, Block block) {
-			this.world = world;
-			this.region = region;
-			this.block = block;
-		}
-
-		public Builder physics(boolean physics) {
-			this.physics = physics;
-			return this;
-		}
-		
-		public GenerateCommand build() {
-			return new GenerateCommand(this);
-		}
-
 	}
 	
 	public Block getBlock() {
@@ -64,27 +31,30 @@ public class GenerateCommand implements UndoableCommand{
 	 */
 	@Override
 	public void execute() {
-		Set<BlockVector3D> vecSet = this.region.asVectorSet();
+		World world = this.sel.getWorld();
+		Set<BlockVector3D> vecSet = this.sel.getBlockRegion3D().asVectorSet();
 		for(BlockVector3D v : vecSet) {
-			Block block = this.world.getBlock(v.getX(), v.getY(), v.getZ());
-			this.world.setBlock(v.getX(), v.getY(), v.getZ(), this.block, this.physics);
+			Block block = world.getBlock(v.getX(), v.getY(), v.getZ());
+			world.setBlock(v.getX(), v.getY(), v.getZ(), this.block, this.physics);
 			this.originalBlockMap.put(v, block);
 		}
 	}
 
 	@Override
 	public void undo() {
+		World world = this.sel.getWorld();
 		for(Entry<BlockVector3D, Block> e : this.originalBlockMap.entrySet()) {
 			BlockVector3D pos = e.getKey();
-			this.world.setBlock(pos.getX(), pos.getY(), pos.getZ(), e.getValue(), this.physics);
+			world.setBlock(pos.getX(), pos.getY(), pos.getZ(), e.getValue(), this.physics);
 		}
 	}
 
 	@Override
 	public void redo() {
-		Set<BlockVector3D> vecSet = this.region.asVectorSet();
+		World world = this.sel.getWorld();
+		Set<BlockVector3D> vecSet = this.sel.getBlockRegion3D().asVectorSet();
 		for(BlockVector3D v : vecSet) {
-			this.world.setBlock(v.getX(), v.getY(), v.getZ(), this.block, this.physics);
+			world.setBlock(v.getX(), v.getY(), v.getZ(), this.block, this.physics);
 		}
 	}
 
