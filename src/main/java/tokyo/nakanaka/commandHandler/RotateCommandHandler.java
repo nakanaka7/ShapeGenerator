5010bfd859a04d449f3d25ef1f962e8e4aff4172
nakanakagames <nakanakagames@gmail.com>
2021-06-20 21:55:24 +0900

    Complete Rotate Command

--- src/main/java/tokyo/nakanaka/commandHandler/RotateCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/RotateCommandHandler.java
@@ -1,18 +1,25 @@
 package tokyo.nakanaka.commandHandler;
 
+import static tokyo.nakanaka.logger.LogConstant.HEAD_ERROR;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import tokyo.nakanaka.Axis;
 import tokyo.nakanaka.Pair;
 import tokyo.nakanaka.Player;
+import tokyo.nakanaka.UndoCommandManager;
+import tokyo.nakanaka.command.GenerateCommand;
+import tokyo.nakanaka.command.MoveCommand;
+import tokyo.nakanaka.command.RotateCommand;
+import tokyo.nakanaka.command.UndoableCommand;
 
 public class RotateCommandHandler implements CommandHandler{
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Rotate the generated blocks";
 	}
 
 	@Override
@@ -27,20 +34,54 @@ public class RotateCommandHandler implements CommandHandler{
 
 	@Override
 	public String getUsage() {
-		// TODO Auto-generated method stub
-		return null;
+		return "rotate x|y|z <degree>";
 	}
 
 	@Override
 	public List<Pair<String, String>> getParameterDescriptionList() {
-		// TODO Auto-generated method stub
-		return null;
+		Pair<String, String> desAxis = new Pair<>("x|y|z", "rotation axis");
+		Pair<String, String> desDegree = new Pair<>("<degree>", "angle of rotation (right-hand rule)");
+		return Arrays.asList(desAxis, desDegree);
 	}
 
 	@Override
 	public boolean onCommand(Player player, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+		if(args.length != 2) {
+			return false;
+		}
+		Axis axis;
+		try{
+			axis = Axis.valueOf(args[0].toUpperCase());
+		}catch(IllegalArgumentException e) {
+			player.getLogger().print(HEAD_ERROR + "Can not parse axis");
+			return true;
+		}
+		double degree;
+		try {
+			degree = Double.valueOf(args[1]);
+		}catch(IllegalArgumentException e) {
+			player.getLogger().print(HEAD_ERROR + "Can not parse double");
+			return true;
+		}
+		UndoCommandManager undoManager = player.getUndoCommandManager();
+		UndoableCommand cmd = undoManager.peekUndoCommand();
+		if(cmd == null) {
+			player.getLogger().print(HEAD_ERROR + "Generate blocks first");
+			return true;
+		}
+		GenerateCommand originalCmd;
+		if(cmd instanceof GenerateCommand) {
+			originalCmd = (GenerateCommand) cmd;
+		}else if(cmd instanceof MoveCommand) {
+			originalCmd = ((MoveCommand)cmd).getLastCommand();
+		}else {
+			player.getLogger().print(HEAD_ERROR + "Invalid Operation");
+			return true;
+		}
+		RotateCommand rotateCmd = new RotateCommand(originalCmd, axis, degree, player.getBlockPhysics());
+		rotateCmd.execute();
+		undoManager.add(rotateCmd);
+		return true;
 	}
 
 	@Override
