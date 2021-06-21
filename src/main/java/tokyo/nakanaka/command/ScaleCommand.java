8b0066b2d0ff47d63a614ed6ddaddaad2460e3a3
nakanakagames <nakanakagames@gmail.com>
2021-06-21 10:15:38 +0900

    Create ScaleCommand class

---
+++ src/main/java/tokyo/nakanaka/command/ScaleCommand.java
@@ -0,0 +1,54 @@
+package tokyo.nakanaka.command;
+
+import tokyo.nakanaka.Axis;
+import tokyo.nakanaka.math.LinearTransformation;
+import tokyo.nakanaka.selection.Selection;
+
+public class ScaleCommand implements MoveCommand{
+	private GenerateCommand originalCmd;
+	private GenerateCommand lastCmd;
+	
+	public ScaleCommand(GenerateCommand originalCmd, Axis axis, double factor, boolean physics) {
+		this.originalCmd = originalCmd;
+		LinearTransformation trans;
+		switch(axis) {
+		case X:
+			trans = LinearTransformation.ofXScale(factor);
+			break;
+		case Y:
+			trans = LinearTransformation.ofYScale(factor);
+			break;
+		case Z:
+			trans = LinearTransformation.ofZScale(factor);
+			break;
+		default:
+			return;
+		}
+		Selection sel = originalCmd.getSelection().getTransformedSelection(trans);
+		this.lastCmd = new GenerateCommand(sel, originalCmd.getBlock(), physics);
+	}
+	
+	@Override
+	public void execute() {
+		this.originalCmd.undo();
+		this.lastCmd.execute();
+	}
+
+	@Override
+	public void undo() {
+		this.lastCmd.undo();
+		this.originalCmd.redo();
+	}
+
+	@Override
+	public void redo() {
+		this.originalCmd.undo();
+		this.lastCmd.redo();
+	}
+
+	@Override
+	public GenerateCommand getLastCommand() {
+		return this.lastCmd;
+	}
+
+}
