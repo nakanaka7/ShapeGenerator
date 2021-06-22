fb4bf4d7587b08f4fe5126b0dd3935160a62c944
nakanakagames <nakanakagames@gmail.com>
2021-06-22 11:38:44 +0900

    Change center BlockVector3D -> Vector3D

--- src/main/java/tokyo/nakanaka/selection/SphereRegionBuilder.java
+++ src/main/java/tokyo/nakanaka/selection/SphereRegionBuilder.java
@@ -1,28 +1,27 @@
 package tokyo.nakanaka.selection;
 
-import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.math.region3D.Region3D;
 import tokyo.nakanaka.math.region3D.Region3Ds;
 import tokyo.nakanaka.math.region3D.SphereRegion3D;
 
 public class SphereRegionBuilder {
-	private BlockVector3D center;
-	private Integer radius;
+	private Vector3D center;
+	private Double radius;
 	
-	public BlockVector3D getCenter() {
+	public Vector3D getCenter() {
 		return center;
 	}
 	
-	public void setCenter(BlockVector3D center) {
+	public void setCenter(Vector3D center) {
 		this.center = center;
 	}
 	
-	public Integer getRadius() {
+	public Double getRadius() {
 		return radius;
 	}
 	
-	public void setRadius(int radius) {
+	public void setRadius(double radius) {
 		if(radius < 0) {
 			throw new IllegalArgumentException();
 		}
@@ -36,8 +35,8 @@ public class SphereRegionBuilder {
 		if(this.center == null || this.radius == null) {
 			throw new IllegalStateException();
 		}
-		Region3D region = new SphereRegion3D(this.radius - 0.5);
-		return Region3Ds.shift(region, new Vector3D(center.getX(), center.getY(), center.getZ()));
+		Region3D region = new SphereRegion3D(this.radius);
+		return Region3Ds.shift(region, this.center);
 	}
 	
 }
