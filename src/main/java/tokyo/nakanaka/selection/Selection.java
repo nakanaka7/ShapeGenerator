c6391570b8818ad7e790fe9ca7baeafa0e0a54a1
nakanakagames <nakanakagames@gmail.com>
2021-06-22 11:31:35 +0900

    Change offset BlockVector3D -> Vector3D

--- src/main/java/tokyo/nakanaka/selection/Selection.java
+++ src/main/java/tokyo/nakanaka/selection/Selection.java
@@ -1,6 +1,5 @@
 package tokyo.nakanaka.selection;
 
-import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.LinearTransformation;
 import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.math.region3D.BoundRegion3D;
@@ -9,9 +8,9 @@ import tokyo.nakanaka.world.World;
 public class Selection {
 	private World world;
 	private BoundRegion3D region;
-	private BlockVector3D offset;
+	private Vector3D offset;
 	
-	public Selection(World world, BoundRegion3D region, BlockVector3D offset) {
+	public Selection(World world, BoundRegion3D region, Vector3D offset) {
 		this.world = world;
 		this.region = region;
 		this.offset = offset;
@@ -27,15 +26,12 @@ public class Selection {
 	
 	public Selection getShiftedSelection(int dx, int dy, int dz) {
 		BoundRegion3D region = this.region.getShiftedRegion(dx, dy, dz);
-		BlockVector3D offset = this.offset.add(new BlockVector3D(dx, dy, dz));
+		Vector3D offset = this.offset.add(new Vector3D(dx, dy, dz));
 		return new Selection(world, region, offset);
 	}
 	
 	public Selection getTransformedSelection(LinearTransformation trans) {
-		int x = this.offset.getX();
-		int y = this.offset.getY();
-		int z = this.offset.getZ();
-		BoundRegion3D region = this.region.getTransformedRegion(trans, new Vector3D(x, y, z));
+		BoundRegion3D region = this.region.getTransformedRegion(trans, this.offset);
 		return new Selection(this.world, region, this.offset);
 	}
 
