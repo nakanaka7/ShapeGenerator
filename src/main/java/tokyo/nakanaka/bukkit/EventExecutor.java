bd6909945e85b293764447ee7387ad5a4020258e
nakanakagames <nakanakagames@gmail.com>
2021-06-22 18:31:19 +0900

    Complete onLeftClickBlock()

--- src/main/java/tokyo/nakanaka/bukkit/EventExecutor.java
+++ src/main/java/tokyo/nakanaka/bukkit/EventExecutor.java
@@ -1,14 +1,49 @@
 package tokyo.nakanaka.bukkit;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import tokyo.nakanaka.ClickBlockEventHandler;
+import tokyo.nakanaka.Player;
+import tokyo.nakanaka.PlayerRepository;
+import tokyo.nakanaka.world.World;
 
 public class EventExecutor implements Listener{
+	private Server server;
+	private PlayerRepository playerRepo;
+	private ClickBlockEventHandler clickHandler = new ClickBlockEventHandler();
+	
+	public EventExecutor(Server server, PlayerRepository playerRepo) {
+		this.server = server;
+		this.playerRepo = playerRepo;
+	}
+
 	@EventHandler
 	public void onLeftClickBlock(BlockBreakEvent e) {
-	
+		org.bukkit.entity.Player bukkitPlayer = e.getPlayer();
+		ItemStack itemStack = bukkitPlayer.getInventory().getItemInMainHand();
+		Material type = itemStack.getType();
+		if(type == Material.BLAZE_ROD) {
+			e.setCancelled(true);
+		}
+		Player player = this.playerRepo.getHumanPlayer(bukkitPlayer.getUniqueId());
+		if(player == null) {
+			player = new Player(bukkitPlayer.getUniqueId(), bukkitPlayer.getName());
+			this.playerRepo.registerHumanPlayer(player);
+		}
+		Location loc = e.getBlock().getLocation();
+		org.bukkit.World bukkitWorld = loc.getWorld();
+		int x = loc.getBlockX();
+		int y = loc.getBlockY();
+		int z = loc.getBlockZ();
+		World world = new BukkitWorld(this.server, bukkitWorld);
+		this.clickHandler.onLeftClickBlock(player, world, x, y, z);
 	}
 	
 	@EventHandler
