c2a97ecfce162401302052acda25b2fdadce8cec
nakanakagames <nakanakagames@gmail.com>
2021-06-22 18:45:26 +0900

    add message/ change constructor

--- src/main/java/tokyo/nakanaka/ClickBlockEventHandler.java
+++ src/main/java/tokyo/nakanaka/ClickBlockEventHandler.java
@@ -1,13 +1,22 @@
 package tokyo.nakanaka;
 
+import tokyo.nakanaka.selection.SelectionMessenger;
 import tokyo.nakanaka.world.World;
 
 public class ClickBlockEventHandler {
+	public SelectionMessenger messenger;
+	
+	public ClickBlockEventHandler(SelectionMessenger messenger) {
+		this.messenger = messenger;
+	}
+
 	public void onLeftClickBlock(Player player, World world, int x, int y, int z) {
 		player.getSelectionBuilder().onLeftClickBlock(world, x, y, z);
+		this.messenger.sendMessage(player);
 	}
 	
 	public void onRightClickBlock(Player player, World world, int x, int y, int z) {
 		player.getSelectionBuilder().onRightClickBlock(world, x, y, z);
+		this.messenger.sendMessage(player);
 	}
 }
