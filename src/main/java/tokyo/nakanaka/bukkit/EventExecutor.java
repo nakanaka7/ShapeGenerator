bb790fe09ea7ccb08eac8e12ce1d376492ffdde6
nakanakagames <nakanakagames@gmail.com>
2021-06-22 16:39:42 +0900

    Create EventExecutor class

---
+++ src/main/java/tokyo/nakanaka/bukkit/EventExecutor.java
@@ -0,0 +1,19 @@
+package tokyo.nakanaka.bukkit;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class EventExecutor implements Listener{
+	@EventHandler
+	public void onLeftClickBlock(BlockBreakEvent e) {
+	
+	}
+	
+	@EventHandler
+	public void onRightClickBlock(PlayerInteractEvent e) {
+		
+	}
+	
+}
