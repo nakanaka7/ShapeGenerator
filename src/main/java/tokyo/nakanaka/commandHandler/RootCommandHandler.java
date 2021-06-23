48bc774817933c8f6548d2f6c47b1f6ad9dc3fc4
nakanakagames <nakanakagames@gmail.com>
2021-06-23 14:08:59 +0900

    rename

--- src/main/java/tokyo/nakanaka/commandHandler/RootCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/RootCommandHandler.java
@@ -10,20 +10,20 @@ import tokyo.nakanaka.commandLine.CommandLine;
 import tokyo.nakanaka.player.Player;
 
 public class RootCommandHandler {
-	private CommandHandlerRepository cmdLineRepo;
+	private CommandHandlerRepository cmdHandlerRepo;
 	
-	public RootCommandHandler(CommandHandlerRepository cmdLineRepo) {
-		this.cmdLineRepo = cmdLineRepo;
+	public RootCommandHandler(CommandHandlerRepository cmdHandlerRepo) {
+		this.cmdHandlerRepo = cmdHandlerRepo;
 	}
 	
 	public Set<CommandHandler> getAllRegisterdCommandHandler(){
-		return this.cmdLineRepo.getAll();
+		return this.cmdHandlerRepo.getAll();
 	}
 	
 	public void onCommand(CommandLine cmdLine) {
 		Player player = cmdLine.getPlayer();
 		String alias = cmdLine.getAlias();
-		CommandHandler cmdHandler = this.cmdLineRepo.findBy(alias);
+		CommandHandler cmdHandler = this.cmdHandlerRepo.findBy(alias);
 		if(cmdHandler != null) {
 			boolean success = cmdHandler.onCommand(player, cmdLine.getArgs());
 			if(!success) {
@@ -38,9 +38,9 @@ public class RootCommandHandler {
 		String alias = cmdLine.getAlias();
 		String[] args = cmdLine.getArgs();
 		if(args.length == 0) {
-			return new ArrayList<>(this.cmdLineRepo.getAliases());
+			return new ArrayList<>(this.cmdHandlerRepo.getAliases());
 		}
-		CommandHandler cmdHandler = this.cmdLineRepo.findBy(alias);
+		CommandHandler cmdHandler = this.cmdHandlerRepo.findBy(alias);
 		if(cmdHandler != null) {
 			return cmdHandler.onTabComplete(cmdLine.getPlayer(), cmdLine.getArgs());
 		}else {
