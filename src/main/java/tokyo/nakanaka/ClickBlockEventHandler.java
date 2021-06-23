f3d1e0a9dbe46f1426e8bb3f4b0e76b5d9a172f5
nakanakagames <nakanakagames@gmail.com>
2021-06-23 12:02:46 +0900

    fix

--- src/main/java/tokyo/nakanaka/ClickBlockEventHandler.java
+++ src/main/java/tokyo/nakanaka/ClickBlockEventHandler.java
@@ -1,6 +1,7 @@
 package tokyo.nakanaka;
 
 import tokyo.nakanaka.player.Player;
+import tokyo.nakanaka.selection.SelectionBuilder;
 import tokyo.nakanaka.selection.SelectionMessenger;
 import tokyo.nakanaka.world.World;
 
@@ -12,12 +13,13 @@ public class ClickBlockEventHandler {
 	}
 
 	public void onLeftClickBlock(Player player, World world, int x, int y, int z) {
-		player.getSelectionBuilder().onLeftClickBlock(world, x, y, z);
+		SelectionBuilder builder = player.getSelectionBuilder();
+		builder.onLeftClickBlock(x, y, z);
 		this.messenger.sendMessage(player);
 	}
 	
 	public void onRightClickBlock(Player player, World world, int x, int y, int z) {
-		player.getSelectionBuilder().onRightClickBlock(world, x, y, z);
+		player.getSelectionBuilder().onRightClickBlock(x, y, z);
 		this.messenger.sendMessage(player);
 	}
 }
