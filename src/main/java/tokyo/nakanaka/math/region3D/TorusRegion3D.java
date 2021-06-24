55ced4e6f74e46aec2ecbe0672c794f487be02fb
nakanakagames <nakanakagames@gmail.com>
2021-06-24 17:08:40 +0900

    Create TorusRegion3D class

---
+++ src/main/java/tokyo/nakanaka/math/region3D/TorusRegion3D.java
@@ -0,0 +1,11 @@
+package tokyo.nakanaka.math.region3D;
+
+public class TorusRegion3D implements Region3D{
+
+	@Override
+	public boolean contains(double x, double y, double z) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
