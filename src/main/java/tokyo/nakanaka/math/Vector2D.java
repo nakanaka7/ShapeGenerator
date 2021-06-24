545e700d2ed955ef3f13e7deb13a47c749d2e966
nakanakagames <nakanakagames@gmail.com>
2021-06-24 17:13:01 +0900

    Create Vector2D

---
+++ src/main/java/tokyo/nakanaka/math/Vector2D.java
@@ -0,0 +1,20 @@
+package tokyo.nakanaka.math;
+
+public class Vector2D {
+	private double x;
+	private double y;
+	
+	public Vector2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+	
+}
