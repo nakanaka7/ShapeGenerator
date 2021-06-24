6a9366bf5b76c4553fb5d2982d3a15462dd892a7
nakanakagames <nakanakagames@gmail.com>
2021-06-24 10:45:29 +0900

    add javadoc comment

--- src/main/java/tokyo/nakanaka/selection/SelectionBuilder.java
+++ src/main/java/tokyo/nakanaka/selection/SelectionBuilder.java
@@ -8,6 +8,13 @@ public interface SelectionBuilder {
 	World getWorld();
 	boolean onLeftClickBlock(int x, int y, int z);
 	boolean onRightClickBlock(int x, int y, int z);
+	/**
+	 * @param posX player position x
+	 * @param posY player position y
+	 * @param posZ player position z
+	 * @param args
+	 * @return
+	 */
 	boolean onCommand(int posX, int posY, int posZ, String[] args);
 	List<String> onTabComplete(String[] args);
 	List<String> getStateLines();
