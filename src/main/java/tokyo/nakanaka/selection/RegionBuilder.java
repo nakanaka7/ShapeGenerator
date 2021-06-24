58bb5d09303c49c69282086753ab44e5c670442b
nakanakagames <nakanakagames@gmail.com>
2021-06-24 21:03:31 +0900

    Create RegionBuilder interface

--- src/main/java/tokyo/nakanaka/selection/RegionBuilder.java
+++ src/main/java/tokyo/nakanaka/selection/RegionBuilder.java
@@ -3,13 +3,15 @@ package tokyo.nakanaka.selection;
 import java.util.List;
 
 import tokyo.nakanaka.logger.Logger;
+import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.region3D.BoundRegion3D;
 
 public interface RegionBuilder {
 	boolean onLeftClickBlock(Logger logger, int x, int y, int z);
 	boolean onRightClickBlock(Logger logger, int x, int y, int z);
-	boolean onCommand(Logger logger, int posX, int posY, int posZ, String[] args);
-	List<String> onTabComplete(String[] args);
+	boolean onCommand(Logger logger, BlockVector3D pos, String label, String[] args);
+	List<String> getLabelList();
+	List<String> onTabComplete(String label, String[] args);
 	List<String> getStateLines();
 	BoundRegion3D build();
 }
