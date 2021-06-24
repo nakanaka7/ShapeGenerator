6904782ad9645df589b8708340fee98e5395b461
nakanakagames <nakanakagames@gmail.com>
2021-06-24 17:19:53 +0900

    Complete TorusRegion3D class

--- src/main/java/tokyo/nakanaka/math/region3D/TorusRegion3D.java
+++ src/main/java/tokyo/nakanaka/math/region3D/TorusRegion3D.java
@@ -1,11 +1,29 @@
 package tokyo.nakanaka.math.region3D;
 
+import tokyo.nakanaka.math.PolarVector2D;
+import tokyo.nakanaka.math.Vector3D;
+
 public class TorusRegion3D implements Region3D{
+	private double radiusMain;
+	private double radiusSub;
+	
+	/**
+	 * @throws IllegalArgumentException
+	 */
+	public TorusRegion3D(double radiusMain, double radiusSub) {
+		if(radiusMain < 0 || radiusSub < 0) {
+			throw new IllegalArgumentException();
+		}
+		this.radiusMain = radiusMain;
+		this.radiusSub = radiusSub;
+	}
 
 	@Override
 	public boolean contains(double x, double y, double z) {
-		// TODO Auto-generated method stub
-		return false;
+		PolarVector2D polar = PolarVector2D.valueOf(x, y);
+		double angle = polar.getArgument();
+		Vector3D q = new Vector3D(this.radiusMain * Math.cos(angle), this.radiusMain * Math.sin(angle), 0);
+		return new Vector3D(x, y, z).distance(q) <= this.radiusSub;
 	}
-
+	
 }
