6955e94f46ea697e18ce1ab9b7a7169349b79706
nakanakagames <nakanakagames@gmail.com>
2021-06-25 19:30:42 +0900

    rename

--- src/main/java/tokyo/nakanaka/selection/SphereSelectionBuilderOld.java
+++ src/main/java/tokyo/nakanaka/selection/SphereSelectionBuilderOld.java
@@ -3,9 +3,9 @@ package tokyo.nakanaka.selection;
 import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.world.World;
 
-public class SphereSelectionBuilder extends AbstractSelectionBuilderOld{
+public class SphereSelectionBuilderOld extends AbstractSelectionBuilderOld{
 
-	public SphereSelectionBuilder(World world) {
+	public SphereSelectionBuilderOld(World world) {
 		super(world, new SphereRegionBuilder());
 	}
 
