06242aed1433c83c3ab1ca3775de24073a9808ad
nakanakagames <nakanakagames@gmail.com>
2021-06-25 16:14:55 +0900

    Create new SphereSelectionBuilder class

---
+++ src/main/java/tokyo/nakanaka/selection/SphereSelectionBuilder.java
@@ -0,0 +1,22 @@
+package tokyo.nakanaka.selection;
+
+import tokyo.nakanaka.math.Vector3D;
+import tokyo.nakanaka.world.World;
+
+public class SphereSelectionBuilder extends AbstractSelectionBuilder{
+
+	public SphereSelectionBuilder(World world) {
+		super(world, new SphereRegionBuilder());
+	}
+
+	@Override
+	String getDefaultOffsetName() {
+		return SphereRegionBuilder.getStringCenter();
+	}
+
+	@Override
+	Vector3D getDefaultOffset() {
+		return ((SphereRegionBuilder)builder).getCenter();
+	}
+
+}
