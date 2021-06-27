74eb0489417307015aa6d9e421d4155399256f92
nakanakagames <nakanakagames@gmail.com>
2021-06-27 19:11:15 +0900

    change arg

--- src/main/java/tokyo/nakanaka/selection/ClickBlockHandler.java
+++ src/main/java/tokyo/nakanaka/selection/ClickBlockHandler.java
@@ -3,7 +3,7 @@ package tokyo.nakanaka.selection;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 
-public interface ClickBlockHandler<B extends RegionBuilder> {
-	void onLeftClickBlock(B builder, Logger logger, BlockVector3D blockPos);
-	void onRightClickBlock(B builder, Logger logger, BlockVector3D blockPos);
+public interface ClickBlockHandler {
+	void onLeftClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos);
+	void onRightClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos);
 }
