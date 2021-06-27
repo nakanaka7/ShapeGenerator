89b8d55f7727312b58df58d5b63f75f0ef1bb17e
nakanakagames <nakanakagames@gmail.com>
2021-06-27 21:50:18 +0900

    add getCommandHelp()

--- src/main/java/tokyo/nakanaka/selection/SelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/SelCommandHandler.java
@@ -2,10 +2,12 @@ package tokyo.nakanaka.selection;
 
 import java.util.List;
 
+import tokyo.nakanaka.commadHelp.CommandHelp;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 
 public interface SelCommandHandler {
+	CommandHelp getCommandHelp();
 	void onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String[] args);
 	List<String> onTabComplete(String[] args);
 }
