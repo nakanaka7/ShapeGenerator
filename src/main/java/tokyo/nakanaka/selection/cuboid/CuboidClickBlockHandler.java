ce9be2f1924255e7705803fd49d37660788370fa
nakanakagames <nakanakagames@gmail.com>
2021-06-27 19:14:28 +0900

    change arg

--- src/main/java/tokyo/nakanaka/selection/cuboid/CuboidClickBlockHandler.java
+++ src/main/java/tokyo/nakanaka/selection/cuboid/CuboidClickBlockHandler.java
@@ -4,13 +4,18 @@ import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.selection.ClickBlockHandler;
+import tokyo.nakanaka.selection.RegionBuilder;
 
-public class CuboidClickBlockHandler implements ClickBlockHandler<CuboidRegionBuilder>{
-	public void onLeftClickBlock(CuboidRegionBuilder cuboidBuilder, Logger logger, BlockVector3D blockPos) {
+public class CuboidClickBlockHandler implements ClickBlockHandler {
+	
+	public void onLeftClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos) {
+		CuboidRegionBuilder cuboidBuilder = (CuboidRegionBuilder)builder;
 		cuboidBuilder.setPos1(new Vector3D(blockPos.getX(), blockPos.getY(), blockPos.getZ()));
 	}
 
-	public void onRightClickBlock(CuboidRegionBuilder cuboidBuilder, Logger logger, BlockVector3D blockPos) {
+	public void onRightClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos) {
+		CuboidRegionBuilder cuboidBuilder = (CuboidRegionBuilder)builder;
 		cuboidBuilder.setPos2(new Vector3D(blockPos.getX(), blockPos.getY(), blockPos.getZ()));
 	}
+	
 }
