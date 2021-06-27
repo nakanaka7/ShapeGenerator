76c379b124a932e7dc4f325ac427f98de94d821e
nakanakagames <nakanakagames@gmail.com>
2021-06-27 21:13:20 +0900

    remove getCommandHelp()

--- src/main/java/tokyo/nakanaka/selection/SelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/SelCommandHandler.java
@@ -2,11 +2,9 @@ package tokyo.nakanaka.selection;
 
 import java.util.List;
 
-import tokyo.nakanaka.commadHelp.CommandHelp;
 import tokyo.nakanaka.math.BlockVector3D;
 
 public interface SelCommandHandler {
-	CommandHelp getCommandHelp();
 	void onCommand(BlockVector3D playerPos, String[] args);
 	List<String> onTabComplete(String[] args);
 }
