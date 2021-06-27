04e98290c4e9191d62543b72c0c6cacdd605b093
nakanakagames <nakanakagames@gmail.com>
2021-06-27 21:33:20 +0900

    change arg

--- src/main/java/tokyo/nakanaka/selection/SelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/SelCommandHandler.java
@@ -2,9 +2,10 @@ package tokyo.nakanaka.selection;
 
 import java.util.List;
 
+import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 
 public interface SelCommandHandler {
-	void onCommand(RegionBuilder builder, BlockVector3D playerPos, String[] args);
+	void onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String[] args);
 	List<String> onTabComplete(String[] args);
 }
