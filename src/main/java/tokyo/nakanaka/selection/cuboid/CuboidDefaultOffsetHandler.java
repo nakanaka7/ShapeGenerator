9b95dba65378e00751f9a4b1696019c77f695277
nakanakagames <nakanakagames@gmail.com>
2021-06-28 11:37:10 +0900

    Create CuboidDefaultOffsetHandler

---
+++ src/main/java/tokyo/nakanaka/selection/cuboid/CuboidDefaultOffsetHandler.java
@@ -0,0 +1,20 @@
+package tokyo.nakanaka.selection.cuboid;
+
+import tokyo.nakanaka.math.Vector3D;
+import tokyo.nakanaka.selection.DefaultOffsetHandler;
+import tokyo.nakanaka.selection.RegionBuilder;
+
+public class CuboidDefaultOffsetHandler implements DefaultOffsetHandler{
+
+	@Override
+	public String getDefaultOffsetLabel() {
+		return "pos1";
+	}
+
+	@Override
+	public Vector3D getDefaultOffset(RegionBuilder builder) {
+		CuboidRegionBuilder cuboidBuilder = (CuboidRegionBuilder)builder;
+		return cuboidBuilder.getPos1();
+	}
+
+}
