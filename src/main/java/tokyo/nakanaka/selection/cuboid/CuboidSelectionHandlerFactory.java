5fe8b5386e70ce1897fb1f1bcad9a26688bc42ac
nakanakagames <nakanakagames@gmail.com>
2021-06-28 11:50:22 +0900

    Create CuboidSelectionHandlerFactory

---
+++ src/main/java/tokyo/nakanaka/selection/cuboid/CuboidSelectionHandlerFactory.java
@@ -0,0 +1,34 @@
+package tokyo.nakanaka.selection.cuboid;
+
+import java.util.Arrays;
+import java.util.List;
+
+import tokyo.nakanaka.selection.ClickBlockHandler;
+import tokyo.nakanaka.selection.DefaultOffsetHandler;
+import tokyo.nakanaka.selection.RegionStateMessageHandler;
+import tokyo.nakanaka.selection.SelSubCommandHandler;
+import tokyo.nakanaka.selection.SelectionHandlerFactory;
+
+public class CuboidSelectionHandlerFactory implements SelectionHandlerFactory{
+
+	@Override
+	public ClickBlockHandler getClickBlockHandler() {
+		return new CuboidClickBlockHandler();
+	}
+
+	@Override
+	public List<SelSubCommandHandler> getSelSubCommandHandlers() {
+		return Arrays.asList(new Pos1CommandHandler(), new Pos2CommandHandler());
+	}
+
+	@Override
+	public DefaultOffsetHandler getDefaultOffsetHandler() {
+		return new CuboidDefaultOffsetHandler();
+	}
+
+	@Override
+	public RegionStateMessageHandler getRegionStateMessageHandler() {
+		return new CuboidStateMessageHandler();
+	}
+
+}
