9fdbc25d1b390f4765aad1cd03447f4d15291b40
nakanakagames <nakanakagames@gmail.com>
2021-06-28 14:10:25 +0900

    remove help

--- src/main/java/tokyo/nakanaka/selection/cuboid/Pos2CommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/cuboid/Pos2CommandHandler.java
@@ -2,9 +2,6 @@ package tokyo.nakanaka.selection.cuboid;
 
 import java.util.List;
 
-import tokyo.nakanaka.commadHelp.CommandHelp;
-import tokyo.nakanaka.commadHelp.Parameter;
-import tokyo.nakanaka.commadHelp.Parameter.Type;
 import tokyo.nakanaka.commandArgument.PositionArgument;
 import tokyo.nakanaka.logger.LogConstant;
 import tokyo.nakanaka.logger.Logger;
@@ -16,16 +13,6 @@ import tokyo.nakanaka.selection.SelSubCommandHandler;
 public class Pos2CommandHandler implements SelSubCommandHandler{
 	private PositionArgument pos2Arg = new PositionArgument();
 
-	@Override
-	public CommandHelp getCommandHelp() {
-		return new CommandHelp.Builder("pos2")
-				.description("Set pos2, which is a corner of cuboid")
-				.addParameter(new Parameter(Type.OPTIONAL, "x"), "x coordinate, if given, type y and z together")
-				.addParameter(new Parameter(Type.OPTIONAL, "y"), "y coordinate, if given, type x and z together")
-				.addParameter(new Parameter(Type.OPTIONAL, "z"), "z coordinate, if given, type x and y together")
-				.build();
-	}
-	
 	@Override
 	public void onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String[] args) {
 		Vector3D pos2;
