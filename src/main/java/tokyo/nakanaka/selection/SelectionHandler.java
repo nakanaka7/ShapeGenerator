a43fd77f7e7b8cef150e9f723e8021ff85c0e9d8
nakanakagames <nakanakagames@gmail.com>
2021-06-28 22:27:53 +0900

    Use ValuePool

--- src/main/java/tokyo/nakanaka/selection/SelectionHandler.java
+++ src/main/java/tokyo/nakanaka/selection/SelectionHandler.java
@@ -3,19 +3,18 @@ package tokyo.nakanaka.selection;
 import java.util.List;
 
 import tokyo.nakanaka.Pair;
+import tokyo.nakanaka.ValuePool;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
-import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.math.region3D.BoundRegion3D;
 
 public interface SelectionHandler {
-	void onLeftClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos);
-	void onRightClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos);
+	void onLeftClickBlock(ValuePool regionPool, Logger logger, BlockVector3D blockPos);
+	void onRightClickBlock(ValuePool regionPool, Logger logger, BlockVector3D blockPos);
 	String getDefaultOffsetLabel();
-	Vector3D getDefaultOffset(RegionBuilder builder);
 	List<String> getCommandLabelList();
-	boolean onCommand(RegionBuilder builder, Logger logger, String label, BlockVector3D playerPos, String[] args);
+	boolean onCommand(ValuePool regionPool, Logger logger, String label, BlockVector3D playerPos, String[] args);
 	List<String> onTabComplete(String label, String[] args);
-	List<Pair<String, String>> onRegionMessage();
-	BoundRegion3D build(RegionBuilder builder);
+	List<Pair<String, String>> onRegionMessage(ValuePool regionPool);
+	BoundRegion3D build(ValuePool regionPool);
 }
