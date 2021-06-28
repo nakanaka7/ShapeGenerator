e93365973fe6e605607f9b5a94f824b9aa97db34
nakanakagames <nakanakagames@gmail.com>
2021-06-28 18:17:04 +0900

    recreate selectionhandler

--- src/main/java/tokyo/nakanaka/selection/SelectionHandler.java
+++ src/main/java/tokyo/nakanaka/selection/SelectionHandler.java
@@ -6,14 +6,16 @@ import tokyo.nakanaka.Pair;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
+import tokyo.nakanaka.math.region3D.BoundRegion3D;
 
 public interface SelectionHandler {
 	void onLeftClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos);
 	void onRightClickBlock(RegionBuilder builder, Logger logger, BlockVector3D blockPos);
-	List<String> getCommandLabels();
-	boolean onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String label, String[] args);
-	List<String> onTabComplete(String label, String[] args);	
 	String getDefaultOffsetLabel();
 	Vector3D getDefaultOffset(RegionBuilder builder);
-	List<Pair<String, String>> onMessage(RegionBuilder builder);
+	List<String> getCommandLabelList();
+	boolean onCommand(RegionBuilder builder, Logger logger, String label, BlockVector3D playerPos, String[] args);
+	List<String> onTabComplete(String label, String[] args);
+	List<Pair<String, String>> onRegionMessage();
+	BoundRegion3D build(RegionBuilder builder);
 }
