f50637736f70dabbbc304eb2a7ea6eb3a27c42fa
nakanakagames <nakanakagames@gmail.com>
2021-06-28 14:09:11 +0900

    remove gethelp

--- src/main/java/tokyo/nakanaka/selection/SelSubCommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/SelSubCommandHandler.java
@@ -2,12 +2,10 @@ package tokyo.nakanaka.selection;
 
 import java.util.List;
 
-import tokyo.nakanaka.commadHelp.CommandHelp;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 
 public interface SelSubCommandHandler {
-	CommandHelp getCommandHelp();
 	void onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String[] args);
 	List<String> onTabComplete(String[] args);
 }
