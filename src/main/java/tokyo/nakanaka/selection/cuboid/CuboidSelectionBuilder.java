9220376d7f5a3947a2e928d41adcc26e0d16b533
nakanakagames <nakanakagames@gmail.com>
2021-06-29 14:48:26 +0900

    remove "factory"

--- src/main/java/tokyo/nakanaka/selection/cuboid/CuboidSelectionBuilder.java
+++ src/main/java/tokyo/nakanaka/selection/cuboid/CuboidSelectionBuilder.java
@@ -1,6 +1,7 @@
 package tokyo.nakanaka.selection.cuboid;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,51 +11,53 @@ import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.math.region3D.BoundRegion3D;
 import tokyo.nakanaka.selection.AbstractSelectionBuilder;
+import tokyo.nakanaka.selection.ClickBlockHandler;
+import tokyo.nakanaka.selection.DefaultOffsetHandler;
 import tokyo.nakanaka.selection.RegionBuilder;
+import tokyo.nakanaka.selection.RegionStateMessageHandler;
 import tokyo.nakanaka.selection.SelSubCommandHandler;
-import tokyo.nakanaka.selection.SelectionHandlerFactory;
 import tokyo.nakanaka.world.World;
 
 public class CuboidSelectionBuilder extends AbstractSelectionBuilder{
-	private RegionBuilder regionBuilder;
-	private SelectionHandlerFactory handlerFactory;
+	private RegionBuilder regionBuilder = new CuboidRegionBuilder();
+	private ClickBlockHandler clickHandler = new CuboidClickBlockHandler();
+	private Map<String, SelSubCommandHandler> cmdMap = new HashMap<>();
+	private DefaultOffsetHandler offsetHandler = new CuboidDefaultOffsetHandler();
+	private RegionStateMessageHandler msgHandler = new CuboidStateMessageHandler();
 	
 	public CuboidSelectionBuilder(World world) {
 		super(world);
-		this.regionBuilder = new CuboidRegionBuilder();
-		this.handlerFactory = new CuboidSelectionHandlerFactory();
+		this.cmdMap.put("pos1", new Pos1CommandHandler());
+		this.cmdMap.put("pos2", new Pos2CommandHandler());
 	}
 	
 	@Override
 	public void onLeftClickBlock(Logger logger, BlockVector3D blockPos) {
-		this.handlerFactory.getClickBlockHandler().onLeftClickBlock(this.regionBuilder, logger, blockPos);
+		this.clickHandler.onLeftClickBlock(this.regionBuilder, logger, blockPos);
 	}
 
 	@Override
 	public void onRightClickBlock(Logger logger, BlockVector3D blockPos) {
-		this.handlerFactory.getClickBlockHandler().onRightClickBlock(regionBuilder, logger, blockPos);
+		this.clickHandler.onRightClickBlock(this.regionBuilder, logger, blockPos);
 	}
 	
 	@Override
 	public boolean onRegionCommand(Logger logger, BlockVector3D playerPos, String label, String[] args) {
-		SelSubCommandHandler handler = this.handlerFactory.getSelSubCommandHandlers().get(label);
+		SelSubCommandHandler handler = this.cmdMap.get(label);
 		if(handler == null) {
 			return false;
 		}
-		handler.onCommand(this.regionBuilder, logger, playerPos, args);
-		return true;
+		return handler.onCommand(this.regionBuilder, logger, playerPos, args);
 	}
 
 	@Override
 	public List<String> getRegionCommandLabelList() {
-		Map<String, SelSubCommandHandler> cmdHandlerMap = this.handlerFactory.getSelSubCommandHandlers();
-		return new ArrayList<>(cmdHandlerMap.keySet());
+		return new ArrayList<>(this.cmdMap.keySet());
 	}
 	
 	@Override
 	public List<String> onRegionCommandTabComplete(String label, String[] args) {
-		Map<String, SelSubCommandHandler> cmdHandlerMap = this.handlerFactory.getSelSubCommandHandlers();
-		SelSubCommandHandler handler = cmdHandlerMap.get(label);
+		SelSubCommandHandler handler = this.cmdMap.get(label);
 		if(handler == null) {
 			return new ArrayList<>();
 		}
@@ -64,8 +67,7 @@ public class CuboidSelectionBuilder extends AbstractSelectionBuilder{
 	@Override
 	public List<String> getRegionStateLines() {
 		List<Pair<String, String>> pairList = 
-				this.handlerFactory
-				.getRegionStateMessageHandler()
+				this.msgHandler
 				.onMessage(this.regionBuilder);
 		List<String> list = new ArrayList<>();
 		for(Pair<String, String> pair : pairList) {
@@ -76,15 +78,13 @@ public class CuboidSelectionBuilder extends AbstractSelectionBuilder{
 	
 	@Override
 	public String getDefaultOffsetName() {
-		return this.handlerFactory
-				.getDefaultOffsetHandler()
+		return this.offsetHandler
 				.getDefaultOffsetLabel();
 	}
 
 	@Override
 	public Vector3D getDefaultOffset() {
-		return this.handlerFactory
-				.getDefaultOffsetHandler()
+		return this.offsetHandler
 				.getDefaultOffset(this.regionBuilder);
 	}
 	
