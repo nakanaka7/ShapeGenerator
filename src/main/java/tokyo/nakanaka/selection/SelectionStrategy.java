14998bd6d03b665dfe74228d2b02c393842f0995
nakanakagames <nakanakagames@gmail.com>
2021-06-29 21:13:56 +0900

    change a bit

--- src/main/java/tokyo/nakanaka/selection/SelectionStrategy.java
+++ src/main/java/tokyo/nakanaka/selection/SelectionStrategy.java
@@ -1,6 +1,6 @@
 package tokyo.nakanaka.selection;
 
-import java.util.Map;
+import java.util.List;
 
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
@@ -11,6 +11,6 @@ public interface SelectionStrategy {
 	void onLeftClickBlock(RegionBuildingData data, Logger logger, BlockVector3D blockPos);
 	void onRightClickBlock(RegionBuildingData data, Logger logger, BlockVector3D blockPos);
 	String getDefaultOffsetLabel();
-	Map<String, SelSubCommandHandler> getSelSubCommandHandlerMap();
+	List<SelSubCommandHandler> getSelSubCommandHandlers();
 	BoundRegion3D buildBoundRegion3D(RegionBuildingData data);
 }
