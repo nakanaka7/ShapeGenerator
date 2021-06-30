586437fc0f70f610f5c26b88280b8e819f3592a3
nakanakagames <nakanakagames@gmail.com>
2021-06-30 18:39:10 +0900

    remove SelectionManager using

--- src/main/java/tokyo/nakanaka/commandHandler/SelCommandHandlerNew.java
+++ src/main/java/tokyo/nakanaka/commandHandler/SelCommandHandlerNew.java
@@ -8,10 +8,10 @@ import java.util.Map;
 import tokyo.nakanaka.commadHelp.CommandHelp;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.player.Player;
+import tokyo.nakanaka.selection.RegionBuildingData;
 import tokyo.nakanaka.selection.SelSubCommandHandler;
-import tokyo.nakanaka.selection.SelectionBuilder;
-import tokyo.nakanaka.selection.SelectionManager;
-import tokyo.nakanaka.selection.SelectionMessenger;
+import tokyo.nakanaka.selection.SelectionBuildingData;
+import tokyo.nakanaka.selection.SelectionMessengerNew;
 import tokyo.nakanaka.selection.SelectionShape;
 import tokyo.nakanaka.selection.SelectionStrategy;
 import tokyo.nakanaka.world.World;
@@ -22,8 +22,6 @@ public class SelCommandHandlerNew implements SubCommandHandler{
 	private CommandHelp help = new CommandHelp.Builder("sel")
 			.description("Specify a selection/ See each shape help")
 			.build();
-	@Deprecated
-	private SelectionManager selManager;
 	
 	public SelCommandHandlerNew(Map<SelectionShape, SelectionStrategy> strategyMap) {
 		this.strategyMap = strategyMap;
@@ -44,27 +42,40 @@ public class SelCommandHandlerNew implements SubCommandHandler{
 		System.arraycopy(args, 1, shiftArgs, 0, args.length - 1);
 		World world = player.getWorld();
 		BlockVector3D playerPos = new BlockVector3D(player.getX(), player.getY(), player.getZ());
-		SelectionBuilder builder = player.getSelectionBuilder();
+		SelectionBuildingData selData = player.getSelectionBuildingData();
+		SelectionShape shape = player.getSelectionShape();
+		SelectionStrategy strategy = this.strategyMap.get(shape);
+		SelectionMessengerNew selMessenger = new SelectionMessengerNew();
 		if(label.equals(RESET)){
-			SelectionShape shape = this.selManager.getShape(builder);
-			SelectionBuilder newBuilder = this.selManager.newInstance(shape, world);
-			player.setSelectionBuilder(newBuilder);
-			new SelectionMessenger(this.selManager).sendMessage(player);
+			SelectionBuildingData newSelData = new SelectionBuildingData(world, strategy.newRegionBuildingData());
+			player.setSelectionBuildingData(newSelData);
+			selMessenger.sendMessage(player.getLogger(), newSelData);
 			return true;
 		}
-		if(!world.equals(builder.getWorld())) {
-			SelectionShape shape = this.selManager.getShape(builder);
-			SelectionBuilder newBuilder = this.selManager.newInstance(shape, world);
-			player.setSelectionBuilder(newBuilder);
+		if(!world.equals(selData.getWorld())) {
+			SelectionBuildingData newSelData = new SelectionBuildingData(world, strategy.newRegionBuildingData());
+			player.setSelectionBuildingData(newSelData);	
 		}
-		builder.onCommand(player.getLogger(), playerPos, label, shiftArgs);
-		new SelectionMessenger(this.selManager).sendMessage(player);
+		RegionBuildingData regionData = selData.getRegionData();
+		List<SelSubCommandHandler> cmdHandlerList = strategy.getSelSubCommandHandlers();
+		for(SelSubCommandHandler cmdHandler : cmdHandlerList) {
+			if(cmdHandler.getLabel().equals(label)) {
+				boolean success = cmdHandler.onCommand(regionData, player.getLogger(), playerPos, shiftArgs);
+				if(!success) {
+					//help
+					return true;
+				}
+				selMessenger.sendMessage(player.getLogger(), selData);
+				return true;
+			}
+		}
+		//help
 		return true;
 	}
 
 	@Override
 	public List<String> onTabComplete(Player player, String[] args) {
-		SelectionShape shape = player.getSelectionBuildingData().getSelectionShape();
+		SelectionShape shape = player.getSelectionShape();
 		List<SelSubCommandHandler> cmdHandlerList = this.strategyMap.get(shape).getSelSubCommandHandlers();
 		if(args.length == 1) {
 			List<String> list = new ArrayList<>();
