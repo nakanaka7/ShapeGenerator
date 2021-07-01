34dc1aa3dfbb0b8d1ccf17d7d706cf571a07a5e0
nakanakagames <nakanakagames@gmail.com>
2021-07-01 23:16:51 +0900

    remove RegionBuilder arg

--- src/main/java/tokyo/nakanaka/selection/cuboid/Pos1CommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/cuboid/Pos1CommandHandler.java
@@ -7,27 +7,12 @@ import tokyo.nakanaka.logger.LogConstant;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
-import tokyo.nakanaka.selection.RegionBuilder;
 import tokyo.nakanaka.selection.RegionBuildingData;
 import tokyo.nakanaka.selection.SelSubCommandHandler;
 
 public class Pos1CommandHandler implements SelSubCommandHandler{
 	private PositionArgument pos1Arg = new PositionArgument();
 	private LengthCalculator lengthCalc = new LengthCalculator();
-
-	@Deprecated
-	@Override
-	public boolean onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String[] args) {
-		Vector3D pos1;
-		try {
-			pos1 = this.pos1Arg.onParse(playerPos, args);
-		}catch(IllegalArgumentException e) {
-			logger.print(LogConstant.HEAD_ERROR + "Can not parse coodinate");
-			return true;
-		}
-		((CuboidRegionBuilder)builder).setPos1(pos1);
-		return true;
-	}
 
 	@Override
 	public List<String> onTabComplete(String[] args) {
--- src/main/java/tokyo/nakanaka/selection/cuboid/Pos2CommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/cuboid/Pos2CommandHandler.java
@@ -7,27 +7,12 @@ import tokyo.nakanaka.logger.LogConstant;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
-import tokyo.nakanaka.selection.RegionBuilder;
 import tokyo.nakanaka.selection.RegionBuildingData;
 import tokyo.nakanaka.selection.SelSubCommandHandler;
 
 public class Pos2CommandHandler implements SelSubCommandHandler{
 	private PositionArgument pos2Arg = new PositionArgument();
 	private LengthCalculator lengthCalc = new LengthCalculator();
-
-	@Deprecated
-	@Override
-	public boolean onCommand(RegionBuilder builder, Logger logger, BlockVector3D playerPos, String[] args) {
-		Vector3D pos2;
-		try {
-			pos2 = this.pos2Arg.onParse(playerPos, args);
-		}catch(IllegalArgumentException e) {
-			logger.print(LogConstant.HEAD_ERROR + "Can not parse coodinate");
-			return true;
-		}
-		((CuboidRegionBuilder)builder).setPos2(pos2);
-		return true;
-	}
 
 	@Override
 	public List<String> onTabComplete(String[] args) {
