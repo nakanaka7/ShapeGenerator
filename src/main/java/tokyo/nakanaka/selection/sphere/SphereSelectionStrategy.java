7aa87806e0af9d85f7eed9a0586da5ed6a3b0719
nakanakagames <nakanakagames@gmail.com>
2021-07-02 08:14:08 +0900

    complete

--- src/main/java/tokyo/nakanaka/selection/sphere/SphereSelectionStrategy.java
+++ src/main/java/tokyo/nakanaka/selection/sphere/SphereSelectionStrategy.java
@@ -1,50 +1,75 @@
 package tokyo.nakanaka.selection.sphere;
 
+import static tokyo.nakanaka.logger.LogConstant.HEAD_ERROR;
+
+import java.util.Arrays;
 import java.util.List;
 
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
+import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.math.region3D.BoundRegion3D;
+import tokyo.nakanaka.math.region3D.Region3D;
+import tokyo.nakanaka.math.region3D.Region3Ds;
+import tokyo.nakanaka.math.region3D.SphereRegion3D;
 import tokyo.nakanaka.selection.RegionBuildingData;
 import tokyo.nakanaka.selection.SelSubCommandHandler;
 import tokyo.nakanaka.selection.SelectionStrategy;
+import tokyo.nakanaka.selection.RegionBuildingData.DataType;
 
 public class SphereSelectionStrategy implements SelectionStrategy{
 
 	@Override
 	public RegionBuildingData newRegionBuildingData() {
-		// TODO Auto-generated method stub
-		return null;
+		return new RegionBuildingData.Builder()
+				.addDataTag("center", DataType.VECTOR3D)
+				.addDataTag("radius", DataType.DOUBLE)
+				.build();
 	}
 
 	@Override
 	public void onLeftClickBlock(RegionBuildingData data, Logger logger, BlockVector3D blockPos) {
-		// TODO Auto-generated method stub
-		
+		Vector3D center = blockPos.toVector3D();
+		data.putVector3D("center", center);
 	}
 
 	@Override
 	public void onRightClickBlock(RegionBuildingData data, Logger logger, BlockVector3D blockPos) {
-		// TODO Auto-generated method stub
-		
+		Vector3D center = data.getVector3D("center");
+		if(center == null) {
+			logger.print(HEAD_ERROR + "Set center first");
+		}
+		Vector3D pos = blockPos.toVector3D();
+		double radius = Math.floor(pos.negate(center).getAbsolute()) + 0.5;
+		data.putDouble("radius", radius);
 	}
 
 	@Override
 	public String getDefaultOffsetLabel() {
-		// TODO Auto-generated method stub
-		return null;
+		return "center";
 	}
 
 	@Override
 	public List<SelSubCommandHandler> getSelSubCommandHandlers() {
-		// TODO Auto-generated method stub
-		return null;
+		return Arrays.asList(new CenterCommandHandler(), new RadiusCommandHandler());
 	}
 
 	@Override
 	public BoundRegion3D buildBoundRegion3D(RegionBuildingData data) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector3D center = data.getVector3D("center");
+		Double radius = data.getDouble("radius");
+		if(center == null || radius == null) {
+			throw new IllegalStateException();
+		}
+		Region3D region = new SphereRegion3D(radius);
+		region = Region3Ds.shift(region, center);
+		double ubx = center.getX() + radius;
+		double uby = center.getY() + radius;
+		double ubz = center.getZ() + radius;
+		double lbx = center.getX() - radius;
+		double lby = center.getY() - radius;
+		double lbz = center.getZ() - radius;
+		return new BoundRegion3D(region, ubx, uby, ubz, lbx, lby, lbz);
 	}
 
 }
