3b49060aab124fd21ac3eac00dc4c90b0f5c72a7
nakanakagames <nakanakagames@gmail.com>
2021-07-02 22:33:50 +0900

    delete deprecated

--- src/main/java/tokyo/nakanaka/selection/RegionBuilder.java
+++
@@ -1,7 +0,0 @@
-package tokyo.nakanaka.selection;
-
-import tokyo.nakanaka.math.region3D.BoundRegion3D;
-@Deprecated
-public interface RegionBuilder {
-	BoundRegion3D buildRegion();
-}
--- src/main/java/tokyo/nakanaka/selection/RegionStateMessageHandler.java
+++
@@ -1,9 +0,0 @@
-package tokyo.nakanaka.selection;
-
-import java.util.List;
-
-import tokyo.nakanaka.Pair;
-@Deprecated
-public interface RegionStateMessageHandler {
-	List<Pair<String, String>> onMessage(RegionBuilder builder);
-}
