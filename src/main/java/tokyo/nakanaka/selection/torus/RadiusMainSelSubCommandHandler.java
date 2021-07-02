01225a4202c996b932ab82da1f1657a86965a2a5
nakanakagames <nakanakagames@gmail.com>
2021-07-02 15:02:23 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/selection/torus/RadiusMainSelSubCommandHandler.java
@@ -0,0 +1,30 @@
+package tokyo.nakanaka.selection.torus;
+
+import java.util.List;
+
+import tokyo.nakanaka.logger.Logger;
+import tokyo.nakanaka.math.BlockVector3D;
+import tokyo.nakanaka.selection.RegionBuildingData;
+import tokyo.nakanaka.selection.SelSubCommandHandler;
+
+public class RadiusMainSelSubCommandHandler implements SelSubCommandHandler {
+
+	@Override
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCommand(RegionBuildingData data, Logger logger, BlockVector3D playerPos, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(String[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
