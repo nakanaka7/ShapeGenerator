a3f1182e0bded95fcc5826d759ce1be8210da5b0
nakanakagames <nakanakagames@gmail.com>
2021-07-04 10:14:27 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/selection/RegularPolygonSideCommandHandler.java
@@ -0,0 +1,37 @@
+package tokyo.nakanaka.selection;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import tokyo.nakanaka.logger.LogColor;
+import tokyo.nakanaka.logger.Logger;
+import tokyo.nakanaka.math.BlockVector3D;
+
+public class RegularPolygonSideCommandHandler implements SelSubCommandHandler {
+
+	@Override
+	public String getLabel() {
+		return "side";
+	}
+
+	@Override
+	public boolean onCommand(RegionBuildingData data, Logger logger, BlockVector3D playerPos, String[] args) {
+		if(args.length != 1) {
+			throw new IllegalArgumentException();
+		}
+		Integer side = Integer.parseInt(args[0]);
+		if(side < 3) {
+			logger.print(LogColor.RED + "Must be 3, 4, 5...");
+			return true;
+		}
+		data.putInteger("side", side);
+		return true;
+	}
+
+	@Override
+	public List<String> onTabComplete(String[] args) {
+		return Arrays.asList("3", "4", "5", "6", "7", "8", "9", "10");
+	}
+
+}
