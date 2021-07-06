0ad6aa79d6fbdba6783e3210ee60e49b39ec6416
nakanakagames <nakanakagames@gmail.com>
2021-07-06 21:36:36 +0900

    use SgSubCommandHandler getLabel()

--- src/main/java/tokyo/nakanaka/commandHandler/SgSubCommandHandlerRepository.java
+++ src/main/java/tokyo/nakanaka/commandHandler/SgSubCommandHandlerRepository.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class SgSubCommandHandlerRepository {
 	private List<SgSubCommandHandler> handlerList = new ArrayList<>();
 
-	public void register(SgSubCommandHandler cmdLine) {
-		this.handlerList.add(cmdLine);
+	public void register(SgSubCommandHandler cmdHandler) {
+		this.handlerList.add(cmdHandler);
 	}
 	
 	public SgSubCommandHandler findBy(String label) {
 		for(SgSubCommandHandler e : this.handlerList) {
-			if(e.getCommandHelp().getLabel().equals(label)) {
+			if(e.getLabel().equals(label)) {
 				return e;
 			}
 		}
@@ -26,7 +26,7 @@ public class SgSubCommandHandlerRepository {
 	public List<String> getAliases(){
 		List<String> list = new ArrayList<>();
 		for(SgSubCommandHandler e : this.handlerList) {
-			list.add(e.getCommandHelp().getLabel());
+			list.add(e.getLabel());
 		}
 		return list;
 	}
