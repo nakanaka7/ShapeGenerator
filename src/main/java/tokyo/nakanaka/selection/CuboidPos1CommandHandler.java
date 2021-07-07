3c15fcf4e5a10cb0090d7f8cc30858d3abdfc786
nakanakagames <nakanakagames@gmail.com>
2021-07-07 12:49:32 +0900

    delegate to PosCommandHandler

--- src/main/java/tokyo/nakanaka/selection/CuboidPos1CommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/CuboidPos1CommandHandler.java
@@ -2,36 +2,31 @@ package tokyo.nakanaka.selection;
 
 import java.util.List;
 
-import tokyo.nakanaka.commandArgument.PositionArgument;
-import tokyo.nakanaka.logger.LogConstant;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.math.Vector3D;
 
 public class CuboidPos1CommandHandler implements SelSubCommandHandler{
-	private PositionArgument pos1Arg = new PositionArgument();
+	private PosCommandHandler posHandler = new PosCommandHandler("pos1");
 	private LengthCalculator lengthCalc = new LengthCalculator();
 
 	@Override
 	public String getLabel() {
-		return "pos1";
+		return this.posHandler.getLabel();
 	}
 	
 	@Override
 	public String getDescription() {
-		return "Set pos1";
+		return this.posHandler.getDescription();
 	}
 	
 	@Override
 	public boolean onCommand(RegionBuildingData data, Logger logger, BlockVector3D playerPos, String[] args) {
-		Vector3D pos1;
-		try {
-			pos1 = this.pos1Arg.onParse(playerPos, args);
-		}catch(IllegalArgumentException e) {
-			logger.print(LogConstant.HEAD_ERROR + "Can not parse coodinate");
-			return true;
+		boolean success = this.posHandler.onCommand(data, logger, playerPos, args);
+		if(!success) {
+			return false;
 		}
-		data.putVector3D("pos1", pos1);
+		Vector3D pos1 = data.getVector3D("pos1");
 		Vector3D pos2 = data.getVector3D("pos2");
 		if(pos2 != null) {
 			data.putDouble("width", this.lengthCalc.calcWidth(pos1, pos2));
@@ -43,7 +38,7 @@ public class CuboidPos1CommandHandler implements SelSubCommandHandler{
 
 	@Override
 	public List<String> onTabComplete(String[] args) {
-		return this.pos1Arg.onTabComplete(args);
+		return this.posHandler.onTabComplete(args);
 	}
 
 }
