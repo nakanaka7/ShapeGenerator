83c5047c8865037de3afe1bac365ec06619ad94f
nakanakagames <nakanakagames@gmail.com>
2021-07-08 16:13:28 +0900

    add getCommandHelp()

--- src/main/java/tokyo/nakanaka/commandHandler/SgSubCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/SgSubCommandHandler.java
@@ -2,10 +2,12 @@ package tokyo.nakanaka.commandHandler;
 
 import java.util.List;
 
+import tokyo.nakanaka.commadHelp.CommandHelp;
 import tokyo.nakanaka.player.Player;
 
 public interface SgSubCommandHandler {
 	String getLabel();
+	CommandHelp getCommandHelp();
 	String getDescription();
 	String getUsage();
 	void onCommand(Player player, String[] args);
