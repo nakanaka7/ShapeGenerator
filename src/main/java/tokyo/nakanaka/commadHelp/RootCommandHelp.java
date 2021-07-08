e21312bd77a358435c93f021e78d90bc966a3dfb
nakanakagames <nakanakagames@gmail.com>
2021-07-08 22:16:28 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/commadHelp/RootCommandHelp.java
@@ -0,0 +1,41 @@
+package tokyo.nakanaka.commadHelp;
+
+public class RootCommandHelp {
+	private String label;
+	private String description;
+	
+	public RootCommandHelp(Builder builder) {
+		this.label = builder.label;
+		this.description = builder.description;
+	}
+
+	public static class Builder {
+		private String label;
+		private String description;
+		
+		public Builder(String label) {
+			this.label = label;
+		}
+		
+		public Builder description(String description) {
+			this.description = description;
+			return this;
+		}
+		
+		public RootCommandHelp build() {
+			return new RootCommandHelp(this);
+		}
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void register(CommandHelp cmdHelp) {
+		
+	}
+}
