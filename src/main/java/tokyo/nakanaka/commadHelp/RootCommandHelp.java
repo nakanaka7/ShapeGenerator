9257a3a248ed8033a41ed77d44e86a68360857b0
nakanakagames <nakanakagames@gmail.com>
2021-07-08 23:21:49 +0900

    change constructor

--- src/main/java/tokyo/nakanaka/commadHelp/RootCommandHelp.java
+++ src/main/java/tokyo/nakanaka/commadHelp/RootCommandHelp.java
@@ -8,29 +8,12 @@ public class RootCommandHelp implements CommandHelp {
 	private String description;
 	private List<CommandHelp> subList = new ArrayList<>();
 	
-	public RootCommandHelp(Builder builder) {
-		this.label = builder.label;
-		this.description = builder.description;
+	public RootCommandHelp(String label, String description) {
+		super();
+		this.label = label;
+		this.description = description;
 	}
 
-	public static class Builder {
-		private String label;
-		private String description;
-		
-		public Builder(String label) {
-			this.label = label;
-		}
-		
-		public Builder description(String description) {
-			this.description = description;
-			return this;
-		}
-		
-		public RootCommandHelp build() {
-			return new RootCommandHelp(this);
-		}
-	}
-	
 	@Override
 	public String getLabel() {
 		return label;
