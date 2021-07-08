4f01bb1e2e2743e1e52bdf29242433c4230a9014
nakanakagames <nakanakagames@gmail.com>
2021-07-09 07:53:53 +0900

    edit

--- src/main/java/tokyo/nakanaka/commadHelp/BranchCommandHelp.java
+++ src/main/java/tokyo/nakanaka/commadHelp/BranchCommandHelp.java
@@ -1,14 +1,48 @@
 package tokyo.nakanaka.commadHelp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BranchCommandHelp implements CommandHelp {
 	private String label;
 	private String description;
-	private String usage;
+	private List<ParameterData> paramDataList = new ArrayList<>();
+	private String usage = "";
 	
 	public BranchCommandHelp(String label, String description, String usage) {
 		this.label = label;
 		this.description = description;
 		this.usage = usage;
+	}
+	
+	private BranchCommandHelp(Builder builder) {
+		this.label = builder.label;
+		this.description = builder.description;
+		this.paramDataList = builder.paramDataList;
+	}
+	
+	public static class Builder {
+		private String label;
+		private String description;
+		private List<ParameterData> paramDataList = new ArrayList<>();
+		
+		public Builder(String label) {
+			this.label = label;
+		}
+		
+		public Builder description(String description) {
+			this.description = description;
+			return this;
+		}
+		
+		public Builder addParameter(ParameterType type, String[] labels) {
+			this.paramDataList.add(new ParameterData(type, labels));
+			return this;
+		}
+		
+		public BranchCommandHelp build() {
+			return new BranchCommandHelp(this);
+		}
 	}
 	
 	@Override
@@ -22,6 +56,23 @@ public class BranchCommandHelp implements CommandHelp {
 	}
 	
 	public String getUsage() {
+		return usage;
+	}
+	
+	public String getUsageNew() {
+		String usage = this.label;
+		for(ParameterData data : this.paramDataList) {
+			switch(data.getType()) {
+			case OPTIONAL:
+				usage += " [" + String.join("|", data.getLabels()) + "]";
+				break;
+			case REQUIRED:
+				usage += " <" + String.join("|", data.getLabels()) + ">";
+				break;
+			default:
+				break;
+			}
+		}
 		return usage;
 	}
 		
