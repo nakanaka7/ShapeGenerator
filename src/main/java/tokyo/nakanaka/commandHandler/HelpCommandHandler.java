e76d4dc163b722f60f4deb3d6998a45736c3da56
nakanakagames <nakanakagames@gmail.com>
2021-07-10 08:59:09 +0900

    refactor

--- src/main/java/tokyo/nakanaka/commandHandler/HelpCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/HelpCommandHandler.java
@@ -2,6 +2,7 @@ package tokyo.nakanaka.commandHandler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import tokyo.nakanaka.commadHelp.BranchCommandHelp;
 import tokyo.nakanaka.commadHelp.CommandHelp;
@@ -44,48 +45,67 @@ public class HelpCommandHandler implements CommandHandler{
 		return rootCmdHelp;
 	}
 	
-	@Override
-	public void onCommand(Player player, String[] args) {
+	public void onCommand(RootCommandHelp help, Player player, String[] args) {
 		Logger logger = player.getLogger();
-		RootCommandHelp sgHelp = this.getSgCommandHelp(player);
-		CommandHelp help = sgHelp.getSubHelp(args);
-		if(help == null) {
-			logger.print(LogColor.RED + "Usage: " + "/sg " + this.cmdHelp.getUsage());
+		String label = help.getLabel();
+		CommandHelp subHelp = help.getSubHelp(args);
+		if(subHelp == null) {
+			logger.print(LogColor.RED + "Usage: " + "/" + label + " " + this.cmdHelp.getUsage());
 			return;
 		}
 		logger.print(LogColor.YELLOW + "---------| " + LogColor.RESET
 				+ "Help: " 
-				+ LogColor.GOLD + "/sg " + String.join(" ", args) + LogColor.RESET
+				+ LogColor.GOLD + "/" + label + " " + String.join(" ", args) + LogColor.RESET
 				+ LogColor.YELLOW + " |-----------------");
-		logger.print(LogColor.GOLD + "Description: " + LogColor.RESET + help.getDescription());
-		String usage = "/sg " + String.join(" ", args);
-		if(help instanceof RootCommandHelp) {
+		logger.print(LogColor.GOLD + "Description: " + LogColor.RESET + subHelp.getDescription());
+		String usage = "/" + label + " " + String.join(" ", args);
+		if(subHelp instanceof RootCommandHelp) {
 			usage += " " + "<subcommand>"; 
-		}else if(help instanceof BranchCommandHelp) {
-			usage += " " + ((BranchCommandHelp) help).getParameterUsage(); 
+		}else if(subHelp instanceof BranchCommandHelp) {
+			usage += " " + ((BranchCommandHelp) subHelp).getParameterUsage(); 
 		}
 		logger.print(LogColor.GOLD + "Usage: " + LogColor.RESET + usage);
-		if(help instanceof RootCommandHelp) {
+		if(subHelp instanceof RootCommandHelp) {
 			logger.print(LogColor.GOLD + "SubCommand:");
-			List<CommandHelp> subList = ((RootCommandHelp) help).getSubCommandHelp();
+			List<CommandHelp> subList = ((RootCommandHelp) subHelp).getSubCommandHelp();
 			for(CommandHelp e : subList) {
 				logger.print(" " + LogColor.GOLD +e.getLabel() + ": " + LogColor.RESET + e.getDescription());
 			}
-			logger.print(LogColor.GREEN + "Type /sg help " + String.join(" ", args) + " <subcommand> " + "for detail");
+			logger.print(LogColor.GREEN + "Type " + "/" + label + " " + "help " + String.join(" ", args) + " <subcommand> " + "for detail");
 		}
 	}
 	
 	@Override
-	public List<String> onTabComplete(Player player, String[] args) {
-		if(args.length == 1) {
-			return new ArrayList<>(this.cmdRepo.getAliases());
-		}else if(args.length == 2 && args[0].equals("sel")) {
-			SelCommandHandler selHandler = (SelCommandHandler) this.cmdRepo.findBy("sel");
-			List<String> subLabels = selHandler.getSubCommandLabels(player);
-			return subLabels;
-		}else {
-			return new ArrayList<>();
-		}
+	public void onCommand(Player player, String[] args) {
+		RootCommandHelp help = this.getSgCommandHelp(player);
+		this.onCommand(help, player, args);
 	}
-
+	
+	public List<String> onTabComplete(CommandHelp help, Player player, String[] args) {
+		String subLabel = args[0];
+		String[] shiftArgs = new String[args.length - 1];
+		System.arraycopy(args, 1, shiftArgs, 0, args.length - 1);
+		
+		if(args.length == 1) {
+			if(help instanceof RootCommandHelp) {
+				List<CommandHelp> subHelpList = ((RootCommandHelp) help).getSubCommandHelp();
+				return subHelpList.stream()
+						.map(s -> s.getLabel())
+						.collect(Collectors.toList());
+			}else {
+				return new ArrayList<>();
+			}	
+		}
+		if(help instanceof RootCommandHelp) {
+			CommandHelp subHelp = help.getSubHelp(subLabel);
+			return onTabComplete(subHelp, player, shiftArgs);
+		}
+		return new ArrayList<>();
+	}
+	
+	public List<String> onTabComplete(Player player, String[] args) {
+		RootCommandHelp help = this.getSgCommandHelp(player);
+		return onTabComplete(help, player, args);
+	}
+	
 }
