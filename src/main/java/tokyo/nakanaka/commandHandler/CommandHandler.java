fbc5496c917f6e658ab85f01cb8057fadaf90fc6
nakanakagames <nakanakagames@gmail.com>
2021-07-09 12:49:06 +0900

    change return

--- src/main/java/tokyo/nakanaka/commandHandler/CommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/CommandHandler.java
@@ -2,12 +2,12 @@ package tokyo.nakanaka.commandHandler;
 
 import java.util.List;
 
-import tokyo.nakanaka.commadHelp.BranchCommandHelp;
+import tokyo.nakanaka.commadHelp.CommandHelp;
 import tokyo.nakanaka.player.Player;
 
 public interface CommandHandler {
 	String getLabel();
-	BranchCommandHelp getCommandHelp(Player player);
+	CommandHelp getCommandHelp(Player player);
 	void onCommand(Player player, String[] args);
 	List<String> onTabComplete(Player player, String[] args);
 }
