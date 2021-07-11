f66b35a2440a36f528d3a7b3a00ea0f9d472c877
nakanakagames <nakanakagames@gmail.com>
2021-07-11 17:25:16 +0900

    remove old

--- src/main/java/tokyo/nakanaka/commandHandler/DelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/DelCommandHandler.java
@@ -88,30 +88,6 @@ public class DelCommandHandler implements CommandHandler {
 		return;
 	}
 	
-	public void onCommandOld(Player player, String[] args) {
-		Logger logger = player.getLogger();
-		if(args.length != 0) {
-			logger.print(LogColor.RED + "Usage: " + "/sg " + this.cmdHelp.getUsage());
-			return;
-		}
-		UndoCommandManager undoManager = player.getUndoCommandManager();
-		UndoableCommand cmd = undoManager.getLastUndoCommand();
-		GenerateCommand originalCmd;
-		if(cmd instanceof GenerateCommand) {
-			originalCmd = (GenerateCommand) cmd;
-		}else if(cmd instanceof AdjustCommand) {
-			originalCmd = ((AdjustCommand)cmd).getLastCommand();
-		}else {
-			logger.print(LogColor.RED + "Generate blocks first");
-			return;
-		}
-		DeleteCommand deleteCmd = new DeleteCommand(originalCmd);
-		deleteCmd.execute();
-		undoManager.add(deleteCmd);
-		logger.print(LogColor.DARK_AQUA + "Deleted block(s)");
-		return;
-	}
-
 	@Override
 	public List<String> onTabComplete(Player player, String[] args) {
 		if(args.length == 1) {
