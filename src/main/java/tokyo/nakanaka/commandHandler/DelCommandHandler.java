8466f330475199cbbf883e0fcf501e033ddced15
nakanakagames <nakanakagames@gmail.com>
2021-07-11 17:24:47 +0900

    enable to delete more than 1

--- src/main/java/tokyo/nakanaka/commandHandler/DelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/DelCommandHandler.java
@@ -1,11 +1,13 @@
 package tokyo.nakanaka.commandHandler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import tokyo.nakanaka.UndoCommandManager;
 import tokyo.nakanaka.commadHelp.BranchCommandHelp;
 import tokyo.nakanaka.commadHelp.CommandHelp;
+import tokyo.nakanaka.commadHelp.ParameterType;
 import tokyo.nakanaka.command.AdjustCommand;
 import tokyo.nakanaka.command.DeleteCommand;
 import tokyo.nakanaka.command.GenerateCommand;
@@ -20,6 +22,7 @@ public class DelCommandHandler implements CommandHandler {
 	public DelCommandHandler() {
 		this.cmdHelp = new BranchCommandHelp.Builder("del")
 				.description("Delete the generated blocks")
+				.addParameter(ParameterType.OPTIONAL, "number")
 				.build();
 	}
 
@@ -33,36 +36,59 @@ public class DelCommandHandler implements CommandHandler {
 		return this.cmdHelp;
 	}
 	
-	public void onCommandNew(Player player, String[] args) {
+	public void onCommand(Player player, String[] args) {
 		Logger logger = player.getLogger();
-		if(args.length != 0) {
+		if(args.length > 1) {
 			logger.print(LogColor.RED + "Usage: " + "/sg " + this.cmdHelp.getUsage());
 			return;
 		}
-		UndoCommandManager undoManager = player.getUndoCommandManager();
-		GenerateCommand originalCmd = null;
-		for(int i = undoManager.undoSize() - 1; 0 <= i; --i) {
-			UndoableCommand cmd = undoManager.getUndoCommand(i);
-			if(cmd instanceof GenerateCommand) {
-				originalCmd = (GenerateCommand) cmd;
-				break;
-			}else if(cmd instanceof AdjustCommand) {
-				originalCmd = ((AdjustCommand)cmd).getLastCommand();
-				break;
+		int num = 1;
+		if(args.length == 1) {
+			try {
+				num = Integer.parseInt(args[0]);
+			}catch(IllegalArgumentException e) {
+				logger.print(LogColor.RED + "Can not parse the number");
+				return;
+			}
+			if(num <= 0) {
+				logger.print(LogColor.RED + "The number must be larger than 0");
+				return;
 			}
 		}
-		if(originalCmd == null) {
+		UndoCommandManager undoManager = player.getUndoCommandManager();
+		List<GenerateCommand> originalList = new ArrayList<>();
+		for(int i = undoManager.undoSize() - 1; i >= 0; --i) {
+			UndoableCommand cmd = undoManager.getUndoCommand(i);
+			GenerateCommand originalCmd = null;
+			if(cmd instanceof GenerateCommand) {
+				originalCmd = (GenerateCommand) cmd;
+			}else if(cmd instanceof AdjustCommand) {
+				originalCmd = ((AdjustCommand)cmd).getLastCommand();
+			}
+			if(originalCmd != null && !originalCmd.hasUndone()) {
+				originalList.add(originalCmd);
+				if(originalList.size() == num) {
+					break;
+				}
+			}
+		}
+		int delNum = originalList.size();
+		DeleteCommand deleteCmd
+			= new DeleteCommand(originalList.toArray(new GenerateCommand[delNum]));
+		deleteCmd.execute();
+		undoManager.add(deleteCmd);
+		if(delNum == 0) {
 			logger.print(LogColor.RED + "Generate blocks first");
 			return;
 		}
-		DeleteCommand deleteCmd = new DeleteCommand(originalCmd);
-		deleteCmd.execute();
-		undoManager.add(deleteCmd);
-		logger.print(LogColor.DARK_AQUA + "Deleted block(s)");
+		logger.print(LogColor.DARK_AQUA + "Deleted " + delNum + "generation");
+		if(delNum < num) {
+			logger.print(LogColor.RED + "reached the first generation");
+		}
 		return;
 	}
 	
-	public void onCommand(Player player, String[] args) {
+	public void onCommandOld(Player player, String[] args) {
 		Logger logger = player.getLogger();
 		if(args.length != 0) {
 			logger.print(LogColor.RED + "Usage: " + "/sg " + this.cmdHelp.getUsage());
@@ -88,6 +114,9 @@ public class DelCommandHandler implements CommandHandler {
 
 	@Override
 	public List<String> onTabComplete(Player player, String[] args) {
+		if(args.length == 1) {
+			return Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+		}
 		return new ArrayList<>();
 	}
 
