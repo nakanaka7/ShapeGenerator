0171b1ef432c1d02f0ab26d8bdde809f1c3291d7
nakanakagames <nakanakagames@gmail.com>
2021-07-12 11:20:35 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/command/MaxXCommand.java
@@ -0,0 +1,38 @@
+package tokyo.nakanaka.command;
+
+import tokyo.nakanaka.selection.Selection;
+
+public class MaxXCommand implements AdjustCommand {
+	private GenerateCommand originalCmd;
+	private GenerateCommand lastCmd;
+	
+	public MaxXCommand(GenerateCommand originalCmd, double maxX, boolean physics){
+		this.originalCmd = originalCmd;
+		Selection sel = originalCmd.getSelection().getXLimitedSelection(maxX);
+		this.lastCmd = new GenerateCommand(sel, originalCmd.getBlock(), physics);
+	}
+	
+	@Override
+	public void execute() {
+		this.originalCmd.undo();
+		this.lastCmd.execute();
+	}
+
+	@Override
+	public void undo() {
+		this.lastCmd.undo();
+		this.originalCmd.redo();
+	}
+
+	@Override
+	public void redo() {
+		this.originalCmd.undo();
+		this.lastCmd.redo();
+	}
+
+	@Override
+	public GenerateCommand getLastCommand() {
+		return this.lastCmd;
+	}
+
+}
