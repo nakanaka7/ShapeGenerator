c73d2f77dad9ed4626e18dd29b4e458c4e995342
nakanakagames <nakanakagames@gmail.com>
2021-07-13 22:05:30 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/command/MinYCommand.java
@@ -0,0 +1,55 @@
+package tokyo.nakanaka.command;
+
+import tokyo.nakanaka.math.boundRegion3D.BoundRegion3D;
+import tokyo.nakanaka.math.boundRegion3D.CuboidBoundRegion;
+import tokyo.nakanaka.math.region3D.LogicalConjunctRegion3D;
+import tokyo.nakanaka.math.region3D.MinYRegion3D;
+import tokyo.nakanaka.math.region3D.Region3D;
+import tokyo.nakanaka.selection.Selection;
+
+public class MinYCommand implements AdjustCommand {
+	private GenerateCommand originalCmd;
+	private GenerateCommand lastCmd;
+	
+	public MinYCommand(GenerateCommand originalCmd, double minY, boolean physics){
+		this.originalCmd = originalCmd;
+		Selection originalSel = originalCmd.getSelection();
+		BoundRegion3D bound = originalSel.getBoundRegion3D();
+		Region3D region = bound.getRegion3D();
+		double ubx = bound.getUpperBoundX();
+		double uby = bound.getUpperBoundY();
+		double ubz = bound.getUpperBoundZ();
+		double lbx = bound.getLowerBoundX();
+		double lby = minY;
+		double lbz = bound.getLowerBoundZ();
+		Region3D minYReg = new MinYRegion3D(minY);
+		Region3D newRegion = new LogicalConjunctRegion3D(region, minYReg);
+		BoundRegion3D newBound = new CuboidBoundRegion(newRegion, ubx, uby, ubz, lbx, lby, lbz);
+		Selection sel = new Selection(originalSel.getWorld(), newBound, originalSel.getOffset());
+		this.lastCmd = new GenerateCommand(sel, originalCmd.getBlock(), physics);
+	}
+	
+	@Override
+	public void execute() {
+		this.originalCmd.undo();
+		this.lastCmd.execute();
+	}
+
+	@Override
+	public void undo() {
+		this.lastCmd.undo();
+		this.originalCmd.redo();
+	}
+
+	@Override
+	public void redo() {
+		this.originalCmd.undo();
+		this.lastCmd.redo();
+	}
+
+	@Override
+	public GenerateCommand getLastCommand() {
+		return this.lastCmd;
+	}
+	
+}
