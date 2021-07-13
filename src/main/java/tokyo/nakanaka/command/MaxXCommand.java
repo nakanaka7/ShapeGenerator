af05bd7eddcf4bf53447eb3145f406e7b0f2a836
nakanakagames <nakanakagames@gmail.com>
2021-07-13 18:17:12 +0900

    fix

--- src/main/java/tokyo/nakanaka/command/MaxXCommand.java
+++ src/main/java/tokyo/nakanaka/command/MaxXCommand.java
@@ -1,5 +1,10 @@
 package tokyo.nakanaka.command;
 
+import tokyo.nakanaka.math.boundRegion3D.BoundRegion3D;
+import tokyo.nakanaka.math.boundRegion3D.CuboidBoundRegion;
+import tokyo.nakanaka.math.region3D.LogicalConjunctRegion3D;
+import tokyo.nakanaka.math.region3D.MaxXRegion3D;
+import tokyo.nakanaka.math.region3D.Region3D;
 import tokyo.nakanaka.selection.Selection;
 
 public class MaxXCommand implements AdjustCommand {
@@ -9,7 +14,17 @@ public class MaxXCommand implements AdjustCommand {
 	public MaxXCommand(GenerateCommand originalCmd, double maxX, boolean physics){
 		this.originalCmd = originalCmd;
 		Selection originalSel = originalCmd.getSelection();
-		Selection sel = originalSel.getXLimitedSelection(maxX);
+		BoundRegion3D bound = originalSel.getBoundRegion3D();
+		Region3D region = bound.getRegion3D();
+		double uby = bound.getUpperBoundY();
+		double ubz = bound.getUpperBoundZ();
+		double lbx = bound.getLowerBoundX();
+		double lby = bound.getLowerBoundY();
+		double lbz = bound.getLowerBoundZ();
+		Region3D maxXReg = new MaxXRegion3D(maxX);
+		Region3D newRegion = new LogicalConjunctRegion3D(region, maxXReg);
+		BoundRegion3D newBound = new CuboidBoundRegion(newRegion, maxX, uby, ubz, lbx, lby, lbz);
+		Selection sel = new Selection(originalSel.getWorld(), newBound, originalSel.getOffset());
 		this.lastCmd = new GenerateCommand(sel, originalCmd.getBlock(), physics);
 	}
 	
