aa29d0bb636af3d515cf47f625e6d5fe265b2990
nakanakagames <nakanakagames@gmail.com>
2021-07-13 10:14:09 +0900

    add createRotatedRegion

--- src/main/java/tokyo/nakanaka/math/boundRegion3D/SphereBoundRegion.java
+++ src/main/java/tokyo/nakanaka/math/boundRegion3D/SphereBoundRegion.java
@@ -1,11 +1,12 @@
 package tokyo.nakanaka.math.boundRegion3D;
 
+import tokyo.nakanaka.geometricProperty.Axis;
 import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.math.region3D.BlockRegion3D;
 import tokyo.nakanaka.math.region3D.Region3D;
 import tokyo.nakanaka.math.region3D.Region3Ds;
 
-public class SphereBoundRegion {
+public class SphereBoundRegion implements BoundRegion3D {
 	private Region3D region;
 	private Vector3D center;
 	private double radius;
@@ -20,20 +21,22 @@ public class SphereBoundRegion {
 		return this.region;
 	}
 	
-	@Override
-	public SphereBoundRegion clone() {
-		return new SphereBoundRegion(this.region, this.center, this.radius);
+	public BoundRegion3D createShiftedRegion(Vector3D displacement) {
+		Region3D newRegion = Region3Ds.shift(this.region, displacement);
+		Vector3D newCenter = this.center.add(displacement);
+		return new SphereBoundRegion(newRegion, newCenter, this.radius);
+	}
+
+	public BoundRegion3D createRotatedRegion(Vector3D offset, Axis axis, double degree) {
+		return null;
 	}
 	
-	public void shift(Vector3D displacement) {
-		this.region = Region3Ds.shift(this.region, displacement);
-	}
 	
-	public BlockRegion3D getBlockRegion3D() {
+	public BlockRegion3D toBlockRegion3D() {
 		double cx = this.center.getX();
 		double cy = this.center.getY();
 		double cz = this.center.getZ();
 		return new BlockRegion3D(this.region, cx + radius, cy + radius, cz + radius, cx - radius, cy - radius, cz - radius);
 	}
-	
+
 }
