dda9cd4b8f725b6a515c3c5030faad46e867a2d0
nakanakagames <nakanakagames@gmail.com>
2021-07-14 12:07:35 +0900

    use SelectionStrategySource

--- src/main/java/tokyo/nakanaka/selection/selSubCommandHandler/ResetCommandHandler.java
+++ src/main/java/tokyo/nakanaka/selection/selSubCommandHandler/ResetCommandHandler.java
@@ -2,7 +2,6 @@ package tokyo.nakanaka.selection.selSubCommandHandler;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import tokyo.nakanaka.commadHelp.BranchCommandHelp;
 import tokyo.nakanaka.logger.LogColor;
@@ -12,15 +11,16 @@ import tokyo.nakanaka.selection.SelectionBuildingData;
 import tokyo.nakanaka.selection.SelectionMessenger;
 import tokyo.nakanaka.selection.SelectionShape;
 import tokyo.nakanaka.selection.selectionStrategy.SelectionStrategy;
+import tokyo.nakanaka.selection.selectionStrategy.SelectionStrategySource;
 import tokyo.nakanaka.world.World;
 
 public class ResetCommandHandler {
-	private Map<SelectionShape, SelectionStrategy> strategyMap;
+	private SelectionStrategySource selStraSource;
 	private SelectionMessenger selMessenger = new SelectionMessenger();
 	private BranchCommandHelp cmdHelp;
 	
-	public ResetCommandHandler(Map<SelectionShape, SelectionStrategy> strategyMap) {
-		this.strategyMap = strategyMap;
+	public ResetCommandHandler(SelectionStrategySource selStraSource) {
+		this.selStraSource = selStraSource;
 		this.cmdHelp = new BranchCommandHelp.Builder("reset")
 				.description("Reset the selection")
 				.build();
@@ -38,7 +38,7 @@ public class ResetCommandHandler {
 		}
 		World world = player.getWorld();
 		SelectionShape shape = player.getSelectionShape();
-		SelectionStrategy strategy = this.strategyMap.get(shape);
+		SelectionStrategy strategy = this.selStraSource.get(shape);
 		SelectionBuildingData newSelData = new SelectionBuildingData(world, strategy.newRegionBuildingData());
 		player.setSelectionBuildingData(newSelData);
 		String defaultOffsetLabel = strategy.getDefaultOffsetLabel();
