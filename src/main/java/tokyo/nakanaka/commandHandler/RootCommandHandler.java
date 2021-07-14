66cc74f2bbb2761b4d9ad8a24495037f830943ce
nakanakagames <nakanakagames@gmail.com>
2021-07-14 19:39:49 +0900

    extends CommandEntry

--- src/main/java/tokyo/nakanaka/commandHandler/RootCommandHandler.java
+++ src/main/java/tokyo/nakanaka/commandHandler/RootCommandHandler.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import tokyo.nakanaka.player.Player;
 
-public interface RootCommandHandler {
+public interface RootCommandHandler extends CommandEntry {
 	String getLabel();
 	String getDescription();
 	List<BranchCommandHandler> getSubList(Player player);
