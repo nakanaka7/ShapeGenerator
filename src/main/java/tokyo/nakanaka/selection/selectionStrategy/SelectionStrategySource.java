797d2985e95938cc2dd7caccd7feb776daf153b7
nakanakagames <nakanakagames@gmail.com>
2021-07-14 11:44:45 +0900

    return null if there is no strategy for the shape

--- src/main/java/tokyo/nakanaka/selection/selectionStrategy/SelectionStrategySource.java
+++ src/main/java/tokyo/nakanaka/selection/selectionStrategy/SelectionStrategySource.java
@@ -20,7 +20,7 @@ public class SelectionStrategySource {
 		case TRIANGLE:
 			return new TriangleSelectionStrategy();
 		default:
-			throw new IllegalArgumentException();
+			return null;
 		}
 	}
 	
