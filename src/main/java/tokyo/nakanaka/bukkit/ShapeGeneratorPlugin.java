998d6ade3f800537cd7d1b8458d33f4d924c6ae4
nakanakagames <nakanakagames@gmail.com>
2021-07-15 18:40:33 +0900

    use SgCommandHandler

--- src/main/java/tokyo/nakanaka/bukkit/ShapeGeneratorPlugin.java
+++ src/main/java/tokyo/nakanaka/bukkit/ShapeGeneratorPlugin.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import tokyo.nakanaka.ClickBlockEventHandler;
 import tokyo.nakanaka.CommandLine;
 import tokyo.nakanaka.Scheduler;
-import tokyo.nakanaka.commandHandler.RootCommandHandler;
+import tokyo.nakanaka.SgCommandHandler;
 import tokyo.nakanaka.commandHandler.SgCommandDirectory;
 import tokyo.nakanaka.player.Player;
 import tokyo.nakanaka.player.PlayerRepository;
@@ -18,7 +18,7 @@ import tokyo.nakanaka.selection.selectionStrategy.SelectionStrategySource;
 
 public class ShapeGeneratorPlugin extends JavaPlugin{
 	private CommandLineBuilder cmdLineBuilder;
-	private RootCommandHandler rootCmdHandler;
+	private SgCommandHandler sgCmdHandler;
 	
 	@Override
 	public void onEnable() {
@@ -26,7 +26,7 @@ public class ShapeGeneratorPlugin extends JavaPlugin{
 		this.cmdLineBuilder = new CommandLineBuilder(this.getServer(), playerRepo);
 		SelectionStrategySource selStraSource = new SelectionStrategySource();
 		SgCommandDirectory sgCmdDir = new SgCommandDirectory(new BukkitBlockArgument(), selStraSource);	
-		this.rootCmdHandler = new RootCommandHandler(sgCmdDir);
+		this.sgCmdHandler = new SgCommandHandler(sgCmdDir);
 		PluginManager plManager = getServer().getPluginManager();
 		ClickBlockEventHandler clickHandler = new ClickBlockEventHandler(selStraSource);
 		Scheduler scheduler = new BukkitScheduler(this);
@@ -37,14 +37,14 @@ public class ShapeGeneratorPlugin extends JavaPlugin{
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		CommandLine cmdLine = this.cmdLineBuilder.build(sender, label, args);
 		Player player = cmdLine.getPlayer();
-		this.rootCmdHandler.onCommand(player, args);
+		this.sgCmdHandler.onCommand(player, args);
 		return true;
 	}
 	
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args){
 		CommandLine cmdLine = this.cmdLineBuilder.build(sender, alias, args);
-		return this.rootCmdHandler.onTabComplete(cmdLine.getPlayer(), args);
+		return this.sgCmdHandler.onTabComplete(cmdLine.getPlayer(), args);
 	}
 	
 }
