cdb7359ee30d86baf522eed4e1e52ac4066e43c9
nakanakagames <nakanakagames@gmail.com>
2021-07-15 12:59:48 +0900

    delete unused methods

--- src/main/java/tokyo/nakanaka/commandHandler/SgCommandDirectory.java
+++ src/main/java/tokyo/nakanaka/commandHandler/SgCommandDirectory.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tokyo.nakanaka.commandArgument.BlockCommandArgument;
-import tokyo.nakanaka.logger.LogColor;
-import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.player.Player;
 import tokyo.nakanaka.selection.selectionStrategy.SelectionStrategySource;
 
@@ -49,45 +47,4 @@ public class SgCommandDirectory implements CommandDirectory {
 		return subList;
 	}
 	
-	public SgSubCommandHandlerRepository getCommandRepository() {
-		SgSubCommandHandlerRepository cmdRepo = new SgSubCommandHandlerRepository();
-		for(CommandEntry cmdEntry : this.subList) {
-			cmdRepo.register((CommandHandler)cmdEntry);
-		}
-		return cmdRepo;
-	}
-	
-	public void onCommand(Player player, String[] args) {
-		Logger logger = player.getLogger();
-		String helpMsg = "See help by " + LogColor.GOLD + "/sg help";
-		if(args.length == 0) {
-			logger.print(helpMsg);
-			return;
-		}
-		String label = args[0];
-		String[] shiftArgs = new String[args.length - 1];
-		System.arraycopy(args, 1, shiftArgs, 0, args.length - 1);
-		CommandHandler cmdHandler = this.getCommandRepository().findBy(label);
-		if(cmdHandler == null) {
-			logger.print(helpMsg);
-			return;
-		}
-		cmdHandler.onCommand(player, shiftArgs);
-	}
-	
-	public List<String> onTabComplete(Player player, String[] args){
-		if(args.length == 1) {
-			return new ArrayList<>(this.getCommandRepository().getAliases());
-		}
-		String label = args[0];
-		String[] shiftArgs = new String[args.length - 1];
-		System.arraycopy(args, 1, shiftArgs, 0, args.length - 1);
-		CommandHandler cmdHandler = this.getCommandRepository().findBy(label);
-		if(cmdHandler != null) {
-			return cmdHandler.onTabComplete(player, shiftArgs);
-		}else {
-			return new ArrayList<>();
-		}
-	}
-		
 }
