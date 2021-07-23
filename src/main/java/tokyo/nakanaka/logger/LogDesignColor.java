4bef15a320c13eccf4acbb71eca36992a2d64661
nakanakagames <nakanakagames@gmail.com>
2021-07-24 08:16:59 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/logger/LogDesignColor.java
@@ -0,0 +1,9 @@
+package tokyo.nakanaka.logger;
+
+public class LogDesignColor {
+	public static LogColor MAIN = LogColor.GOLD;
+	public static LogColor SUB = LogColor.WHITE;
+	public static LogColor NORMAL = LogColor.GRAY;
+	public static LogColor ERROR = LogColor.RED;
+	
+}
