a2997c734570a70172f817a8dfddbf8c055e1e99
nakanakagames <nakanakagames@gmail.com>
2021-07-24 10:05:20 +0900

    change NORMAL color

--- src/main/java/tokyo/nakanaka/logger/LogDesignColor.java
+++ src/main/java/tokyo/nakanaka/logger/LogDesignColor.java
@@ -3,7 +3,7 @@ package tokyo.nakanaka.logger;
 public class LogDesignColor {
 	public static LogColor MAIN = LogColor.GOLD;
 	public static LogColor SUB = LogColor.WHITE;
-	public static LogColor NORMAL = LogColor.GRAY;
+	public static LogColor NORMAL = LogColor.DARK_AQUA;
 	public static LogColor ERROR = LogColor.RED;
 	
 }
