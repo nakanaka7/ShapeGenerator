e279a17529259e08c035fcb3121c269871421305
nakanakagames <nakanakagames@gmail.com>
2021-07-26 11:51:57 +0900

    add ofPair()

--- src/main/java/tokyo/nakanaka/logger/LogTemplate.java
+++ src/main/java/tokyo/nakanaka/logger/LogTemplate.java
@@ -1,10 +1,17 @@
 package tokyo.nakanaka.logger;
 
+import tokyo.nakanaka.Pair;
+
 public class LogTemplate {
 	public static String ofLine(String title) {
 		return LogColor.GRAY + "--- " 
 			+ LogColor.RESET + LogColor.GOLD + "[" + title + "]"
 			+ LogColor.RESET + LogColor.GRAY + " ---------------------";		
+	}
+	
+	public static String ofPair(Pair<?, ?> pair) {
+		return LogColor.GOLD + pair.getFirst().toString() + ": " 
+			+ LogColor.RESET + LogColor.WHITE + pair.getSecond();
 	}
 	
 }
