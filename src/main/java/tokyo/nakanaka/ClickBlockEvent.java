98bc63a5780390e0105b56d164bd958490f01c91
nakanakagames <nakanakagames@gmail.com>
2021-07-29 15:55:37 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/ClickBlockEvent.java
@@ -0,0 +1,39 @@
+package tokyo.nakanaka;
+
+import tokyo.nakanaka.commandSender.PlayerCommandSender;
+
+public class ClickBlockEvent {
+	private PlayerCommandSender player;
+	private BlockPosition blockPos;
+	private HandType handType;
+	private Item item;
+	
+	public ClickBlockEvent(PlayerCommandSender player, BlockPosition blockPos, HandType handType, Item item) {
+		this.player = player;
+		this.blockPos = blockPos;
+		this.handType = handType;
+		this.item = item;
+	}
+	
+	public static enum HandType{
+		LEFT_HAND,
+		RIGHT_HAND;
+	}
+
+	public PlayerCommandSender getPlayer() {
+		return player;
+	}
+
+	public BlockPosition getBlockPos() {
+		return blockPos;
+	}
+
+	public HandType getHandType() {
+		return handType;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+}
