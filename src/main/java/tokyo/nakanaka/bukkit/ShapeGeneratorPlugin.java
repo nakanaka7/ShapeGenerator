4b18d82c5b129b367be615b46ab11646696f9b44
nakanakagames <nakanakagames@gmail.com>
2021-07-30 09:09:55 +0900

    delegate to main

--- src/main/java/tokyo/nakanaka/bukkit/ShapeGeneratorPlugin.java
+++ src/main/java/tokyo/nakanaka/bukkit/ShapeGeneratorPlugin.java
@@ -1,50 +1,64 @@
 package tokyo.nakanaka.bukkit;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import tokyo.nakanaka.ClickBlockEventHandler;
-import tokyo.nakanaka.CommandLine;
-import tokyo.nakanaka.Scheduler;
-import tokyo.nakanaka.SgCommandHandler;
-import tokyo.nakanaka.commandHandler.SgCommandDirectory;
-import tokyo.nakanaka.player.Player;
-import tokyo.nakanaka.player.PlayerRepository;
+import tokyo.nakanaka.Main;
+import tokyo.nakanaka.bukkit.commandSender.BukkitBlockCommandSender;
+import tokyo.nakanaka.bukkit.commandSender.BukkitConsoleCommandSender;
+import tokyo.nakanaka.bukkit.commandSender.BukkitPlayerCommandSender;
+import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.selection.selectionStrategy.SelectionStrategySource;
 
-public class ShapeGeneratorPlugin extends JavaPlugin{
-	private CommandLineBuilder cmdLineBuilder;
-	private SgCommandHandler sgCmdHandler;
+public class ShapeGeneratorPlugin extends JavaPlugin {
+	private Main main;
 	
 	@Override
 	public void onEnable() {
-		PlayerRepository playerRepo = new PlayerRepository();
-		this.cmdLineBuilder = new CommandLineBuilder(this.getServer(), playerRepo);
-		SelectionStrategySource selStraSource = new SelectionStrategySource();
-		SgCommandDirectory sgCmdDir = new SgCommandDirectory(new BukkitBlockArgument(), selStraSource);	
-		this.sgCmdHandler = new SgCommandHandler(sgCmdDir);
-		PluginManager plManager = getServer().getPluginManager();
-		ClickBlockEventHandler clickHandler = new ClickBlockEventHandler(selStraSource);
-		Scheduler scheduler = new BukkitScheduler(this);
-		plManager.registerEvents(new ClickBlockEventHandlerAdapter(this.getServer(), playerRepo, scheduler, clickHandler), this);
+		SelectionStrategySource selStrtgSource = new SelectionStrategySource();
+		this.main = new Main(new BukkitBlockArgument(), selStrtgSource);
+		this.getServer().getPluginManager()
+			.registerEvents(new BukkitClickBlockEventListener(this.main, new BukkitScheduler(this)), this);
 	}
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		CommandLine cmdLine = this.cmdLineBuilder.build(sender, label, args);
-		Player player = cmdLine.getPlayer();
-		this.sgCmdHandler.onCommand(player, args);
+	public boolean onCommand(org.bukkit.command.CommandSender cmdSender0, Command command, String label, String[] args) {
+		CommandSender cmdSender;
+		try{
+			cmdSender = convertCommandSender(cmdSender0);
+		}catch(IllegalArgumentException e) {
+			return true;
+		}
+		this.main.onSgCommand(cmdSender, args);
 		return true;
 	}
 	
 	@Override
-	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args){
-		CommandLine cmdLine = this.cmdLineBuilder.build(sender, alias, args);
-		return this.sgCmdHandler.onTabComplete(cmdLine.getPlayer(), args);
+	public List<String> onTabComplete(org.bukkit.command.CommandSender cmdSender0, Command command, String alias, String[] args){
+		CommandSender cmdSender;
+		try{
+			cmdSender = convertCommandSender(cmdSender0);
+		}catch(IllegalArgumentException e) {
+			return new ArrayList<>();
+		}
+		return this.main.onSgTabComplete(cmdSender, args);
+	}
+	
+	private static CommandSender convertCommandSender(org.bukkit.command.CommandSender cmdSender0) {
+		if(cmdSender0 instanceof Player p) {
+			return new BukkitPlayerCommandSender(p);
+		}else if(cmdSender0 instanceof BlockCommandSender b) {
+			return new BukkitBlockCommandSender(b);
+		}else if(cmdSender0 instanceof ConsoleCommandSender c) {
+			return new BukkitConsoleCommandSender(c);
+		}
+		throw new IllegalArgumentException();
 	}
 	
 }
