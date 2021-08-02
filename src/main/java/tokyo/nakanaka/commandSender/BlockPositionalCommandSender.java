1d46dc3d4afd61d6b46bd67f1d2e72b235eac2b3
nakanakagames <nakanakagames@gmail.com>
2021-08-02 16:26:40 +0900

    rename

--- src/main/java/tokyo/nakanaka/commandSender/BlockPositionalCommandSender.java
+++ src/main/java/tokyo/nakanaka/commandSender/BlockPositionalCommandSender.java
@@ -2,6 +2,6 @@ package tokyo.nakanaka.commandSender;
 
 import tokyo.nakanaka.BlockPosition;
 
-public interface PositionalCommandSender extends CommandSender {
+public interface BlockPositionalCommandSender extends CommandSender {
 	public BlockPosition getBlockPosition();
 }
