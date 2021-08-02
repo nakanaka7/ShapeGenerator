09db00bfd98a31dccf56ff0b866ec03d0e425a69
nakanakagames <nakanakagames@gmail.com>
2021-08-02 16:45:01 +0900

    extends BukkitLogger

--- src/main/java/tokyo/nakanaka/bukkit/commandSender/BukkitConsoleCommandSender.java
+++ src/main/java/tokyo/nakanaka/bukkit/commandSender/BukkitConsoleCommandSender.java
@@ -1,17 +1,12 @@
 package tokyo.nakanaka.bukkit.commandSender;
 
+import tokyo.nakanaka.bukkit.BukkitLogger;
 import tokyo.nakanaka.commandSender.ConsoleCommandSender;
 
-public class BukkitConsoleCommandSender implements ConsoleCommandSender {
-	private org.bukkit.command.ConsoleCommandSender consoleCmdSender;
+public class BukkitConsoleCommandSender extends BukkitLogger implements ConsoleCommandSender {	
 	
 	public BukkitConsoleCommandSender(org.bukkit.command.ConsoleCommandSender consoleCmdSender) {
-		this.consoleCmdSender = consoleCmdSender;
-	}
-
-	@Override
-	public void print(String msg) {
-		this.consoleCmdSender.sendMessage(msg);
+		super(consoleCmdSender);
 	}
 
 }
