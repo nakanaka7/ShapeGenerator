adca962e126f26032ed56c293a5694aa2ee67b1e
nakanakagames <nakanakagames@gmail.com>
2021-08-02 16:48:57 +0900

    extends BukkitLogger

--- src/main/java/tokyo/nakanaka/bukkit/commandSender/BukkitBlockCommandSender.java
+++ src/main/java/tokyo/nakanaka/bukkit/commandSender/BukkitBlockCommandSender.java
@@ -1,30 +1,26 @@
 package tokyo.nakanaka.bukkit.commandSender;
 
 import tokyo.nakanaka.BlockPosition;
+import tokyo.nakanaka.bukkit.BukkitLogger;
 import tokyo.nakanaka.bukkit.BukkitWorld;
 import tokyo.nakanaka.commandSender.BlockCommandSender;
 import tokyo.nakanaka.world.World;
 
-public class BukkitBlockCommandSender implements BlockCommandSender { 
-	private org.bukkit.command.BlockCommandSender blockCmdSender;
+public class BukkitBlockCommandSender extends BukkitLogger implements BlockCommandSender { 
 	
 	public BukkitBlockCommandSender(org.bukkit.command.BlockCommandSender blockCmdSender) {
-		this.blockCmdSender = blockCmdSender;
+		super(blockCmdSender);
 	}
 
 	@Override
 	public BlockPosition getBlockPosition() {
-		org.bukkit.block.Block block = this.blockCmdSender.getBlock();
-		World world = new BukkitWorld(this.blockCmdSender.getServer(), block.getWorld());
+		org.bukkit.command.BlockCommandSender b = (org.bukkit.command.BlockCommandSender)this.sender;
+		org.bukkit.block.Block block = b.getBlock();
+		World world = new BukkitWorld(b.getServer(), block.getWorld());
 		int x = block.getX();
 		int y = block.getY();
 		int z = block.getZ();
 		return new BlockPosition(world, x, y, z);
-	}
-
-	@Override
-	public void print(String msg) {
-		blockCmdSender.sendMessage(msg);
 	}
 
 }
