d0f3f16b22ff3157eca8822b9309f9351dc82dbc
nakanakagames <nakanakagames@gmail.com>
2021-08-19 22:57:11 +0900

    delete

--- src/main/java/tokyo/nakanaka/commandSender/BlockPositionalCommandSender.java
+++
@@ -1,7 +0,0 @@
-package tokyo.nakanaka.commandSender;
-
-import tokyo.nakanaka.BlockPosition;
-
-public interface BlockPositionalCommandSender extends CommandSender {
-	public BlockPosition getBlockPosition();
-}
