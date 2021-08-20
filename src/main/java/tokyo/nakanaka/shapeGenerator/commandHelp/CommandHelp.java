59148f41fcda4d4b965f9d90643d32d789a57897
nakanakagames <nakanakagames@gmail.com>
2021-08-20 22:35:03 +0900

    remove getUsage()

--- src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/CommandHelp.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/CommandHelp.java
@@ -1,8 +1,5 @@
 package tokyo.nakanaka.shapeGenerator.commandHelp;
 
 public interface CommandHelp {
-	/**
-	 * @return a usage, which is a simple plain text(It does not contain colored text)
-	 */
-	String getUsage();
+	
 }
