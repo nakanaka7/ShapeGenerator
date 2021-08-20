569a011dd0ec2306107efd3ca77effe0cc6a7bfb
nakanakagames <nakanakagames@gmail.com>
2021-08-20 22:47:12 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/RotHelp.java
@@ -0,0 +1,9 @@
+package tokyo.nakanaka.shapeGenerator.commandHelp;
+
+public class RotHelp {
+
+	public String getUsage() {
+		return "/sg rot <x|y|z> <degree>";
+	}
+	
+}
---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/ScaleHelp.java
@@ -0,0 +1,9 @@
+package tokyo.nakanaka.shapeGenerator.commandHelp;
+
+public class ScaleHelp {
+
+	public String getUsage() {
+		return "/sg scale <x|y|z> <factor>";
+	}
+	
+}
