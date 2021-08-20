5b7caee57e2dd417c218743a986350cab06fc4ae
nakanakagames <nakanakagames@gmail.com>
2021-08-20 20:19:30 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/CommandHelp.java
@@ -0,0 +1,8 @@
+package tokyo.nakanaka.shapeGenerator.commandHelp;
+
+public interface CommandHelp {
+	/**
+	 * @return a usage, which is a simple plain text(It does not contain colored text)
+	 */
+	String getUsage();
+}
---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/GenrHelp.java
@@ -0,0 +1,10 @@
+package tokyo.nakanaka.shapeGenerator.commandHelp;
+
+public class GenrHelp implements CommandHelp {
+
+	@Override
+	public String getUsage() {
+		return "/sg genr <block>";
+	}
+
+}
