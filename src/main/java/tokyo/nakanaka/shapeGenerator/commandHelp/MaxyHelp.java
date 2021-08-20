3aff121b1b13b24ce8e93df70d632ad7cba80b63
nakanakagames <nakanakagames@gmail.com>
2021-08-20 20:44:39 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/MaxyHelp.java
@@ -0,0 +1,10 @@
+package tokyo.nakanaka.shapeGenerator.commandHelp;
+
+public class MaxyHelp implements CommandHelp {
+	
+	@Override
+	public String getUsage() {
+		return "/sg maxy <value>";
+	}
+	
+}
---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/MaxzHelp.java
@@ -0,0 +1,10 @@
+package tokyo.nakanaka.shapeGenerator.commandHelp;
+
+public class MaxzHelp implements CommandHelp {
+
+	@Override
+	public String getUsage() {
+		return "/sg maxz <value>";
+	}
+
+}
