1a1860595f1a495aa6c33bedbb4a30e79e289c20
nakanakagames <nakanakagames@gmail.com>
2021-08-20 21:07:40 +0900

    used minxHelp class

--- src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/MinXCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/MinXCommandHandler.java
@@ -10,9 +10,11 @@ import tokyo.nakanaka.command.AdjustCommand;
 import tokyo.nakanaka.command.GenerateCommand;
 import tokyo.nakanaka.command.MinXCommand;
 import tokyo.nakanaka.command.UndoableCommand;
+import tokyo.nakanaka.logger.LogColor;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.logger.shapeGenerator.LogDesignColor;
 import tokyo.nakanaka.shapeGenerator.UndoCommandManager;
+import tokyo.nakanaka.shapeGenerator.commandHelp.MinxHelp;
 import tokyo.nakanaka.shapeGenerator.user.User;
 
 public class MinXCommandHandler implements UserCommandHandler {
@@ -38,7 +40,8 @@ public class MinXCommandHandler implements UserCommandHandler {
 	public boolean onCommand(User user, String[] args) {
 		Logger logger = user.getLogger();
 		if(args.length != 1) {
-			return false;
+			logger.print(LogColor.RED + "Usage: " + new MinxHelp().getUsage());
+			return true;
 		}
 		double value;
 		try {
