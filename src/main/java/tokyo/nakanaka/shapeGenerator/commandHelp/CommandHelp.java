95ab8b2ae416f986173b5553a2cbd02db5b81ba6
nakanakagames <nakanakagames@gmail.com>
2021-08-21 16:40:46 +0900

    add toSingleLine()

--- src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/CommandHelp.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/commandHelp/CommandHelp.java
@@ -1,5 +1,10 @@
 package tokyo.nakanaka.shapeGenerator.commandHelp;
 
 public interface CommandHelp {
-	
+	/**
+	 * Return a single line which contains the information for the command.
+	 * This is used by HelpCommandHandler class
+	 * @return a single line which contains the information for the command
+	 */
+	String toSingleLine();
 }
