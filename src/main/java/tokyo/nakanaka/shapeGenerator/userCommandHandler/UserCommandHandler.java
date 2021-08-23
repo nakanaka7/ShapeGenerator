d7ca446c45f0e08dbea3c0bbf261febdf5250f54
nakanakagames <nakanakagames@gmail.com>
2021-08-23 12:40:37 +0900

    change argument of onCommand() and onTabComplete()

--- src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/UserCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/UserCommandHandler.java
@@ -3,10 +3,11 @@ package tokyo.nakanaka.shapeGenerator.userCommandHandler;
 import java.util.List;
 
 import tokyo.nakanaka.commadHelp.ParameterHelp;
+import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
 public interface UserCommandHandler extends CommandEntry {
 	List<ParameterHelp> getParameterHelpList();
-	void onCommand(UserData user, String[] args);
-	List<String> onTabComplete(UserData user, String[] args);
+	void onCommand(UserData userData, CommandSender cmdSender, String[] args);
+	List<String> onTabComplete(UserData userData, CommandSender cmdSender, String[] args);
 }
