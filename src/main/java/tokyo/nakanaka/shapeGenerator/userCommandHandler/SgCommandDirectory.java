18e326e39aff2b0ca961c2af79c82a145aeeabb7
nakanakagames <nakanakagames@gmail.com>
2021-08-24 13:10:23 +0900

    remove commands except SelCommandDirectory

--- src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/SgCommandDirectory.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/SgCommandDirectory.java
@@ -5,30 +5,13 @@ import java.util.List;
 
 import tokyo.nakanaka.commandArgument.BlockCommandArgument;
 import tokyo.nakanaka.selection.selectionStrategy.SelectionStrategySource;
-import tokyo.nakanaka.shapeGenerator.commandHandler.PhyCommandHandler;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
 public class SgCommandDirectory implements CommandDirectory {
 	private List<CommandEntry> subList = new ArrayList<>();
 	
 	public SgCommandDirectory(BlockCommandArgument blockArg, SelectionStrategySource selStraSource) {
-		subList.add(new ShapeCommandHandler(selStraSource));
 		subList.add(new SelCommandDirectory(selStraSource));
-		subList.add(new GenrCommandHandler(blockArg, selStraSource));
-		subList.add(new PhyCommandHandler());
-		subList.add(new ShiftCommandHandler());
-		subList.add(new ScaleCommandHandler());
-		subList.add(new MirrorCommandHandler());
-		subList.add(new RotCommandHandler());
-		subList.add(new MaxXCommandHandler());
-		subList.add(new MaxYCommandHandler());
-		subList.add(new MaxZCommandHandler());
-		subList.add(new MinXCommandHandler());
-		subList.add(new MinYCommandHandler());
-		subList.add(new MinZCommandHandler());
-		subList.add(new DelCommandHandler());
-		subList.add(new UndoCommandHandler());
-		subList.add(new RedoCommandHandler());
 	}
 	
 	@Override
