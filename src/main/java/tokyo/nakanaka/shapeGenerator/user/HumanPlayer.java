cbdac58c0929d406c6b16138e67ce84137225d0b
nakanakagames <nakanakagames@gmail.com>
2021-08-27 20:57:36 +0900

    delete

--- src/main/java/tokyo/nakanaka/shapeGenerator/user/HumanPlayer.java
+++
@@ -1,8 +0,0 @@
-package tokyo.nakanaka.shapeGenerator.user;
-
-import tokyo.nakanaka.shapeGenerator.legacy.Item;
-
-public interface HumanPlayer {
-	void giveItem(Item item, int num);
-	String getName();
-}
