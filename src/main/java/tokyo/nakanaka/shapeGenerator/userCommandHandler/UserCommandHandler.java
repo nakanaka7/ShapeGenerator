caed1dfc4fa91590bd44f5baa5a6b6f35ba80c97
nakanakagames <nakanakagames@gmail.com>
2021-08-27 21:16:56 +0900

    change arg command sender -> player

--- src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/UserCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/UserCommandHandler.java
@@ -2,12 +2,12 @@ package tokyo.nakanaka.shapeGenerator.userCommandHandler;
 
 import java.util.List;
 
+import tokyo.nakanaka.Player;
 import tokyo.nakanaka.commadHelp.ParameterHelp;
-import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
 public interface UserCommandHandler extends CommandEntry {
 	List<ParameterHelp> getParameterHelpList();
-	void onCommand(UserData userData, CommandSender cmdSender, String[] args);
-	List<String> onTabComplete(UserData userData, CommandSender cmdSender, String[] args);
+	void onCommand(UserData userData, Player player, String[] args);
+	List<String> onTabComplete(UserData userData, Player player, String[] args);
 }
