c6131aadd70bfeeb44929049fe03febdcfe169b9
nakanakagames <nakanakagames@gmail.com>
2021-08-28 15:19:35 +0900

    delete

--- src/main/java/tokyo/nakanaka/shapeGenerator/userCommandHandler/CommandDirectory.java
+++
@@ -1,9 +0,0 @@
-package tokyo.nakanaka.shapeGenerator.userCommandHandler;
-
-import java.util.List;
-
-import tokyo.nakanaka.shapeGenerator.user.UserData;
-
-public interface CommandDirectory extends CommandEntry {
-	List<CommandEntry> getSubList(UserData user);
-}
