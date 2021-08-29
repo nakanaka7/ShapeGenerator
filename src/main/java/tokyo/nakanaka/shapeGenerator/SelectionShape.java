17ddc5b8d0438133b2bf1bcdcd2668eb01d2315e
nakanakagames <nakanakagames@gmail.com>
2021-08-29 20:31:06 +0900

    add getSelSubCommandHandlerMap()

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
@@ -1,12 +1,32 @@
 package tokyo.nakanaka.shapeGenerator;
 
+import java.util.Map;
+
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.SelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.cuboid.CuboidSelectionStrategy;
+
 public enum SelectionShape {
-	CUBOID,
-	DIAMOND,
-	SPHERE,
-	TORUS,
-	LINE,
-	TRIANGLE,
-	TETRAHEDRON,
-	REGULAR_POLYGON;
+	CUBOID(new CuboidSelectionStrategy()),
+	DIAMOND(null),
+	SPHERE(null),
+	TORUS(null),
+	LINE(null),
+	TRIANGLE(null),
+	TETRAHEDRON(null),
+	REGULAR_POLYGON(null);
+	
+	private SelectionStrategy selStrtg;
+	
+	private SelectionShape(SelectionStrategy selStrtg) {
+		this.selStrtg = selStrtg;
+	}
+	
+	/**
+	 * Get SelSubCommandHandlerMap
+	 * @return SelSubCommandHandlerMap
+	 */
+	public Map<String, SelSubCommandHandler> getSelSubCommandHandlerMap() {
+		return this.selStrtg.getSelSubCommandHandlerMap();
+	}
+	
 }
