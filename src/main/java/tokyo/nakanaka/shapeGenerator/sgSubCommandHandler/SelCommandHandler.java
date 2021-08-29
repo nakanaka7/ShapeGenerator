4a90fbaf2488e0f9f71d2ee70c5c947640a4e1be
nakanakagames <nakanakagames@gmail.com>
2021-08-29 20:37:01 +0900

    use SelectionShape getSelSubCommandHandlerMap()

--- src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/SelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/SelCommandHandler.java
@@ -1,6 +1,5 @@
 package tokyo.nakanaka.shapeGenerator.sgSubCommandHandler;
 
-import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,24 +9,17 @@ import tokyo.nakanaka.logger.LogColor;
 import tokyo.nakanaka.shapeGenerator.SelSubCommandHandler;
 import tokyo.nakanaka.shapeGenerator.SelectionShape;
 import tokyo.nakanaka.shapeGenerator.commandHelp.SelHelp;
-import tokyo.nakanaka.shapeGenerator.selectionStrategy.SelectionStrategy;
-import tokyo.nakanaka.shapeGenerator.selectionStrategy.cuboid.CuboidSelectionStrategy;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
 /**
  * Handles "/sg sel" command
  */
 public class SelCommandHandler implements SgSubCommandHandler {
-	private Map<SelectionShape, SelectionStrategy> selStrtgMap = new EnumMap<>(SelectionShape.class);
-	
-	public SelCommandHandler() {
-		this.selStrtgMap.put(SelectionShape.CUBOID, new CuboidSelectionStrategy());
-	}
 	
 	@Override
 	public void onCommand(UserData userData, Player player, String[] args) {
 		SelectionShape shape = userData.getSelectionShape();
-		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = this.selStrtgMap.get(shape).getSelSubCommandHandlerMap();
+		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = shape.getSelSubCommandHandlerMap();
 		if(args.length == 0) {
 			player.print(LogColor.RED + "Usage:" + new SelHelp().getUsage());
 			player.print(LogColor.RED + "See help");
@@ -48,7 +40,7 @@ public class SelCommandHandler implements SgSubCommandHandler {
 	@Override
 	public List<String> onTabComplete(UserData userData, Player player, String[] args) {
 		SelectionShape shape = userData.getSelectionShape();
-		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = this.selStrtgMap.get(shape).getSelSubCommandHandlerMap();
+		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = shape.getSelSubCommandHandlerMap();
 		String subLabel = args[0];
 		if(args.length == 1) {
 			return selSubCmdHandlerMap.keySet().stream()
