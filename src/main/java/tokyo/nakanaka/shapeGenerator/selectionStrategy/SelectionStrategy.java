669a19b37bc4fa0dda815511b2fcb345e50736e3
nakanakagames <nakanakagames@gmail.com>
2021-08-30 10:13:40 +0900

    add newRegionData()

--- src/main/java/tokyo/nakanaka/shapeGenerator/selectionStrategy/SelectionStrategy.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/selectionStrategy/SelectionStrategy.java
@@ -3,6 +3,7 @@ package tokyo.nakanaka.shapeGenerator.selectionStrategy;
 import java.util.Map;
 
 import tokyo.nakanaka.shapeGenerator.SelSubCommandHandler;
+import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
 
 /**
  * Holds methods which is special to a selection shape
@@ -13,5 +14,10 @@ public interface SelectionStrategy {
 	 * @return SelSubCommandHandlerMap
 	 */
 	Map<String, SelSubCommandHandler> getSelSubCommandHandlerMap();
+	/**
+	 * Create new region data
+	 * @return new region data
+	 */
+	RegionData newRegionData();
 	
 }
