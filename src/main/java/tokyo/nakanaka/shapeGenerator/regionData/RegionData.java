f29da37166551de6b97e1da78fed827f9fbebdcc
nakanakagames <nakanakagames@gmail.com>
2021-08-30 18:05:29 +0900

    add getDefaultOffset()

--- src/main/java/tokyo/nakanaka/shapeGenerator/regionData/RegionData.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/regionData/RegionData.java
@@ -1,5 +1,6 @@
 package tokyo.nakanaka.shapeGenerator.regionData;
 
+import tokyo.nakanaka.math.Vector3D;
 import tokyo.nakanaka.shapeGenerator.math.boundRegion3D.BoundRegion3D;
 
 /**
@@ -11,4 +12,9 @@ public interface RegionData {
 	 * @return BoundRegion3D object from this data
 	 */
 	BoundRegion3D createBoundRegion3D();
+	/**
+	 * Returns a default offset
+	 * @return a default offset
+	 */
+	Vector3D getDefaultOffset();
 }
