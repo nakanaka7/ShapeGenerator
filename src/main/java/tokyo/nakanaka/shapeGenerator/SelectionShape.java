466c376b83f96c70e2b49515375f1297e56d57f3
nakanakagames <nakanakagames@gmail.com>
2021-08-31 13:07:26 +0900

    add createSelection()

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
@@ -3,6 +3,9 @@ package tokyo.nakanaka.shapeGenerator;
 import java.util.Map;
 
 import tokyo.nakanaka.World;
+import tokyo.nakanaka.math.Vector3D;
+import tokyo.nakanaka.selection.Selection;
+import tokyo.nakanaka.shapeGenerator.math.boundRegion3D.BoundRegion3D;
 import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
 import tokyo.nakanaka.shapeGenerator.selectionStrategy.SelectionStrategy;
 import tokyo.nakanaka.shapeGenerator.selectionStrategy.cuboid.CuboidSelectionStrategy;
@@ -40,6 +43,19 @@ public enum SelectionShape {
 	public SelectionData newSelectionData(World world) {
 		RegionData regData = this.selStrtg.newRegionData();
 		return new SelectionData(world, regData);
+	}
+	
+	/**
+	 * Returns a selection from the selection data
+	 * @param selData a selection data
+	 * @return a selection from the selection data
+	 */
+	public Selection createSelection(SelectionData selData) {
+		World world = selData.getWorld();
+		RegionData regData = selData.getRegionData();
+		BoundRegion3D boundReg = this.selStrtg.createBoundRegion3D(regData);
+		Vector3D offset = selData.getOffset();
+		return new Selection(world, boundReg, offset);
 	}
 	
 }
