db5e8f03c64d96d5ad8be447ce9f292817e9330f
nakanakagames <nakanakagames@gmail.com>
2021-08-31 10:52:07 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/regionTag/RegionTag.java
@@ -0,0 +1,5 @@
+package tokyo.nakanaka.shapeGenerator.regionTag;
+
+public interface RegionTag {
+
+}
---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/regionTag/Vector3DTag.java
@@ -0,0 +1,16 @@
+package tokyo.nakanaka.shapeGenerator.regionTag;
+
+import tokyo.nakanaka.math.Vector3D;
+
+public class Vector3DTag {
+	private Vector3D value;
+
+	public Vector3D getValue() {
+		return value;
+	}
+
+	public void setValue(Vector3D value) {
+		this.value = value;
+	}
+	
+}
