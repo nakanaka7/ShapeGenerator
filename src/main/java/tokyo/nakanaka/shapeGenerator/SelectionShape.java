35ee4191a9178041f504a945717738e134c475e7
nakanakagames <nakanakagames@gmail.com>
2021-09-01 12:23:03 +0900

    add newSelectionData()

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
@@ -1,9 +1,19 @@
 package tokyo.nakanaka.shapeGenerator;
 
+import java.util.List;
 import java.util.Map;
 
+import tokyo.nakanaka.World;
 import tokyo.nakanaka.shapeGenerator.selSubCommandHandler.SelSubCommandHandler;
-import tokyo.nakanaka.shapeGenerator.selectionStrategy.*;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.CuboidSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.DiamondSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.LineSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.RegularPolygonSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.SelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.SphereSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.TetrahedronSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.TorusSelectionStrategy;
+import tokyo.nakanaka.shapeGenerator.selectionStrategy.TriangleSelectionStrategy;
 
 public enum SelectionShape {
 	CUBOID(new CuboidSelectionStrategy()),
@@ -28,4 +38,16 @@ public enum SelectionShape {
 	public Map<String, SelSubCommandHandler> getSelSubCommandHandlerMap() {
 		return this.selStrtg.selSubCommandHandlerMap();
 	}
+	
+	/**
+	 * Get SelectionData
+	 * @param world a world of the selection data
+	 * @return SelectionData
+	 */
+	public SelectionData newSelectionData(World world) {
+		List<String> keyList = this.selStrtg.regionKeyList();
+		String[] regionDataKeys = keyList.toArray(new String[keyList.size()]);
+		return new SelectionData(world, this.selStrtg.defaultOffsetKey(), regionDataKeys);
+	}
+	
 }
