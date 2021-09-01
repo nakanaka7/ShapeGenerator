28a00143ed8a000370588caf488b4cf62139a5ad
nakanakagames <nakanakagames@gmail.com>
2021-09-01 13:23:12 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/ShapeCommandHandler.java
@@ -0,0 +1,50 @@
+package tokyo.nakanaka.shapeGenerator.sgSubCommandHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import tokyo.nakanaka.Player;
+import tokyo.nakanaka.logger.LogColor;
+import tokyo.nakanaka.shapeGenerator.SelectionData;
+import tokyo.nakanaka.shapeGenerator.SelectionShape;
+import tokyo.nakanaka.shapeGenerator.commandHelp.ShapeHelp;
+import tokyo.nakanaka.shapeGenerator.user.UserData;
+
+/**
+ * Handles "/sg shape" command
+ */
+public class ShapeCommandHandler implements SgSubCommandHandler {
+	
+	@Override
+	public void onCommand(UserData userData, Player player, String[] args) {
+		if(args.length != 1) {
+			player.print(LogColor.RED + "Usage: " + new ShapeHelp().getUsage());
+			return;
+		}
+		SelectionShape shape;
+		try{
+			shape = SelectionShape.valueOf(args[0].toUpperCase());
+		}catch(IllegalArgumentException e) {
+			player.print(LogColor.RED + "Invalid shape");
+			return;
+		}
+		SelectionShape original = userData.getSelectionShape();
+		if(shape != original) {
+			userData.setSelectionShape(shape);
+			SelectionData selData = shape.newSelectionData(player.getEntityPosition().world());
+			userData.setSelectionData(selData);
+		}
+		player.print(LogColor.GOLD + "Set the shape -> " + shape);
+	}
+
+	@Override
+	public List<String> onTabComplete(UserData userData, Player player, String[] args) {
+		return switch(args.length) {
+			case 1 -> List.of(SelectionShape.values()).stream()
+					.map(s -> s.toString().toLowerCase())
+					.collect(Collectors.toList());
+			default -> List.of();
+		};
+	}
+	
+}
