5e598d38205e284c9d569bef258c2cdd20345c74
nakanakagames <nakanakagames@gmail.com>
2021-09-01 22:17:13 +0900

    fix

--- src/main/java/tokyo/nakanaka/shapeGenerator/selSubCommandHandler/OffsetCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/selSubCommandHandler/OffsetCommandHandler.java
@@ -1,33 +1,30 @@
 package tokyo.nakanaka.shapeGenerator.selSubCommandHandler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import tokyo.nakanaka.BlockPosition;
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.commandArgument.CoordinateCommandArgument;
 import tokyo.nakanaka.logger.LogColor;
-import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.math.Vector3D;
-import tokyo.nakanaka.selection.SelectionBuildingData;
-import tokyo.nakanaka.selection.SelectionMessenger;
-import tokyo.nakanaka.shapeGenerator.sgSubCommandHandler.SgSubCommandHandler;
+import tokyo.nakanaka.shapeGenerator.SelectionData;
+import tokyo.nakanaka.shapeGenerator.Utils;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
-public class OffsetCommandHandler implements SgSubCommandHandler {
-	private SelectionMessenger selMessenger = new SelectionMessenger();
+public class OffsetCommandHandler implements SelSubCommandHandler {
 	
 	@Override
 	public void onCommand(UserData userData, Player player, String[] args) {
-		Logger logger = userData.getLogger();
+		SelectionData selData = userData.getSelectionData();
 		if(args.length != 0 && args.length != 3) {
-			logger.print(LogColor.RED + "Usage: " + "/sg sel offset [x] [y] [z]");
-			logger.print(LogColor.RED + "Note: When specifing the coordinates, [x], [y], [z] must be given altogether");
+			player.print(LogColor.RED + "Usage: " + "/sg sel offset [x] [y] [z]");
+			player.print(LogColor.RED + "Note: When specifing the coordinates, [x], [y], [z] must be given altogether");
 			return;
 		}
 		Vector3D pos;
 		if(args.length == 0) {
-			pos = new Vector3D(userData.getX(), userData.getY(), userData.getZ());
+			BlockPosition playerPos = player.getBlockPosition();
+			pos = new Vector3D(playerPos.x(), playerPos.y(), playerPos.z());
 		}else if(args.length == 3) {
 			CoordinateCommandArgument coordArg = new CoordinateCommandArgument();
 			double x;
@@ -38,27 +35,29 @@ public class OffsetCommandHandler implements SgSubCommandHandler {
 				y = coordArg.onParsingDouble(args[1], userData.getY());
 				z = coordArg.onParsingDouble(args[2], userData.getZ());
 			}catch(IllegalArgumentException e) {
-				logger.print(LogColor.RED + "Can not parse the coordinates");
+				player.print(LogColor.RED + "Can not parse the coordinates");
 				return;
 			}
 			pos = new Vector3D(x, y, z);
 		}else {
 			return;
 		}
-		SelectionBuildingData selData = userData.getSelectionBuildingData();
 		selData.setOffset(pos);
-		//this.selMessenger.printSelection(logger, userData.getSelectionShapeNew(), selData, "");
-		logger.print(LogColor.RED + "Usage: " + "/sg sel offset [x] [y] [z]");
-		logger.print(LogColor.RED + "Note: When specifing the coordinates, [x], [y], [z] must be given altogether");
+		List<String> lines = Utils.getSelectionMessageLines(selData);
+		for(String line : lines) {
+			player.print(line);
+		}
 	}
 	
 	@Override
 	public List<String> onTabComplete(UserData userData, Player player, String[] args) {
-		if(args.length <= 3) {
-			return Arrays.asList("~");
-		}else {
-			return new ArrayList<>();
-		}
+		BlockPosition pos = player.getBlockPosition();
+		return switch(args.length) {
+			case 1 -> List.of(String.valueOf(pos.x()));
+			case 2 -> List.of(String.valueOf(pos.y()));
+			case 3 -> List.of(String.valueOf(pos.z()));
+			default -> List.of();
+		};
 	}
 
 }
--- src/main/java/tokyo/nakanaka/shapeGenerator/selSubCommandHandler/ResetCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/selSubCommandHandler/ResetCommandHandler.java
@@ -1,42 +1,26 @@
 package tokyo.nakanaka.shapeGenerator.selSubCommandHandler;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.World;
 import tokyo.nakanaka.logger.LogColor;
-import tokyo.nakanaka.logger.Logger;
-import tokyo.nakanaka.selection.SelectionBuildingData;
-import tokyo.nakanaka.selection.SelectionMessenger;
-import tokyo.nakanaka.shapeGenerator.SelectionShapeNew;
+import tokyo.nakanaka.shapeGenerator.SelectionData;
+import tokyo.nakanaka.shapeGenerator.SelectionShape;
 import tokyo.nakanaka.shapeGenerator.Utils;
-import tokyo.nakanaka.shapeGenerator.selectionStrategy.SelectionStrategy;
-import tokyo.nakanaka.shapeGenerator.selectionStrategy.SelectionStrategySource;
-import tokyo.nakanaka.shapeGenerator.sgSubCommandHandler.SgSubCommandHandler;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
-public class ResetCommandHandler implements SgSubCommandHandler {
-	private SelectionStrategySource selStraSource;
-	private SelectionMessenger selMessenger = new SelectionMessenger();
-	
-	public ResetCommandHandler(SelectionStrategySource selStraSource) {
-		this.selStraSource = selStraSource;
-	}
+public class ResetCommandHandler implements SelSubCommandHandler {
 	
 	public void onCommand(UserData userData, Player player, String[] args) {
-		Logger logger = userData.getLogger();
 		if(args.length != 0) {
-			logger.print(LogColor.RED + "Usage: " + "/sg sel reset");
+			player.print(LogColor.RED + "Usage: " + "/sg sel reset");
 			return;
 		}
 		World world = userData.getWorld();
-	//	SelectionShapeNew shape = userData.getSelectionShapeNew();
-		//SelectionStrategy strategy = this.selStraSource.get(shape);
-		//SelectionBuildingData newSelData = new SelectionBuildingData(world, strategy.newRegionBuildingData());
-		//userData.setSelectionBuildingData(newSelData);
-		//String defaultOffsetLabel = strategy.defaultOffsetKey();
-		//this.selMessenger.printSelection(logger, shape, newSelData, defaultOffsetLabel);
+		SelectionShape shape = userData.getSelectionShape();
+		SelectionData newSelData = shape.newSelectionData(world);
+		userData.setSelectionData(newSelData);
 		List<String> lines = Utils.getSelectionMessageLines(userData.getSelectionData());
 		for(String line : lines) {
 			player.print(line);
@@ -44,7 +28,7 @@ public class ResetCommandHandler implements SgSubCommandHandler {
 	}
 	
 	public List<String> onTabComplete(UserData user, Player player, String[] args) {
-		return new ArrayList<>();
+		return List.of();
 	}
 
 }
