5b9db53b55586e2024ca7c10357ba8c0750fdd3b
nakanakagames <nakanakagames@gmail.com>
2021-09-03 13:17:22 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionUtils.java
@@ -0,0 +1,15 @@
+package tokyo.nakanaka.shapeGenerator;
+
+import java.util.Map;
+
+import tokyo.nakanaka.shapeGenerator.selectionShapeDelegator.SelectionShapeDelegator;
+
+public class SelectionUtils {
+	private static Map<SelectionShape, SelectionShapeDelegator> selmap;
+	
+	public static SelectionData newSelectionData(SelectionShape selShape) {
+		//TODO
+		return null;
+	}
+	
+}
