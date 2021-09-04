d4dbf1d2f058a3da68ecfbc924e3922509f174cc
nakanakagames <nakanakagames@gmail.com>
2021-09-04 14:03:12 +0900

    unuse SelectionHandler

--- src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/ShapeCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/ShapeCommandHandler.java
@@ -4,10 +4,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import tokyo.nakanaka.Player;
+import tokyo.nakanaka.World;
 import tokyo.nakanaka.logger.LogColor;
 import tokyo.nakanaka.shapeGenerator.SelectionData;
-import tokyo.nakanaka.shapeGenerator.SelectionHandler;
 import tokyo.nakanaka.shapeGenerator.SelectionShape;
+import tokyo.nakanaka.shapeGenerator.regionData.CuboidRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.DiamondRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.LineRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.RegularPolygonRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.SphereRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.TetrahedronRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.TorusRegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.TriangleRegionData;
 import tokyo.nakanaka.shapeGenerator.sgSubCommandHelp.ShapeHelp;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 
@@ -15,11 +24,6 @@ import tokyo.nakanaka.shapeGenerator.user.UserData;
  * Handles "/sg shape" command
  */
 public class ShapeCommandHandler implements SgSubCommandHandler {
-	private SelectionHandler selHandler;
-	
-	public ShapeCommandHandler(SelectionHandler selHandler) {
-		this.selHandler = selHandler;
-	}
 
 	@Override
 	public void onCommand(UserData userData, Player player, String[] args) {
@@ -37,8 +41,18 @@ public class ShapeCommandHandler implements SgSubCommandHandler {
 		SelectionShape original = userData.getSelectionShape();
 		if(shape != original) {
 			userData.setSelectionShape(shape);
-			SelectionData selData = this.selHandler.newSelectionData(shape);
-			selData.setWorld(player.getEntityPosition().world());
+			RegionData regData = switch(shape) {
+				case CUBOID -> new CuboidRegionData();
+				case DIAMOND -> new DiamondRegionData();
+				case SPHERE -> new SphereRegionData();
+				case TORUS -> new TorusRegionData();
+				case LINE -> new LineRegionData();
+				case TRIANGLE -> new TriangleRegionData();
+				case TETRAHEDRON -> new TetrahedronRegionData();
+				case REGULAR_POLYGON -> new RegularPolygonRegionData();		
+			};
+			World world = player.getEntityPosition().world();
+			SelectionData selData = new SelectionData(world, regData);
 			userData.setSelectionData(selData);
 		}
 		player.print(LogColor.GOLD + "Set the shape -> " + shape);
