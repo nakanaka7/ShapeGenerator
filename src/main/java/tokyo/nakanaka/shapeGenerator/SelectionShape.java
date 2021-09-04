0124b79def57021ee1200ae82f44eb759575ffa0
nakanakagames <nakanakagames@gmail.com>
2021-09-04 16:53:41 +0900

    remove methods

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShape.java
@@ -1,9 +1,5 @@
 package tokyo.nakanaka.shapeGenerator;
 
-import tokyo.nakanaka.BlockPosition;
-import tokyo.nakanaka.Player;
-import tokyo.nakanaka.shapeGenerator.user.UserData;
-
 public enum SelectionShape {
 	CUBOID,
 	DIAMOND,
@@ -13,24 +9,5 @@ public enum SelectionShape {
 	TRIANGLE,
 	TETRAHEDRON,
 	REGULAR_POLYGON;
-	
-	/**
-	 * Handles a left click block event
-	 * @param userData the user data
-	 * @param player the player of the event
-	 * @param blockPos the clicked block position 
-	 */
-	public void onLeftClickBlock(UserData userData, Player player, BlockPosition blockPos) {
-		
-	}
-	
-	/**
-	 * Handles a right click block event
-	 * @param userData the user data
-	 * @param player the player of the event
-	 * @param blockPos the clicked block position 
-	 */
-	public void onRightClickBlock(UserData userData, Player player, BlockPosition blockPos) {
-		
-	}
+
 }
