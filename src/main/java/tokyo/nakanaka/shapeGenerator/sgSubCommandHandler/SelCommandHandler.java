2146d9432ddee9b8a050a773ea93775601f1d12d
nakanakagames <nakanakagames@gmail.com>
2021-09-04 20:47:37 +0900

    rewrite

--- src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/SelCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/SelCommandHandler.java
@@ -1,13 +1,18 @@
 package tokyo.nakanaka.shapeGenerator.sgSubCommandHandler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.logger.LogColor;
+import tokyo.nakanaka.shapeGenerator.SelectionDataCreator;
 import tokyo.nakanaka.shapeGenerator.SelectionHandler;
 import tokyo.nakanaka.shapeGenerator.SelectionShape;
+import tokyo.nakanaka.shapeGenerator.selSubCommandHandler.OffsetCommandHandler;
+import tokyo.nakanaka.shapeGenerator.selSubCommandHandler.ResetCommandHandler;
 import tokyo.nakanaka.shapeGenerator.selSubCommandHandler.SelSubCommandHandler;
 import tokyo.nakanaka.shapeGenerator.sgSubCommandHelp.SelHelp;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
@@ -16,16 +21,21 @@ import tokyo.nakanaka.shapeGenerator.user.UserData;
  * Handles "/sg sel" command
  */
 public class SelCommandHandler implements SgSubCommandHandler {
+	private Map<String, SelSubCommandHandler> commonSelSubCmdHandlerMap = new HashMap<>();
 	private SelectionHandler selHandler;
-
-	public SelCommandHandler(SelectionHandler selHandler) {
+	
+	{
+		this.commonSelSubCmdHandlerMap.put("offset", new OffsetCommandHandler());
+	}
+	
+	public SelCommandHandler(SelectionDataCreator selDataCreator, SelectionHandler selHandler) {
+		this.commonSelSubCmdHandlerMap.put("reset", new ResetCommandHandler(selDataCreator));
 		this.selHandler = selHandler;
 	}
 
 	@Override
 	public void onCommand(UserData userData, Player player, String[] args) {
 		SelectionShape shape = userData.getSelectionShape();
-		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = this.selHandler.selSubCommandHandlerMap(shape);
 		if(args.length == 0) {
 			player.print(LogColor.RED + "Usage:" + new SelHelp().getUsage());
 			player.print(LogColor.RED + "See help");
@@ -34,31 +44,43 @@ public class SelCommandHandler implements SgSubCommandHandler {
 		String subLabel = args[0];
 		String[] subArgs = new String[args.length - 1];
 		System.arraycopy(args, 1, subArgs, 0, args.length - 1);
-		SelSubCommandHandler selSubCmdHandler = selSubCmdHandlerMap.get(subLabel);
-		if(selSubCmdHandler == null) {
-			player.print(LogColor.RED + "Unkown subcommand");
-			player.print(LogColor.RED + "See help");
+		SelSubCommandHandler commonHandler = this.commonSelSubCmdHandlerMap.get(subLabel);
+		if(commonHandler != null) {
+			commonHandler.onCommand(userData, player, subArgs);
 			return;
 		}
-		selSubCmdHandler.onCommand(userData, player, subArgs);
+		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = this.selHandler.selSubCommandHandlerMap(shape);
+		SelSubCommandHandler properHandler = selSubCmdHandlerMap.get(subLabel);
+		if(properHandler != null) {
+			properHandler.onCommand(userData, player, subArgs);
+			return;
+		}
+		player.print(LogColor.RED + "Unkown subcommand");
+		player.print(LogColor.RED + "See help");	
 	}
 
 	@Override
 	public List<String> onTabComplete(UserData userData, Player player, String[] args) {
 		SelectionShape shape = userData.getSelectionShape();
 		Map<String, SelSubCommandHandler> selSubCmdHandlerMap = this.selHandler.selSubCommandHandlerMap(shape);
-		String subLabel = args[0];
 		if(args.length == 1) {
-			return selSubCmdHandlerMap.keySet().stream()
-					.collect(Collectors.toList());
+			List<String> subLabelList = new ArrayList<>(this.commonSelSubCmdHandlerMap.keySet());
+			subLabelList.addAll(selSubCmdHandlerMap.keySet().stream()
+					.collect(Collectors.toList()));
+			return subLabelList;
 		}
+		String subLabel = args[0];
 		String[] subArgs = new String[args.length - 1];
 		System.arraycopy(args, 1, subArgs, 0, args.length - 1);
-		SelSubCommandHandler selSubCmdHandler = selSubCmdHandlerMap.get(subLabel);
-		if(selSubCmdHandler == null) {
-			return List.of();
+		SelSubCommandHandler commonHandler = this.commonSelSubCmdHandlerMap.get(subLabel);
+		if(commonHandler != null) {
+			return commonHandler.onTabComplete(userData, player, subArgs);
 		}
-		return selSubCmdHandler.onTabComplete(userData, player, subArgs);
+		SelSubCommandHandler properHandler = selSubCmdHandlerMap.get(subLabel);
+		if(properHandler != null) {
+			return properHandler.onTabComplete(userData, player, subArgs);
+		}
+		return List.of();
 	}
 
 }
