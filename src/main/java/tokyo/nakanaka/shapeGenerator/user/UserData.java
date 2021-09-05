d0e8b8ab31db3720b37829ad4052cc34ce5eb862
nakanakagames <nakanakagames@gmail.com>
2021-09-05 16:11:44 +0900

    remove unused methods

--- src/main/java/tokyo/nakanaka/shapeGenerator/user/UserData.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/user/UserData.java
@@ -1,42 +1,16 @@
 package tokyo.nakanaka.shapeGenerator.user;
 
-import java.util.UUID;
-
-import tokyo.nakanaka.World;
 import tokyo.nakanaka.logger.Logger;
 import tokyo.nakanaka.shapeGenerator.SelectionData;
 import tokyo.nakanaka.shapeGenerator.SelectionShape;
 import tokyo.nakanaka.shapeGenerator.UndoCommandManager;
 
 public class UserData {
-	private UUID uid;
-	private String name;
 	private Logger logger;
-	private World world;
 	private SelectionShape shape;
 	private SelectionData selData;
 	private UndoCommandManager undoCmdManager = new UndoCommandManager();
 	private boolean blockPhysics = false;
-	
-	public UserData() {
-	}
-	
-	public UserData(UUID uid, String name) {
-		this.uid = uid;
-		this.name = name;
-	}
-
-	public UserData(UUID uid) {
-		this.uid = uid;
-	}
-	
-	public UUID getUniqueId() {
-		return uid;
-	}
-	
-	public String getName() {
-		return name;
-	}
 	
 	public Logger getLogger() {
 		return logger;
@@ -45,15 +19,7 @@ public class UserData {
 	public void setLogger(Logger logger) {
 		this.logger = logger;
 	}
-		
-	public World getWorld() {
-		return world;
-	}
 
-	public void setWorld(World world) {
-		this.world = world;
-	}
-	
 	/**
 	 * Returns a selection shape for the user
 	 * @return a selection shape for the user
