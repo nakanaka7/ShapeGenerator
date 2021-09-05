65adbdd601da691c875e67847d4457655eca77ab
nakanakagames <nakanakagames@gmail.com>
2021-09-05 10:29:47 +0900

    writing

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelSubCommandHandlerMapCreator.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelSubCommandHandlerMapCreator.java
@@ -2,19 +2,22 @@ package tokyo.nakanaka.shapeGenerator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import tokyo.nakanaka.shapeGenerator.selSubCommandHandler.SelSubCommandHandler;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.SelectionShapeStrategy;
 
 public class SelSubCommandHandlerMapCreator {
-	private Map<SelectionShape, Map<String, SelSubCommandHandler>> handlerMap = new HashMap<>();
+	private Map<SelectionShape, Map<String, SubCommandHandler>> handlerMap = new HashMap<>();
 	
 
 	public SelSubCommandHandlerMapCreator(Map<SelectionShape, SelectionShapeStrategy> selStrtgMap) {
-		
+		for(Entry<SelectionShape, SelectionShapeStrategy> e : selStrtgMap.entrySet()) {
+			
+		}
 	}
 	
-	public Map<String, SelSubCommandHandler> selSubCommandHandlerMap(SelectionShape selShape){
+	public Map<String, SubCommandHandler> selSubCommandHandlerMap(SelectionShape selShape){
 		return this.handlerMap.get(selShape);
 	}
 	
