88dbd6169c73717fc1556b4817f767a4ac7defbf
nakanakagames <nakanakagames@gmail.com>
2021-09-06 09:21:19 +0900

    use SelectionHandler

--- src/main/java/tokyo/nakanaka/shapeGenerator/SgEventHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SgEventHandler.java
@@ -1,6 +1,5 @@
 package tokyo.nakanaka.shapeGenerator;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import tokyo.nakanaka.BlockPosition;
@@ -9,7 +8,10 @@ import tokyo.nakanaka.NamespacedID;
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.World;
 import tokyo.nakanaka.event.ClickBlockEvent;
+import tokyo.nakanaka.event.HandType;
 import tokyo.nakanaka.math.BlockVector3D;
+import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
+import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.SelSubCommandHandlerUtils;
 import tokyo.nakanaka.shapeGenerator.user.UserData;
 import tokyo.nakanaka.shapeGenerator.user.UserDataRepository;
 
@@ -37,27 +39,24 @@ public class SgEventHandler {
 		BlockPosition blockPos = evt.getBlockPos();
 		UserData userData = this.userDataRepository.prepareUserData(player);
 		SelectionShape selShape = userData.getSelectionShape();
-		//reset the selection data if the clicked block world is not equal to the world of the selection data
+		//reset the selection data if the world changes or clicked type is left hand
 		World evtWorld = blockPos.world();
-		if(!evtWorld.equals(userData.getSelectionData().getWorld())) {
+		if(!evtWorld.equals(userData.getSelectionData().getWorld()) || evt.getHandType() == HandType.LEFT_HAND) {
 			SelectionData newSelData = this.selHandler.newSelectionData(selShape);
 			newSelData.setWorld(evtWorld);
 			userData.setSelectionData(newSelData);
 		}
 		//update the selection data
-		LinkedHashMap<String,Object> regDataMap = userData.getSelectionData().getRegionDataMap();
+		SelectionData selData = userData.getSelectionData();
+		RegionData regData = selData.getRegionData();
 		BlockVector3D v = new BlockVector3D(blockPos.x(), blockPos.y(), blockPos.z());
 		switch(evt.getHandType()) {
-			case LEFT_HAND -> {
-			}
-			case RIGHT_HAND -> {
-			}
+			case LEFT_HAND -> this.selHandler.setFirstClickData(selShape, regData, v);
+			case RIGHT_HAND -> this.selHandler.setAdditionalClickData(selShape, regData, v);
 		}
 		//print the selection message
-		List<String> lines = Utils.getSelectionMessageLines(userData.getSelectionData());
-		for(String line : lines) {
-			player.print(line);
-		}
+		List<String> lines = SelSubCommandHandlerUtils.selectionMessage(selShape, selData);
+		lines.stream().forEach(player::print);
 	}
 	
 }
