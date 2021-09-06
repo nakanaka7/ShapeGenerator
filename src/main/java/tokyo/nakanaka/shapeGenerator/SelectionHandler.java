94ac4f1bbf8a114b7530334309b15de6ff3db800
nakanakagames <nakanakagames@gmail.com>
2021-09-06 11:40:51 +0900

    remove setFirstClickData() and setAdditionalClickData()

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionHandler.java
@@ -2,7 +2,6 @@ package tokyo.nakanaka.shapeGenerator;
 
 import java.util.Map;
 
-import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.SelectionShapeStrategy;
 
@@ -54,24 +53,6 @@ public class SelectionHandler {
 	 */
 	public String rightClickDescription(SelectionShape selShape) {
 		return selStrtgMap.get(selShape).rightClickDescription();
-	}
-	
-	/**
-	 * Set a first block click position into the region data
-	 * @param regData the region data 
-	 * @param blockPos the clicked block position 
-	 */
-	public void setFirstClickData(SelectionShape selShape, RegionData regionData, BlockVector3D blockPos) {
-		selStrtgMap.get(selShape).setFirstClickData(regionData, blockPos);
-	}
-	
-	/**
-	 * Set data on additional block clicking
-	 * @param regData the region data
-	 * @param blockPos the clicked block position 
-	 */
-	public void setAdditionalClickData(SelectionShape selShape, RegionData regionData, BlockVector3D blockPos) {
-		selStrtgMap.get(selShape).setAdditionalClickData(regionData, blockPos);
 	}
 	
 }
