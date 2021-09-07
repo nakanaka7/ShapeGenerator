211cebc028870de5fb4149429145cdfa3de3fc9e
nakanakagames <nakanakagames@gmail.com>
2021-09-07 19:04:53 +0900

    remove unused methods /add toLinkedHashMap()

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionData.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionData.java
@@ -1,6 +1,7 @@
 package tokyo.nakanaka.shapeGenerator;
 
 import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
 import tokyo.nakanaka.World;
 import tokyo.nakanaka.math.Vector3D;
@@ -12,22 +13,12 @@ import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
  */
 public class SelectionData {
 	private World world;
-	private LinkedHashMap<String, Object> regDataMap;
 	private RegionData regData;
 	private Vector3D offset;
-	
-	public SelectionData() {
-	}
 	
 	public SelectionData(World world, RegionData regData) {
 		this.world = world;
 		this.regData = regData;
-	}
-	
-	@Deprecated
-	public SelectionData(World world, LinkedHashMap<String, Object> regDataMap, String defaultOffsetKey) {
-		this.world = world;
-		this.regDataMap = regDataMap;
 	}
 	
 	/**
@@ -39,30 +30,6 @@ public class SelectionData {
 	}
 	
 	/**
-	 * Set a world to the selection data
-	 * @param world a world to set to the selection data
-	 */
-	public void setWorld(World world) {
-		this.world = world;
-	}
-	
-	/**
-	 * Get the region data map which the selection data has
-	 * @return the region data map which the selection data has
-	 */
-	public LinkedHashMap<String, Object> getRegionDataMap() {
-		return this.regDataMap;
-	}
-	
-	/**
-	 * Set a region data map
-	 * @param regDataMap a region data map
-	 */
-	public void setRegionDataMap(LinkedHashMap<String, Object> regDataMap) {
-		this.regDataMap = regDataMap;
-	}
-
-	/**
 	 * Return the region data
 	 * @return the region data
 	 */
@@ -70,22 +37,6 @@ public class SelectionData {
 		return regData;
 	}
 
-	/**
-	 * Set a region data
-	 * @param regionData a region data
-	 */
-	public void setRegionData(RegionData regionData) {
-		this.regData = regionData;
-	}
-
-	/**
-	 * Returns the offset of this selection
-	 * @return the offset of this selection
-	 */
-	public Vector3D getOffset() {
-		return offset;
-	}
-	
 	/**
 	 * Set an offset of this selection
 	 * @param offset an offset of this selection
@@ -106,6 +57,25 @@ public class SelectionData {
 			offset = regData.defaultOffset();
 		}
 		return new Selection(this.world, boundReg, offset);
+	}
+	
+	/**
+	 * Returns the map which holds this object's information
+	 * @return the map which holds this object's information
+	 */
+	LinkedHashMap<String, String> toLinkedHashMap() {
+		var map = new LinkedHashMap<String, String>();
+		for(Entry<String, String> e : this.regData.toLinkedHashMap().entrySet()) {
+			map.put(e.getKey(), e.getValue());
+		}
+		String offset;
+		if(this.offset != null) {
+			offset = this.offset.toString();
+		}else {
+			offset = this.regData.defaultOffsetLabel();
+		}
+		map.put("offset", offset);
+		return map;
 	}
 		
 }
