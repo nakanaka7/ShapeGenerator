94366d10a550d2aafc904d6458cf0f75186b854d
nakanakagames <nakanakagames@gmail.com>
2021-09-07 09:05:31 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/selectionShapeStrategy/torusSelSubCommandHandler/AxisCommandHandler.java
@@ -0,0 +1,48 @@
+package tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.torusSelSubCommandHandler;
+
+import java.util.List;
+
+import tokyo.nakanaka.Axis;
+import tokyo.nakanaka.Player;
+import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
+import tokyo.nakanaka.shapeGenerator.regionData.TorusRegionData;
+import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.BaseSelSubCommandHandler;
+import tokyo.nakanaka.shapeGenerator.user.UserData;
+
+/**
+ * Handles "/sg sel axis" command in torus selection mode
+ */
+public class AxisCommandHandler extends BaseSelSubCommandHandler<Axis> {
+	
+	public AxisCommandHandler() {
+		super("/sg sel axis <x|y|z>");
+	}
+
+	@Override
+	protected Axis parse(Player player, String[] args) {
+		return switch(args.length) {
+		case 1 -> Axis.valueOf(args[0].toUpperCase());
+		default -> throw new IllegalArgumentException();
+		};
+	}
+
+	@Override
+	protected RegionData newRegionData() {
+		return new TorusRegionData();
+	}
+	
+	@Override
+	protected void setParsedValue(RegionData regData, Axis parsed) {
+		var torusRegData = (TorusRegionData)regData;
+		torusRegData.setAxis(parsed);
+	}
+
+	@Override
+	public List<String> onTabComplete(UserData userData, Player player, String[] args) {
+		return switch(args.length) {
+		case 1 -> List.of("x", "y", "z");
+		default -> List.of();
+		};
+	}
+	
+}
