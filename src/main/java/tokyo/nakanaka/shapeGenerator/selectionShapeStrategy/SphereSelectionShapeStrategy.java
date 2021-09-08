ff68be937590f7aed98f0dec7b5d56fcf5a24ab6
nakanakagames <nakanakagames@gmail.com>
2021-09-08 21:25:52 +0900

    replace new methods with old ones

--- src/main/java/tokyo/nakanaka/shapeGenerator/selectionShapeStrategy/SphereSelectionShapeStrategy.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/selectionShapeStrategy/SphereSelectionShapeStrategy.java
@@ -3,7 +3,19 @@ package tokyo.nakanaka.shapeGenerator.selectionShapeStrategy;
 import java.util.HashMap;
 import java.util.Map;
 
+import tokyo.nakanaka.World;
+import tokyo.nakanaka.math.BlockVector3D;
+import tokyo.nakanaka.math.Vector3D;
+import tokyo.nakanaka.shapeGenerator.Selection;
+import tokyo.nakanaka.shapeGenerator.SelectionData;
 import tokyo.nakanaka.shapeGenerator.SubCommandHandler;
+import tokyo.nakanaka.shapeGenerator.math.boundRegion3D.BoundRegion3D;
+import tokyo.nakanaka.shapeGenerator.math.boundRegion3D.CuboidBoundRegion;
+import tokyo.nakanaka.shapeGenerator.math.boundRegion3D.SphereBoundRegion;
+import tokyo.nakanaka.shapeGenerator.math.region3D.Cuboid;
+import tokyo.nakanaka.shapeGenerator.math.region3D.Region3D;
+import tokyo.nakanaka.shapeGenerator.math.region3D.Region3Ds;
+import tokyo.nakanaka.shapeGenerator.math.region3D.Sphere;
 import tokyo.nakanaka.shapeGenerator.regionData.RegionData;
 import tokyo.nakanaka.shapeGenerator.regionData.SphereRegionData;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.sphereSelSubCommandHandler.CenterCommandHandler;
@@ -12,8 +24,21 @@ import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.sphereSelSubCommandH
 public class SphereSelectionShapeStrategy implements SelectionShapeStrategy{
 
 	@Override
+	public SelectionData newSelectionData(World world) {
+		return new SelectionData(world, "center", "center", "radius");
+	}
+	
+	@Override
 	public RegionData newRegionData() {
 		return new SphereRegionData();
+	}
+		
+	@Override
+	public Map<String, SubCommandHandler> selSubCommandHandlerMap() {
+		Map<String, SubCommandHandler> map = new HashMap<>();
+		map.put("center", new PosCommandHandlerNew("center"));
+		map.put("radius", new LengthCommandHandlerNew("radius"));
+		return map;
 	}
 	
 	@Override
@@ -27,11 +52,32 @@ public class SphereSelectionShapeStrategy implements SelectionShapeStrategy{
 	}
 	
 	@Override
-	public Map<String, SubCommandHandler> selSubCommandHandlerMap() {
-		Map<String, SubCommandHandler> map = new HashMap<>();
-		map.put("center", new CenterCommandHandler());
-		map.put("radius", new RadiusCommandHandler());
-		return map;
+	public void onLeftClick(SelectionData selData, BlockVector3D blockPos) {
+		selData.setExtraData("center", blockPos.toVector3D());
+	}
+	
+	@Override
+	public void onRightClick(SelectionData selData, BlockVector3D blockPos) {
+		Vector3D center = (Vector3D) selData.getExtraData("center");
+		if(center == null) {
+			throw new IllegalStateException();
+		}
+		Vector3D pos = blockPos.toVector3D();
+		Double radius = Math.floor(pos.negate(center).getAbsolute()) + 0.5;
+		selData.setExtraData("radius", radius);
+	}
+	
+	@Override
+	public Selection buildSelection(SelectionData selData) {
+		var center = (Vector3D)selData.getExtraData("center");
+		var radius = (Double)selData.getExtraData("radius");
+		if(center == null || radius == null) {
+			throw new IllegalStateException();
+		}
+		Region3D region = new Sphere(radius);
+		region = Region3Ds.shift(region, center);
+		BoundRegion3D boundReg = new SphereBoundRegion(region, center, radius);
+		return new Selection(selData.world(), boundReg, selData.getOffset());
 	}
 	
 }
