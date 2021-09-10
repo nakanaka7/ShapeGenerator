d04f501572bdddce6f682cfb63864a6ffc10a25c
nakanakagames <nakanakagames@gmail.com>
2021-09-10 19:09:12 +0900

    delete

--- src/main/java/tokyo/nakanaka/shapeGenerator/commandArgument/CommandArgumentList.java
+++
@@ -1,8 +0,0 @@
-package tokyo.nakanaka.shapeGenerator.commandArgument;
-
-import java.util.List;
-
-public interface CommandArgumentList<E> {
-	E onParse(String[] args);
-	List<String> onTabComplete(String[] args);
-}
