c76347ec0f85c20288b72717457439e4913dee0c
nakanakagames <nakanakagames@gmail.com>
2021-09-10 14:38:56 +0900

    remove deprecated

--- src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShapeStrategyRepository.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SelectionShapeStrategyRepository.java
@@ -3,8 +3,6 @@ package tokyo.nakanaka.shapeGenerator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import tokyo.nakanaka.World;
-import tokyo.nakanaka.math.BlockVector3D;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.SelectionShapeStrategy;
 
 /**
@@ -41,78 +39,6 @@ public class SelectionShapeStrategyRepository {
 			throw new IllegalArgumentException();
 		}
 		return this.selStrtgMap.get(selShape);
-	}
-	
-	/**
-	 * Returns a new selection builder for the selection shape
-	 * @param selShape a selection shape
-	 * @param world a world
-	 * @return a new selection builder for the selection shape
-	 */
-	@Deprecated
-	public SelectionData newSelectionData(SelectionShape selShape, World world) {
-		return selStrtgMap.get(selShape).newSelectionData(world);
-	}
-	
-	/**
-	 * Returns a selection from the selection data 
-	 * @param selShape a selection shape
-	 * @param selData a selection data
-	 * @return a selection from the selection data 
-	 * @throws IllegalStateException if the selection data cannot create a selection
-	 */
-	@Deprecated
-	public Selection buildSelection(SelectionShape selShape, SelectionData selData) {
-		return selStrtgMap.get(selShape).buildSelection(selData);
-	}
-	
-	/**
-	 * Get SelSubCommandHandlerMap
-	 * @return SelSubCommandHandlerMap
-	 */
-	@Deprecated
-	public Map<String, SubCommandHandler> selSubCommandHandlerMap(SelectionShape selShape) {
-		return selStrtgMap.get(selShape).selSubCommandHandlerMap();
-	}
-	
-	/**
-	 * Returns a short description for left clicking block event
-	 * @return a short description for left clicking block event
-	 */
-	@Deprecated
-	public String leftClickDescription(SelectionShape selShape) {
-		return selStrtgMap.get(selShape).leftClickDescription();
-	}
-	
-	/**
-	 * Returns a short description for right clicking block event
-	 * @return a short description for right clicking block event
-	 */
-	@Deprecated
-	public String rightClickDescription(SelectionShape selShape) {
-		return selStrtgMap.get(selShape).rightClickDescription();
-	}
-	
-	/**
-	 * Update the selection data on left block click
-	 * @param selShape a selection shape
-	 * @param selData the selection data
-	 * @param blockPos the block position
-	 */
-	@Deprecated
-	public void onLeftClick(SelectionShape selShape, SelectionData selData, BlockVector3D blockPos) {
-		this.selStrtgMap.get(selShape).onLeftClick(selData, blockPos);
-	}
-	
-	/**
-	 * Update the selection data on right block click
-	 * @param selShape a selection shape
-	 * @param selData the selection data
-	 * @param blockPos the block position
-	 */
-	@Deprecated
-	public void onRightClick(SelectionShape selShape, SelectionData selData, BlockVector3D blockPos) {
-		this.selStrtgMap.get(selShape).onRightClick(selData, blockPos);
 	}
 	
 }
