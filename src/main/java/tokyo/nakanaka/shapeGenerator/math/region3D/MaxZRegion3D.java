549bacaea2e1c3054d036387e9f47c71abc4482e
nakanakagames <nakanakagames@gmail.com>
2021-09-12 12:42:39 +0900

    add APIannotation, add java doc

--- src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/MaxZRegion3D.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/MaxZRegion3D.java
@@ -1,8 +1,19 @@
 package tokyo.nakanaka.shapeGenerator.math.region3D;
 
+import tokyo.nakanaka.annotation.PublicAPI;
+
+/**
+ * Represents a region which holds all the points which z coordinate is less than or equals
+ * to a specified value
+ */
+@PublicAPI
 public class MaxZRegion3D implements Region3D {
 	private double maxZ;
 
+	/**
+	 * @param maxZ a z coordinate which specifies the max boundary along z axis. All the point
+	 * which z coordinate is less than or equals to this value is included the region.
+	 */
 	public MaxZRegion3D(double maxZ) {
 		this.maxZ = maxZ;
 	}
