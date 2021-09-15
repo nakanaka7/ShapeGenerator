85b1617697ddfc0815d25255f567b9877aa920b8
nakanakagames <nakanakagames@gmail.com>
2021-09-15 14:03:38 +0900

    java doc

--- src/main/java/tokyo/nakanaka/shapeGenerator/command/AdjustCommand.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/command/AdjustCommand.java
@@ -1,9 +1,18 @@
 package tokyo.nakanaka.shapeGenerator.command;
 
 import tokyo.nakanaka.UndoableCommand;
-import tokyo.nakanaka.annotation.PrivateAPI;
+import tokyo.nakanaka.annotation.PublicAPI;
 
-@PrivateAPI
+/**
+ * A command which adjust existing block generation which is done by GenerateCommand.
+ * The getLastCommand() method will returns GenerateCommand that is responsible for the adjusted generation.
+ * The original generation must be delete in execute() method internally. 
+ */
+@PublicAPI
 public interface AdjustCommand extends UndoableCommand{
+	/**
+	 * Returns GenerateCommand which is responsible for adjusted generation
+	 * @return GenerateCommand which is responsible for adjusted generation
+	 */
 	GenerateCommand getLastCommand();
 }
