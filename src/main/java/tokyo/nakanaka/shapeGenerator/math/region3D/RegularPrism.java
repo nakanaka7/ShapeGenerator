a20164a1e8e6b613c7d29cc7e80b5d85b30d1902
nakanakagames <nakanakagames@gmail.com>
2021-09-15 19:11:27 +0900

    javadoc

--- src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/RegularPrism.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/RegularPrism.java
@@ -7,18 +7,17 @@ import tokyo.nakanaka.shapeGenerator.math.region2D.RegularPolygon;
  * Represents a prism which base is regular polygon. The base is on x-y plane with its center the origin of the plane.
  * It extends to positive z. The x axis always has one vertex of the base polygon, which is the necessary and sufficient information
  * to decide other vertexes.
- * 
  */
 @PublicAPI
 public class RegularPrism implements Region3D {
 	private RegularPolygon rp;
 	private double height;
 	
-	
 	/**
-	 * @param radius the radius of the base disc
+	 * @param radius the radius of the base polygon
+	 * @param vertexNum the vertex number of the base regular polygon
 	 * @param height the height of the cylinder
-	 * @throws if height is smaller than zero
+	 * @throws if radius is smaller than 0, vertex number is smaller than 3, or height is smaller than zero
 	 */
 	public RegularPrism(double radius, int vertexNum, double height) {
 		this.rp = new RegularPolygon(radius, vertexNum);
@@ -27,7 +26,6 @@ public class RegularPrism implements Region3D {
 		}
 		this.height = height;
 	}
-	
 	
 	@Override
 	public boolean contains(double x, double y, double z) {
