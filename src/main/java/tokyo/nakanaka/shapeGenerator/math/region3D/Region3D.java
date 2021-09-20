a1c7293239aa3fa9c9efdbd85b8b50c35d7f4f28
nakanakagames <nakanakagames@gmail.com>
2021-09-20 22:09:11 +0900

    add shift()

--- src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/Region3D.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/Region3D.java
@@ -1,6 +1,7 @@
 package tokyo.nakanaka.shapeGenerator.math.region3D;
 
 import tokyo.nakanaka.annotation.PublicAPI;
+import tokyo.nakanaka.math.Vector3D;
 
 /**
  * Represents 3 dimensional region
@@ -15,4 +16,25 @@ public interface Region3D {
 	 * @return true if this region contains (x, y, z), otherwise false
 	 */
 	boolean contains(double x, double y, double z);
+	
+	/**
+	 * Returns a region which is given by shifting this region
+	 * @param dx the displacement along x axis
+	 * @param dy the displacement along y axis
+	 * @param dz the displacement along z axis
+	 * @return a region which is given by shifting this region
+	 */
+	default Region3D shift(double dx, double dy, double dz) {
+		return new ShiftedRegion3D(this, dx, dy, dz);
+	}
+	
+	/**
+	 * Returns a region which is given by shifting this region
+	 * @param dis the displacement of the shifting
+	 * @return a region which is given by shifting this region
+	 */
+	default Region3D shift(Vector3D dis) {
+		return this.shift(dis.getX(), dis.getY(), dis.getZ());
+	}
+	
 }
