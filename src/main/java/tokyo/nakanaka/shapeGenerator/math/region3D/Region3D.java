7249bac56e8a9a27b823fcf22535d6c23f192d9f
nakanakagames <nakanakagames@gmail.com>
2021-09-23 10:30:41 +0900

    java doc

--- src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/Region3D.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/Region3D.java
@@ -10,14 +10,19 @@ import tokyo.nakanaka.math.Vector3D;
 @PublicAPI
 public interface Region3D {
 	/**
-	 * Returns true if this region contains (x, y, z), otherwise false
-	 * @param x x coordinate of the position
-	 * @param y y coordinate of the position
-	 * @param z z coordinate of the position 
+	 * Returns true if this region contains a given point, otherwise false
+	 * @param x x coordinate of the position of the point
+	 * @param y y coordinate of the position of the point
+	 * @param z z coordinate of the position of the point
 	 * @return true if this region contains (x, y, z), otherwise false
 	 */
 	boolean contains(double x, double y, double z);
 	
+	/**
+	 * Returns true if this region contains a given point, otherwise false
+	 * @param pos position of the point 
+	 * @return true if this region contains a given point, otherwise false
+	 */
 	default boolean contains(Vector3D pos) {
 		return this.contains(pos.getX(), pos.getY(), pos.getZ());
 	}
