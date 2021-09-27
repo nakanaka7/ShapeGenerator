670a3d2609404fca44762a300098eade66ea95ef
nakanakagames <nakanakagames@gmail.com>
2021-09-27 19:37:57 +0900

    deprecated

--- src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/Region3Ds.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/math/region3D/Region3Ds.java
@@ -1,10 +1,9 @@
 package tokyo.nakanaka.shapeGenerator.math.region3D;
 
 import tokyo.nakanaka.annotation.PrivateAPI;
-import tokyo.nakanaka.math.LinearTransformation;
-import tokyo.nakanaka.math.Vector3D;
 
 @PrivateAPI
+@Deprecated
 public class Region3Ds {
 	private Region3Ds() {
 	}
