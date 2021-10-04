51e8c697bac148475899b19c646cb56d682ddabd
nakanakagames <nakanakagames@gmail.com>
2021-10-04 14:55:54 +0900

    add constructor, addSubhelp(), and subHelpList()

--- src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHelp/RootCommandHelp.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHelp/RootCommandHelp.java
@@ -1,8 +1,18 @@
 package tokyo.nakanaka.shapeGenerator.sgSubCommandHelp;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class RootCommandHelp implements CommandHelp {
     private String label;
     private String desc;
+    private LinkedHashMap<String, CommandHelp> subMap = new LinkedHashMap<>();
+
+    public RootCommandHelp(String label, String desc) {
+        this.label = label;
+        this.desc = desc;
+    }
 
     @Override
     public String label() {
@@ -14,9 +24,17 @@ public class RootCommandHelp implements CommandHelp {
         return this.desc;
     }
 
+    public void addSubHelp(CommandHelp cmdHelp) {
+        this.subMap.put(cmdHelp.label(), cmdHelp);
+    }
+
+    public List<CommandHelp> subHelpList() {
+        return new ArrayList<>(this.subMap.values());
+    }
+
     @Override
     public String syntax() {
-        return null;
+        return label + " <subcommand>";
     }
 
 }
