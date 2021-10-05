57eb4da916b2adf8b31c1ea8aba92aa1c6908648
nakanakagames <nakanakagames@gmail.com>
2021-10-05 12:14:00 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHelp/ParameterUsage.java
@@ -0,0 +1,8 @@
+package tokyo.nakanaka.shapeGenerator.sgSubCommandHelp;
+
+/**
+ * @param syntax the syntax of the parameter
+ * @param desc the description of the parameter
+ */
+public record ParameterUsage(String syntax, String desc) {
+}
