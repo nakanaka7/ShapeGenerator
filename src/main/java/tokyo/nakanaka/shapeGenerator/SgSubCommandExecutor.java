0416a1eb1e9a5b481478d2b0d8e1f9e9bbc0e18e
nakanakagames <nakanakagames@gmail.com>
2021-10-07 12:08:33 +0900

    delete

--- src/main/java/tokyo/nakanaka/shapeGenerator/SgSubCommandExecutor.java
+++
@@ -1,8 +0,0 @@
-package tokyo.nakanaka.shapeGenerator;
-
-import tokyo.nakanaka.Player;
-import tokyo.nakanaka.shapeGenerator.playerData.PlayerData;
-
-public interface SgSubCommandExecutor {
-    void onCommand(PlayerData playerData, Player player, String[] args);
-}
