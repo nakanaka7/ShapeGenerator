29df7b93849c5c053ad0fad20cd19f3a9d3bb8cf
nakanakagames <nakanakagames@gmail.com>
2021-10-07 12:40:22 +0900

    code improvement: unuse private method, decrease nested if

--- src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/HelpCommandHandler.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/HelpCommandHandler.java
@@ -2,6 +2,7 @@ package tokyo.nakanaka.shapeGenerator.sgSubCommandHandler;
 
 import tokyo.nakanaka.Player;
 import tokyo.nakanaka.shapeGenerator.CommandLogColor;
+import static tokyo.nakanaka.shapeGenerator.SgSublabel.*;
 import tokyo.nakanaka.shapeGenerator.SubCommandHandler;
 import tokyo.nakanaka.shapeGenerator.playerData.PlayerData;
 import tokyo.nakanaka.shapeGenerator.sgSubCommandHelp.*;
@@ -16,26 +17,22 @@ public class HelpCommandHandler implements SubCommandHandler {
 	private LinkedHashMap<String, CommandHelp> cmdHelpMap = new LinkedHashMap<>();
 
 	public HelpCommandHandler() {
-		registerHelp(SgBranchHelpConstants.HELP);
-		registerHelp(SgBranchHelpConstants.VERSION);
-		registerHelp(SgBranchHelpConstants.WAND);
-		registerHelp(SgBranchHelpConstants.SHAPE);
-		registerHelp(new SelHelp());
-		registerHelp(SgBranchHelpConstants.GENR);
-		registerHelp(SgBranchHelpConstants.PHY);
-		registerHelp(SgBranchHelpConstants.SHIFT);
-		registerHelp(SgBranchHelpConstants.SCALE);
-		registerHelp(SgBranchHelpConstants.MIRROR);
-		registerHelp(SgBranchHelpConstants.ROT);
-		registerHelp(SgBranchHelpConstants.MAX);
-		registerHelp(SgBranchHelpConstants.MIN);
-		registerHelp(SgBranchHelpConstants.DEL);
-		registerHelp(SgBranchHelpConstants.UNDO);
-		registerHelp(SgBranchHelpConstants.REDO);
-	}
-
-	public void registerHelp(CommandHelp cmdHelp){
-		this.cmdHelpMap.put(cmdHelp.label(), cmdHelp);
+		this.cmdHelpMap.put(HELP, SgBranchHelpConstants.HELP);
+		this.cmdHelpMap.put(VERSION, SgBranchHelpConstants.VERSION);
+		this.cmdHelpMap.put(WAND, SgBranchHelpConstants.WAND);
+		this.cmdHelpMap.put(SHAPE, SgBranchHelpConstants.SHAPE);
+		this.cmdHelpMap.put(SEL, new SelHelp());
+		this.cmdHelpMap.put(GENR, SgBranchHelpConstants.GENR);
+		this.cmdHelpMap.put(PHY, SgBranchHelpConstants.PHY);
+		this.cmdHelpMap.put(SHIFT, SgBranchHelpConstants.SHIFT);
+		this.cmdHelpMap.put(SCALE, SgBranchHelpConstants.SCALE);
+		this.cmdHelpMap.put(MIRROR, SgBranchHelpConstants.MIRROR);
+		this.cmdHelpMap.put(ROT, SgBranchHelpConstants.ROT);
+		this.cmdHelpMap.put(MAX, SgBranchHelpConstants.MAX);
+		this.cmdHelpMap.put(MIN, SgBranchHelpConstants.MIN);
+		this.cmdHelpMap.put(DEL, SgBranchHelpConstants.DEL);
+		this.cmdHelpMap.put(UNDO, SgBranchHelpConstants.UNDO);
+		this.cmdHelpMap.put(REDO, SgBranchHelpConstants.REDO);
 	}
 
 	@Override
@@ -66,21 +63,21 @@ public class HelpCommandHandler implements SubCommandHandler {
 			lines.forEach(player::print);
 		}else if(args.length == 1) {
 			CommandHelp cmdHelp = this.cmdHelpMap.get(args[0]);
-			if(cmdHelp != null) {
-				if(cmdHelp instanceof BranchCommandHelp branchHelp){
-					var msgBuilder = new BranchHelpMessageCreator.Builder(cmdLogColor.main(), "/sg", args[0])
-							.description(branchHelp.description());
-					for(int i = 0; i < branchHelp.parameterSyntaxes().length; i++){
-						msgBuilder = msgBuilder.parameter(branchHelp.parameterSyntaxes()[i]
-								, branchHelp.parameterDescription(i));
-					}
-					List<String> lines = msgBuilder.build().toMessageLines();
-					lines.forEach(player::print);
-				}else if(cmdHelp instanceof  SelHelp selHelp){
-					selHelp.toMultipleLines(playerData.getSelectionShape()).forEach(player::print);
-				}
-			}else {
+			if(cmdHelp == null) {
 				player.print(cmdLogColor.error() + "Unknown subcommand");
+				return;
+			}
+			if(cmdHelp instanceof BranchCommandHelp branchHelp){
+				var msgBuilder = new BranchHelpMessageCreator.Builder(cmdLogColor.main(), "/sg", args[0])
+						.description(branchHelp.description());
+				for(int i = 0; i < branchHelp.parameterSyntaxes().length; i++){
+					msgBuilder = msgBuilder.parameter(branchHelp.parameterSyntaxes()[i]
+							, branchHelp.parameterDescription(i));
+				}
+				List<String> lines = msgBuilder.build().toMessageLines();
+				lines.forEach(player::print);
+			}else if(cmdHelp instanceof SelHelp selHelp){
+				selHelp.toMultipleLines(playerData.getSelectionShape()).forEach(player::print);
 			}
 		}else {
 			player.print(cmdLogColor.error() + "Usage: /sg help [subcommand]");
