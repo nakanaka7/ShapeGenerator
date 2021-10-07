7401fb57dfd472949e42b08c67de22a5f0ebf299
nakanakagames <nakanakagames@gmail.com>
2021-10-07 12:08:43 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/SgSublabel.java
@@ -0,0 +1,24 @@
+package tokyo.nakanaka.shapeGenerator;
+
+public class SgSublabel {
+    private SgSublabel(){
+    }
+
+    public static final String HELP = "help";
+    public static final String VERSION = "version";
+    public static final String WAND = "wand";
+    public static final String SHAPE = "shape";
+    public static final String SEL = "sel";
+    public static final String GENR = "genr";
+    public static final String PHY = "phy";
+    public static final String SHIFT = "shift";
+    public static final String SCALE = "scale";
+    public static final String MIRROR = "mirror";
+    public static final String ROT = "rot";
+    public static final String MAX = "max";
+    public static final String MIN = "min";
+    public static final String DEL = "del";
+    public static final String UNDO = "undo";
+    public static final String REDO = "redo";
+
+}
