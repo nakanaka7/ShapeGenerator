15320ced0a053c9b7746fb4b0850f91cc1ffe42a
nakanakagames <nakanakagames@gmail.com>
2021-10-11 18:05:51 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/mirror/MirrorTabCompleter.java
@@ -0,0 +1,20 @@
+package tokyo.nakanaka.shapeGenerator.sgSubCommandHandler.mirror;
+
+import tokyo.nakanaka.Player;
+import tokyo.nakanaka.shapeGenerator.SgSubcommandTabCompleter;
+import tokyo.nakanaka.shapeGenerator.playerData.PlayerData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MirrorTabCompleter implements SgSubcommandTabCompleter {
+
+    @Override
+    public List<String> onTabComplete(PlayerData playerData, Player player, String[] args) {
+        return switch(args.length) {
+            case 1 -> List.of("x", "y", "z");
+            default -> new ArrayList<>();
+        };
+    }
+    
+}
