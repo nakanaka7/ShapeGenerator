bd8e5d5a9d49852f11eb2bcc21465338b00ab625
nakanakagames <nakanakagames@gmail.com>
2021-10-12 11:36:02 +0900

    not use SgEventHandler

--- src/main/java/tokyo/nakanaka/shapeGenerator/Main.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/Main.java
@@ -1,13 +1,15 @@
 package tokyo.nakanaka.shapeGenerator;
 
-import tokyo.nakanaka.Player;
-import tokyo.nakanaka.SemVer;
+import tokyo.nakanaka.*;
 import tokyo.nakanaka.annotation.PublicAPI;
 import tokyo.nakanaka.commandSender.CommandSender;
 import tokyo.nakanaka.event.ClickBlockEvent;
 import tokyo.nakanaka.logger.LogColor;
+import tokyo.nakanaka.math.BlockVector3D;
+import tokyo.nakanaka.shapeGenerator.message.MessageUtils;
 import tokyo.nakanaka.shapeGenerator.playerData.PlayerData;
 import tokyo.nakanaka.shapeGenerator.playerData.PlayerDataRepository;
+import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.SelectionShapeStrategy;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.cone.ConeSelectionShapeStrategy;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.cuboid.CuboidSelectionShapeStrategy;
 import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.cylinder.CylinderSelectionShapeStrategy;
@@ -68,15 +70,15 @@ import java.util.Map;
 public class Main {
 	public static final String SG = "/sg";
 	private PlayerDataRepository playerDataRepository;
+	private SelectionShapeStrategyRepository shapeStrtgRepo;
 	private Map<String, SgSubcommandExecutor> cmdExecutorMap = new HashMap<>();
 	private Map<String, SgSubcommandTabCompleter> tabCompleterMap = new HashMap<>();
 
-	private SgEventHandler sgEvtHandler;
-	
 	public Main(BlockIDListFactory blockIDListFactory) {
 		List<SelectionShape> shapeList = new ArrayList<>();
 		Map<SelectionShape, InitialSelectionDataCreator> dataCreatorMap = new HashMap<>();
 		SelectionShapeStrategyRepository shapeStrtgRepo = new SelectionShapeStrategyRepository();
+		this.shapeStrtgRepo = shapeStrtgRepo;
 		shapeList.add(SelectionShape.CUBOID);
 		var cuboidStrtg = new CuboidSelectionShapeStrategy();
 		dataCreatorMap.put(SelectionShape.CUBOID, cuboidStrtg::newSelectionData);
@@ -180,7 +182,6 @@ public class Main {
 		this.tabCompleterMap.put(SgSublabel.UNDO, new UndoTabCompleter());
 		this.cmdExecutorMap.put(SgSublabel.REDO, new RedoCommandExecutor());
 		this.tabCompleterMap.put(SgSublabel.REDO, new RedoTabCompleter());
-		this.sgEvtHandler = new SgEventHandler(playerDataRepository, shapeStrtgRepo);
 	}
 
 	/**
@@ -240,9 +241,39 @@ public class Main {
 	 * Handles a click block event
 	 * @param evt a click block event
 	 */
-
 	public void onClickBlockEvent(ClickBlockEvent evt) {
-		this.sgEvtHandler.onClickBlockEvent(evt);
+		//ignore the event if the clicking item is not "minecraft:blaze_rod"
+		Item item = evt.getItemStack().getItem();
+		if(!item.equals(new Item(new NamespacedID("minecraft", "blaze_rod")))) {
+			return;
+		}
+		evt.cancel();
+		Player player = evt.getPlayer();
+		BlockPosition blockPos = evt.getBlockPos();
+		PlayerData playerData = this.playerDataRepository.preparePlayerData(player);
+		SelectionShape selShape = playerData.getSelectionShape();
+		SelectionShapeStrategy shapeStrtg = this.shapeStrtgRepo.get(selShape);
+		//reset the selection builder if the world changes
+		World evtWorld = blockPos.world();
+		if(!evtWorld.equals(playerData.getSelectionData().world())) {
+			SelectionData newSelData = shapeStrtg.newSelectionData(evtWorld);
+			playerData.setSelectionData(newSelData);
+		}
+		//update the selection builder
+		SelectionData selData = playerData.getSelectionData();
+		BlockVector3D v = new BlockVector3D(blockPos.x(), blockPos.y(), blockPos.z());
+		try{
+			switch(evt.getHandType()) {
+				case LEFT_HAND -> shapeStrtg.onLeftClick(selData, v);
+				case RIGHT_HAND -> shapeStrtg.onRightClick(selData, v);
+			}
+		}catch(IllegalStateException e) {
+			player.print(LogColor.RED + "Invalid Operation");
+			return;
+		}
+		//print the selection message
+		List<String> lines = MessageUtils.selectionMessage(LogColor.GOLD, selShape, selData);
+		lines.stream().forEach(player::print);
 	}
 		
 }
