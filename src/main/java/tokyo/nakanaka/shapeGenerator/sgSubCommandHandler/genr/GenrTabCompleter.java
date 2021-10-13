b6b03364147a6a1513a55799f87feb48976a3c00
nakanakagames <nakanakagames@gmail.com>
2021-10-13 09:54:26 +0900

    first commit

---
+++ src/main/java/tokyo/nakanaka/shapeGenerator/sgSubCommandHandler/genr/GenrTabCompleter.java
@@ -0,0 +1,26 @@
+package tokyo.nakanaka.shapeGenerator.sgSubCommandHandler.genr;
+
+import tokyo.nakanaka.Player;
+import tokyo.nakanaka.shapeGenerator.BlockIDListFactory;
+import tokyo.nakanaka.shapeGenerator.SgSubcommandTabCompleter;
+import tokyo.nakanaka.shapeGenerator.playerData.PlayerData;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GenrTabCompleter implements SgSubcommandTabCompleter {
+    private BlockIDListFactory blockIDFactory;
+    public GenrTabCompleter(BlockIDListFactory blockIDFactory) {
+        this.blockIDFactory = blockIDFactory;
+    }
+
+    public List<String> onTabComplete(PlayerData playerData, Player player, String[] args) {
+        return switch(args.length) {
+            case 1 -> this.blockIDFactory.getBlockIDList().stream()
+                    .map(s -> s.toString())
+                    .collect(Collectors.toList());
+            default -> List.of();
+        };
+    }
+
+}
