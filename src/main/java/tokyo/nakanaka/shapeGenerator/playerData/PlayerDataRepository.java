612414510bd67c3c6d81b7f7e264386e91467b9f
nakanakagames <nakanakagames@gmail.com>
2021-10-15 12:16:15 +0900

    change constructor

--- src/main/java/tokyo/nakanaka/shapeGenerator/playerData/PlayerDataRepository.java
+++ src/main/java/tokyo/nakanaka/shapeGenerator/playerData/PlayerDataRepository.java
@@ -4,19 +4,14 @@ import tokyo.nakanaka.Player;
 import tokyo.nakanaka.World;
 import tokyo.nakanaka.shapeGenerator.SelectionData;
 import tokyo.nakanaka.shapeGenerator.SelectionShape;
-import tokyo.nakanaka.shapeGenerator.SelectionShapeStrategyRepository;
+import tokyo.nakanaka.shapeGenerator.selectionShapeStrategy.cuboid.CuboidSelectionShapeStrategy;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class PlayerDataRepository {
-	private SelectionShapeStrategyRepository shapeStrtgRepo;
 	private Map<UUID, PlayerData> playerDataMap = new HashMap<>();
-	
-	public PlayerDataRepository(SelectionShapeStrategyRepository shapeStrtgRepo) {
-		this.shapeStrtgRepo = shapeStrtgRepo;
-	}
 
 	/**
 	 * Returns a player data. If there is a player data for the player, return it, otherwise create new one and return it.
@@ -31,7 +26,7 @@ public class PlayerDataRepository {
 			SelectionShape defaultShape = SelectionShape.CUBOID;
 			playerData.setSelectionShape(defaultShape);
 			World world = player.getEntityPosition().world();
-			SelectionData selData = this.shapeStrtgRepo.get(defaultShape).newSelectionData(world);
+			SelectionData selData = CuboidSelectionShapeStrategy.newSelectionData(world);
 			playerData.setSelectionData(selData);
 			this.playerDataMap.put(uid, playerData);
 		}
